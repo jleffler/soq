2faf094558008f7963762e424d506c4e470e552b
Jonathan Leffler <jonathan.leffler@gmail.com>
2012-02-18 19:47:55 -0800

    Add unwrap.c - it is an intriguing idea

---
+++ unwrap.c
@@ -0,0 +1,24 @@
+extern int printf(const char *format, ...);
+
+/* "unwrapper": */
+#define UNWRAP(...) __VA_ARGS__
+
+struct a { int a1; double a2; };
+struct b { struct a a; int b; };
+
+/* your macros: */
+#define WRAP(NAME, ELEMS) static const char *NAME[] = { UNWRAP ELEMS }
+#define ARRAY(type, name, initializer) static const type name[] = { UNWRAP initializer }
+
+int
+main (void)
+{
+    WRAP (some_test, ("a", "b", "c"));
+    ARRAY(char, another, ('a', 'b', 'c'));
+    ARRAY(struct a, more, (1, 2.0));
+    ARRAY(struct b, less, ({ 1, 2.0 }, 3));
+    printf ("The second element in some_test is: '%s'\n", some_test[1]);
+    printf ("The second element in another is: '%c'\n", another[1]);
+    return 0;
+}
+
