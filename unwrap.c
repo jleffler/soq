4dfb3665ecfcf37423eeba5bcac7e193d9bd11ed
Jonathan Leffler <jonathan.leffler@gmail.com>
2012-06-01 10:09:27 -0700

    Sanitize to the point of compiling cleanly (and running).  Layout leaves much to be desired.

--- unwrap.c
+++ unwrap.c
@@ -15,10 +15,12 @@ main (void)
 {
     WRAP (some_test, ("a", "b", "c"));
     ARRAY(char, another, ('a', 'b', 'c'));
-    ARRAY(struct a, more, (1, 2.0));
-    ARRAY(struct b, less, ({ 1, 2.0 }, 3));
+    ARRAY(struct a, more, ({1, 2.0}));
+    ARRAY(struct b, less, ({{ 1, 2.0 }, 3)});
     printf ("The second element in some_test is: '%s'\n", some_test[1]);
     printf ("The second element in another is: '%c'\n", another[1]);
+    printf ("The first  element in more is: %d\n", more[0].a1);
+    printf ("The first  element in less is: %d\n", less[0].a.a1);
     return 0;
 }
 
