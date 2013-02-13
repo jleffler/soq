7330d5a2371218fa7e33db9338bdc2d017fac1fb
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-02-13 06:11:50 -0800

    Reinstate msort.c.  Fixed up sortAtt.c

--- sortAtt.c
+++ sortAtt.c
@@ -1,152 +1,86 @@
+#include <assert.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-void isort(int a[], int n)
+static void sort_check(int *array, size_t n)
 {
-    int i, j, val;
-	
-    for(i=1; i<n; i++)
+    size_t fail = 0;
+
+    for (size_t i = 1; i < n; i++)
     {
-        j=i-1;
-        val = a[i];
-        while(j >= 0 && a[j] > val)
+        if (array[i-1] > array[i])
         {
-            a[j+1] = a[j];
-            j--;
+            fprintf(stderr, "Elements %zu (value %d) and %zu (value %d) are out of order\n",
+                    i-1, array[i-1], i, array[i]);
+            fail++;
         }
-        a[j+1] = val;
-		
     }
+    if (fail != 0)
+        exit(1);
 }
 
-void ssort(int a[], int n)
+static void dump_int_array(int *a, int n)
 {
-    int i, j, small, temp;
-	
-    for (i = 0; i < n-1; i++)
+    printf("%d: ", n);
+    for (int i = 0; i < n; i++)
+        printf(" %d", a[i]);
+    putchar('\n');
+}
+
+static void msort_intR(int a[], int lo, int hi, int scratch[])
+{
+    int i, j, k, m;
+
+    if (lo >= hi)
+        return;
+
+    printf("-->> msort_intR(%u)\n", hi - lo + 1);
+    dump_int_array(&a[lo], hi - lo + 1);
+    m = (lo + hi) / 2;
+
+    msort_intR(a, lo, m, scratch);
+    msort_intR(a, m+1, hi, scratch);
+
+    i = lo;    // lhs index
+    j = m + 1; // rhs index
+    k = 0;     // index into scratch
+
+    while (i <= m && j <= hi)
     {
-        small = i;
-        for (j = i+1; j < n; j++)
-            if (a[j] < a[small])
-                small = j;
-        temp = a[small];
-        a[small] = a[i];
-        a[i] = temp;
+        if (a[i] < a[j])
+            scratch[k++] = a[i++];
+        else
+            scratch[k++] = a[j++];
+    }
+    while (i <= m)
+        scratch[k++] = a[i++];
+    while (j <= hi)
+        scratch[k++] = a[j++];
+
+    for (k=0, i=lo; i<=hi; i++, k++)
+        a[i] = scratch[k];
+
+    printf("<<-- msort_intR(%u)\n", hi -lo + 1);
+    dump_int_array(&a[lo], hi - lo + 1);
+}
+
+static void msort_int(int a[], int n)
+{
+    int *scratch = (int *)malloc(n * sizeof(int));
+
+    if (scratch != 0)
+    {
+        msort_intR(a, 0, n-1, scratch);
+        free(scratch);
     }
 }
 
-
-void msort_intR(int a[], int lo, int hi, int scratch[]) {
-	int i, j, k, m;
-	
-	if(lo >= hi)
-		return;
-	m = (lo + hi)/2;
-	msort_intR(a, lo, m, scratch);
-	msort_intR(a, m+1, hi, scratch);
-	
-	i=lo;  // lhs index
-	j=m+1; // rhs index
-	k=0;   // index into scratch
-	
-	while(i <= m && j <= hi) {
-		if(a[i] < a[j]){
-			scratch[k] = a[i];
-			i++;
-		}
-		else{
-			scratch[k] = a[j];
-			j++;
-		}
-		k++;
-	}
-	while(i <= m) {
-		scratch[k] = a[i];
-		i++;
-		k++;
-	}
-	while(j <= hi) {
-		scratch[k] = a[j];
-		j++;
-		k++;
-	}
-	for(k=0, i=lo; i<=hi; i++, k++) {
-		a[i] = scratch[k];
-	}
-	
-	
-	
-}
-void msort_int(int a[], int n) {
-    int *scratch;
-    scratch = (int *)malloc(n * sizeof(int));
-	
-    msort_intR(a, 0, n-1, scratch);
-    free(scratch);
-}
-
-
-/** \brief Implementation of merge sort algorithm for sorting generic
- *     data a la qsort library function.
- *
- * \param base is the base address of the array being sorted
- * \param n is the number of elements in the array
- * \param size is the size of an individual
- * \return
- *
- */
-//void msort(void *base, size_t n, size_t size,
-//int (*cmp)(const void *, const void *)){
-//}
-
-//int intcmp(const void *a, const void *b)
-//{
-//return (*(int*)a - *(int*)b);
-//}
-
-//void print_points(int base[], int n){
-//int i;
-
-//for(i=0; i<n; i++){
-//printf("%f\n", base[i]);
-//}
-//}
-
-
-
-/*void msort (void *base, size_t nmemb, size_t size,
- int (*fcmp) (const void *, const void *))
- {
- void *tmp;
- 
- if (nmemb < 2)
- return;
- 
- if (!(tmp = malloc (nmemb * size)))
- return;
- 
- msort (base, nmemb - 1, size, fcmp);
- 
- free (tmp);
- }*/
-
-/*void msort(void *base, size_t n, size_t size,
- int (*cmp)(const void *, const void *)){
- 
- void *temp;
- 
- temp = malloc(size * n);
- free(temp);
- (void) msort(base, n, size, fcmp);
- 
- }*/
-
-int cmp (const void *v1, const void *v2)
+static int cmp(const void *v1, const void *v2)
 {
-	int x = *(int*) v1;
-	int y = *(int*) v2;
-    
+    int x = *(int*) v1;
+    int y = *(int*) v2;
+
     if (x < y)
         return -1;
     else if (x > y)
@@ -155,53 +89,33 @@ int cmp (const void *v1, const void *v2)
         return 0;
 }
 
-/*void msort (
-			void *b,
-			size_t n,
-			size_t s,
-			int(*cmp)(const void*,const void*))
-{
-	void *a;
-	void *b;
-	
-	
-}*/
-
-
 static void msort(void *b, size_t n, size_t s, int (*cmp)(const void *v1, const void *v2) )
 {
     if (n <= 1)
         return;     /* Already sorted */
-	
-    printf("-->> msort(%zu)\n", n);
+
+    //printf("-->> msort(%zu)\n", n);
+    //dump_array(stdout, "Entry to msort()", (int *)b, n);
     void *t = malloc(s*n);
-	
+
     if (t == NULL)
     {
         fprintf(stderr, "Error: No Memory.\n");
-        printf("<<-- msort(%zu)\n", n);
+        //printf("<<-- msort(%zu)\n", n);
         return;
     }
-	
+
     size_t n1 = n / 2;
     size_t n2 = n - n1;
-	
-    if (n2 <= n1)
-    {
-        fprintf(stderr, "Oops: %zu <= %zu\n", n2, n1);
-        free(t);
-        printf("<<-- msort(%zu)\n", n);
-        return;
-    }
-	
+
     char *b1 = b;
     char *b2 = (char *) b + (n1 * s);
-	
-    msort(b1, n2, s, cmp);
-    msort(b2, n1+1, s, cmp);
-	
+
+    msort(b1, n1, s, cmp);
+    msort(b2, n2, s, cmp);
+
     char *tmp = t;
-	
+
     while (n1 > 0 && n2 > 0)
     {
         if ((*cmp)(b1, b2) <= 0)
@@ -221,79 +135,82 @@ static void msort(void *b, size_t n, size_t s, int (*cmp)(const void *v1, const 
     }
     if (n1 > 0)
         memcpy(tmp, b1, n1 * s);
-    memcpy(b, t, (n - n2) * s);
+    else if (n2 > 0)
+        memcpy(tmp, b2, n2 * s);
+    memcpy(b, t, n * s);
     free(t);
-    printf("<<-- msort(%zu)\n", n);
+
+    //dump_array(stdout, "Exit from msort()", (int *)b, n);
+    //printf("<<-- msort(%zu)\n", n);
 }
 
-int * gen_int_array(int n, int max_val) {
-	int i;
-	int *a = malloc(n*sizeof(int));
-	for(i=0; i<n; i++){
-		a[i] = rand() % max_val;
-	}
-	return a;
-}
-
-int * clone_int_array(int *a, int n){
-	int *b = (int *)malloc(n * sizeof(int));
-	int i;
-	for(i=0; i<n; i++)
-		b[i] = a[i];
-	return b;
-}
-
-
-void dump_int_array(int *a, int n) {
-	int i;
-	for(i=0; i<n; i++)
-		printf("  %d\n", a[i]);
-}
-
-int main()
+static int *gen_int_array(int n, int max_val)
 {
-    int n;
-    int *a, *b, *c;
-	
-    printf("enter n: ");
-    if(scanf("%d", &n) != 1 || n < 10)
+    int *a = malloc(n*sizeof(int));
+    if (a != 0)
     {
-        fprintf(stderr, "bad input.  goodbye\n");
-        abort();
+        for (int i = 0; i < n; i++)
+        {
+            a[i] = rand() % max_val;
+        }
     }
-	
-    printf("sizeof(double) %d\n", sizeof(double) );
-	
-    printf("running experiments with n=%d\n", n);
-	
-    a = gen_int_array(n, 5000);
-    //b = clone_int_array(a, n);
-    //c = clone_int_array(a, n);
-	
-	d = clone_int_array(a, n);
-    //ssort(a, n);
-    //isort(b, n);
-    //msort_int(c, n);
-	
-	//msort_int(d, n);
-	
-	msort (d, n, sizeof(int), cmp );
-	//qsort (d, n, sizeof(int), cmp);
-    //print_points(c, 5);
-	
-	if(n<50)
-        dump_int_array(d, n);
-	
-    free(a);
-    free(b );
-    free(c);
-	
+    return a;
+}
 
-	
-    //if(n<50)
-	//dump_int_array(c, n);
-	//if(n<50)
-	//dump_int_array(d, n);	
-	
+static int *clone_int_array(int *a, int n)
+{
+    int *b = (int *)malloc(n * sizeof(int));
+    if (b != 0)
+    {
+        for (int i = 0; i < n; i++)
+            b[i] = a[i];
+    }
+    return b;
+}
+
+int main(int argc, char **argv)
+{
+    int n = 10;
+    int *a;
+    int *b = 0;
+    int *c = 0;
+    int *d;
+
+    if (argc > 1)
+        n = atoi(argv[1]);
+    else
+    {
+        printf("enter n: ");
+        if (scanf("%d", &n) != 1 || n < 10)
+        {
+            fprintf(stderr, "bad input (%d: should be at least 10). Goodbye\n", n);
+            exit(1);
+        }
+    }
+
+    if (n <= 0)
+        n = 10;
+
+    printf("running experiments with n=%d\n", n);
+
+    a = gen_int_array(n, 5000);
+    c = clone_int_array(a, n);
+    d = clone_int_array(a, n);
+
+    msort_int(c, n);
+    msort(d, n, sizeof(int), cmp);
+
+    sort_check(d, n);
+    sort_check(c, n);
+
+    if (n < 50)
+        dump_int_array(d, n);
+
+    free(a);
+    free(b);
+    free(c);
+    free(d);
+
+    return(0);
 }
 
