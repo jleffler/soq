7649884a2cea5501cd57fd522722c284180c3161
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-06-02 21:19:38 -0700

    Messy code but it passes

--- bst.c
+++ bst.c
@@ -10,35 +10,44 @@ struct BST
     BST *right;
 };
 
-static int roottoleaf(const BST *tree, int target)
+static int roottoleaf(const BST *tree, int local_target, int overall_target)
 {
     if (tree == 0)
         return 0;
     if (tree->left == 0 && tree->right == 0)
-        return(tree->data == target);
-    if (tree->data > target)
-        return 0;
-    if (roottoleaf(tree->left, target - tree->data))
+        return(tree->data == local_target);
+    // Is there a path including this node?
+    if (tree->data < local_target)
+    {
+        // Path to leaf starts at this node via left
+        if (roottoleaf(tree->left, local_target - tree->data, overall_target))
+            return 1;
+        // Path to leaf starts at this node via right
+        if (roottoleaf(tree->right, local_target - tree->data, overall_target))
+            return 1;
+    }
+    // Path to leaf starts below this node on left
+    if (roottoleaf(tree->left, overall_target, overall_target))
         return 1;
-    if (roottoleaf(tree->right, target - tree->data))
+    // Path to leaf starts below this node on right
+    if (roottoleaf(tree->right, overall_target, overall_target))
         return 1;
     return 0;
 }
 
 static int pathtoleaf(const BST *tree, int target)
 {
-    if (tree == 0)
-        return 0;
-    if (tree->left == 0 && tree->right == 0)
-        return(tree->data == target);
-    if (roottoleaf(tree, target))
-        return 1;
-    if (pathtoleaf(tree->left, target))
-        return 1;
-    if (pathtoleaf(tree->right, target))
-        return 1;
-    else
-        return 0;
+    return roottoleaf(tree, target, target);
+//    if (tree == 0)
+//        return 0;
+//    if (roottoleaf(tree, target))
+//        return 1;
+//    if (pathtoleaf(tree->left, target))
+//        return 1;
+//    if (pathtoleaf(tree->right, target))
+//        return 1;
+//    else
+//        return 0;
 }
 
 static void printtoleaf(const BST *tree)
