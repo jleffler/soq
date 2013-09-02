f83ed50999925f48d1b961390079f082c5cbb702
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-09-01 18:21:46 -0700

    e2big.c: convert to binary search and account for environment

--- e2big.c
+++ e2big.c
@@ -1,3 +1,6 @@
+/* SO 18559403: How big an argument list is allowed */
+
+#include <assert.h>
 #include <errno.h>
 #include <fcntl.h>
 #include <stdio.h>
@@ -7,118 +10,158 @@
 #include <unistd.h>
 #include <signal.h>
 
+extern char **environ;  /* Sometimes in <unistd.h> */
+
 enum { BYTES_PER_KIBIBYTE = 1024 };
 enum { BYTES_PER_MEBIBYTE = BYTES_PER_KIBIBYTE * BYTES_PER_KIBIBYTE };
 enum { E_GOT_E2BIG =  37 };
 enum { E_NOT_E2BIG = 219 };
+
+enum { R_TOO_LARGE = +1, R_TOO_SMALL = -1 };
 
 static void sigchld_handler(int signum)
 {
     signal(signum, sigchld_handler);
 }
 
+static int test_arg_size(int size)
+{
+    int result = R_TOO_SMALL;
+    assert(size % 8 == 0);
+    fflush(0);
+    pid_t pid = fork();
+    if (pid < 0)
+    {
+        fprintf(stderr, "Failed to fork at size %d\n", size);
+        exit(1);
+    }
+    else if (pid == 0)
+    {
+        int self = getpid();
+        printf("Child: %d\n", self);
+        char *args[10] = { "ls" };
+        size_t bytes_per_arg = size / 8;
+        for (int j = 1; j < 9; j++)
+        {
+            args[j] = malloc(bytes_per_arg);
+            if (args[j] == 0)
+            {
+                fprintf(stderr, "Failed to allocate argument space at size %d\n", size);
+                exit(E_NOT_E2BIG);
+            }
+            memset(args[j], j + '0', bytes_per_arg - 1);
+            args[j][bytes_per_arg - 1] = '\0';
+        }
+
+        /* Close standard I/O channels so executed command doesn't spew forth */
+        int dev_null = open("/dev/null", O_RDWR);
+        if (dev_null < 0)
+        {
+            fprintf(stderr, "Failed to open /dev/null for reading and writing\n");
+            exit(E_NOT_E2BIG);
+        }
+        int dev_stderr = dup(2);
+        if (dev_stderr < 0)
+        {
+            fprintf(stderr, "Failed to dup() standard error\n");
+            exit(E_NOT_E2BIG);
+        }
+        close(0);
+        dup(dev_null);
+        close(1);
+        dup(dev_null);
+        close(2);
+        dup(dev_null);
+        close(dev_null);
+
+        /* Execute ls on big file names -- error is ENAMETOOLONG */
+        execvp(args[0], args);
+
+        /* Reinstate standard error so we can report failure */
+        dup2(dev_stderr, 2);
+        int errnum = errno;
+        if (errnum == E2BIG)
+        {
+            fprintf(stderr, "%d: got E2BIG (%d: %s) at size %d\n", self, errnum, strerror(errnum), size);
+            exit(E_GOT_E2BIG);
+        }
+        fprintf(stderr, "%d: got errno %d (%s) at size %d\n", self, errnum, strerror(errnum), size);
+        exit(E_NOT_E2BIG);
+    }
+    else
+    {
+        int self = getpid();
+        int corpse;
+        int status;
+        while ((corpse = waitpid(pid, &status, 0)) != -1)
+        {
+            if (!WIFEXITED(status))
+                printf("%d: child %d died with exit status 0x%.4X", self, corpse, status);
+            else
+            {
+                int statval = WEXITSTATUS(status);
+                printf("%d: child %d died with exit status %d: ", self, corpse, statval);
+                switch (statval)
+                {
+                case E_GOT_E2BIG:
+                    printf("success: got E2BIG");
+                    result = R_TOO_LARGE;
+                    break;
+                case E_NOT_E2BIG:
+                    printf("failed: indeterminate error in child");
+                    break;
+                case 1:
+                    printf("command exited with status 1 - it worked");
+                    break;
+                default:
+                    printf("unknown: unexpected exit status %d", statval);
+                    break;
+                }
+            }
+            printf(" at size %d (%d KiB)\n", size, size / BYTES_PER_KIBIBYTE);
+            fflush(stdout);
+        }
+    }
+    return result;
+}
+
+static void print_kib(int size)
+{
+    printf("%d (%d KiB)", size, size / BYTES_PER_KIBIBYTE);
+}
+
+static int env_size(void)
+{
+    int size = 0;
+    for (char **ep = environ; *ep != 0; ep++)
+        size += strlen(*ep) + 1;
+    return size;
+}
+
 int main(void)
 {
     signal(SIGCHLD, sigchld_handler);
-    for (int i = 4 * BYTES_PER_KIBIBYTE; i < BYTES_PER_MEBIBYTE; i *= 2)
+
+    int env = env_size();
+    int lo = 0;
+    int hi = BYTES_PER_MEBIBYTE;
+
+    /* Binary search -- the kilobyte slop means termination does not have to be accurate */
+    while (lo + 1 * BYTES_PER_KIBIBYTE < hi)
     {
-        fflush(0);
-        pid_t pid = fork();
-        if (pid < 0)
-        {
-            fprintf(stderr, "Failed to fork at %d\n", i);
-            return 1;
-        }
-        else if (pid == 0)
-        {
-            int self = getpid();
-            printf("Child: %d\n", self);
-            char *args[10] = { "ls" };
-            size_t bytes_per_arg = i / 8;
-            for (int j = 1; j < 9; j++)
-            {
-                args[j] = malloc(bytes_per_arg + 1);
-                if (args[j] == 0)
-                {
-                    fprintf(stderr, "Failed to allocate argument space at size %d\n", i);
-                    exit(E_NOT_E2BIG);
-                }
-                memset(args[j], i + '0', bytes_per_arg);
-                args[j][bytes_per_arg] = '\0';
-            }
-
-            /* Close standard I/O channels so executed command doesn't spew forth */
-            int dev_null = open("/dev/null", O_RDWR);
-            if (dev_null < 0)
-            {
-                fprintf(stderr, "Failed to open /dev/null for reading and writing\n");
-                exit(E_NOT_E2BIG);
-            }
-            int dev_stderr = dup(2);
-            if (dev_stderr < 0)
-            {
-                fprintf(stderr, "Failed to dup() standard error\n");
-                exit(E_NOT_E2BIG);
-            }
-            close(0);
-            dup(dev_null);
-            close(1);
-            dup(dev_null);
-            close(2);
-            dup(dev_null);
-            close(dev_null);
-
-            /* Execute ls on big file names -- error is ENAMETOOLONG */
-            execvp(args[0], args);
-
-            /* Reinstate standard error so we can report failure */
-            dup2(dev_stderr, 2);
-            int errnum = errno;
-            if (errnum == E2BIG)
-            {
-                fprintf(stderr, "%d: got E2BIG (%d: %s) at size %d\n", self, errnum, strerror(errnum), i);
-                exit(E_GOT_E2BIG);
-            }
-            fprintf(stderr, "%d: got errno %d (%s) at size %d\n", self, errnum, strerror(errnum), i);
-            exit(E_NOT_E2BIG);
-        }
+        int mid = (lo + hi) / 2;
+        if (test_arg_size(mid) == R_TOO_LARGE)
+            hi = mid;
         else
-        {
-            int self = getpid();
-            int corpse;
-            int status;
-            int exit_loop = 0;
-            while ((corpse = waitpid(pid, &status, 0)) != -1)
-            {
-                if (!WIFEXITED(status))
-                    printf("%d: child %d died with exit status 0x%.4X", self, corpse, status);
-                else
-                {
-                    int statval = WEXITSTATUS(status);
-                    printf("%d: child %d died with exit status %d: ", self, corpse, statval);
-                    switch (statval)
-                    {
-                    case E_GOT_E2BIG:
-                        printf("success: got E2BIG");
-                        exit_loop = 1;
-                        break;
-                    case E_NOT_E2BIG:
-                        printf("failed: indeterminate error in child");
-                        break;
-                    case 1:
-                        printf("command exited with status 1 - it worked");
-                        break;
-                    default:
-                        printf("unknown: unexpected exit status %d", statval);
-                        break;
-                    }
-                }
-                printf(" at size %d (%d KiB)\n", i, i / BYTES_PER_KIBIBYTE);
-                fflush(stdout);
-            }
-            if (exit_loop)
-                break;
-        }
+            lo = mid;
     }
+
+    printf("Environment size = %d\n", env);
+    printf("Best guess: maximum argument size in range ");
+    print_kib(lo + env);
+    fputs(" to ", stdout);
+    print_kib(hi + env);
+    putchar('\n');
+
     return 0;
 }
