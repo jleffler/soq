ee4d539c95d23022d0d6d0877597cdbcfb7ced07
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-09-02 23:03:26 -0700

    Clean up most of the files not related to pipes. Use systematic names for files related to pipes.

--- const-stuff.c
+++
@@ -1,5 +0,0 @@
-char const         *ccp   = 0;     /* OK */
-char const * const *ccpcp = &ccp;  /* OK */
-char       * const *cpcp  = &ccp;  /* X: OK  - actually BAD */
-char const *       *ccpp  = &ccp;  /* X: BAD - actually OK  */
-
--- quine.c
+++
@@ -1,3 +0,0 @@
-#include <stdio.h>
-const char*s="#include <stdio.h>%cconst char*s=%c%s%c;%cint main(void){printf(s,10,34,s,34,10,10);}%c";
-int main(void){printf(s,10,34,s,34,10,10);}
