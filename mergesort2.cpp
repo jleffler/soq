91333199bbea33cd9c65e40c024c292dbd8e8565
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-09-21 16:46:18 -0700

    mergesort2.cpp: converted to a sequence of templated functions, and add demonstrations with double and std::string (and minimal pain)

--- mergesort2.cpp
+++ mergesort2.cpp
@@ -6,15 +6,17 @@ using namespace std;
 
 namespace {
 
-void merge(int *l, int m, int *r, int n, int *result);
+template <typename T>
+void merge(T *l, int m, T *r, int n, T *result);
 
-void dump_array(const char *tag, int *a, int size)
+template <typename T>
+void dump_array(const char *tag, T *a, int size)
 {
     int i;
     cout << tag << ": (" << size << ") ";
     for (i = 0; i < size; i++)
     {
-        cout << ' ' << a[i];
+        cout << "  " << a[i];
         if (i % 10 == 9)
             cout << '\n';
     }
@@ -24,15 +26,16 @@ void dump_array(const char *tag, int *a, int size)
 
 };
 
-void merge_sort(int *a, int size)
+template <typename T>
+void merge_sort(T *a, int size)
 {
     if (size <= 1)
         return;
 
     dump_array("-->> merge_sort", a, size);
     int middle = size/2;
-    int *left = new int[middle];
-    int *right = new int[size - middle];
+    T *left = new T[middle];
+    T *right = new T[size - middle];
 
     for (int i = 0; i < middle; i++)
         left[i] = a[i];
@@ -50,11 +53,12 @@ void merge_sort(int *a, int size)
 
 namespace {
 
-void merge(int *l, int m, int *r, int n, int *result)
+template <typename T>
+void merge(T *l, int m, T *r, int n, T *result)
 {
-    int *l_end = l + m;
-    int *r_end = r + n;
-    int *out = result;
+    T *l_end = l + m;
+    T *r_end = r + n;
+    T *out = result;
     cout << "-->> merge: (" << m << "," << n << ")\n";
     dump_array("L", l, m);
     dump_array("R", r, n);
@@ -76,8 +80,11 @@ void merge(int *l, int m, int *r, int n, int *result)
 
 };
 
+#include <string>
+
 int main()
 {
+
     for (int i = 1; i <= 10; i++)
     {
         int array1[] = { 9, 3, 5, 7, 1, 8, 0, 6, 2, 4 };
@@ -86,5 +93,24 @@ int main()
         merge_sort(array1, i);
         dump_array("After", array1, i);
     }
+
+    for (int i = 1; i <= 10; i++)
+    {
+        double array2[] = { 9.9, 3.1, 5.2, 7.3, 1.4, 8.5, 0.6, 6.7, 2.8, 4.9 };
+        cout << "\nMerge array of size " << i << "\n\n";
+        dump_array("Before", array2, i);
+        merge_sort(array2, i);
+        dump_array("After", array2, i);
+    }
+
+    for (int i = 1; i <= 10; i++)
+    {
+        std::string array3[] = { "nine", "three", "five", "seven", "one", "eight", "zero", "six", "two", "four" };
+        cout << "\nMerge array of size " << i << "\n\n";
+        dump_array("Before", array3, i);
+        merge_sort(array3, i);
+        dump_array("After", array3, i);
+    }
+
     return 0;
 }
