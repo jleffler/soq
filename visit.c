8766ad3bbab2181651ccd331771788285ee40896
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-10-09 21:05:07 -0700

    visit.c: revised to make the matrix a parameter to the search function (visit())

--- visit.c
+++ visit.c
@@ -1,21 +1,5 @@
 #include <stdio.h>
 
-static int const edges[12][12] =
-{
-    {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    {1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
-    {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
-    {0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0},
-    {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
-    {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
-    {0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
-    {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
-    {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1},
-    {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
-};
-
 static int visited[12] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
 
 struct Node
@@ -24,37 +8,37 @@ struct Node
     struct Node *prev;
 };
 
-static void visit(int node, struct Node *prev_node)
+static void visit(int node, struct Node *prev_node, int size, int const edges[size][size], int end)
 {
     struct Node n = { node, prev_node };
     struct Node *p = &n;
 
     printf("-->> %s\n", __func__);
-    if (node == 11)
+    if (node == end)
         printf("Solution: ");
     do
         printf("%d%s", p->node + 1, (p->prev != NULL) ?  "->" : "\n");
     while ((p = p->prev) != NULL);
 
     visited[node] = 1;
-    for (int i = 0; i < 12; ++i)
+    for (int i = 0; i < size; ++i)
     {
         if ((visited[i] == 0) && (edges[node][i] == 1))
         {
-            if (node == 11)
+            if (node == end)
                 printf("Continuing\n");
-            visit(i, &n);
+            visit(i, &n, size, edges, end);
         }
     }
     visited[node] = 0;
     printf("<<-- %s\n", __func__);
 }
 
-static void chk_symmetry(void)
+static void chk_symmetry(int size, int const edges[size][size])
 {
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < 12; j++)
+        for (int j = 0; j < size; j++)
         {
             if (edges[i][j] != edges[j][i])
                 printf("E[%d,%d] = %d, E[%d,%d] = %d\n", i, j, edges[i][j], j, i, edges[j][i]);
@@ -62,11 +46,11 @@ static void chk_symmetry(void)
     }
 }
 
-static void prt_links(void)
+static void prt_links(int size, int const edges[size][size])
 {
-    for (int i = 0; i < 12; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = i; j < 12; j++)
+        for (int j = i; j < size; j++)
         {
             if (edges[i][j])
                 printf("%d -> %d\n", i+1, j+1);
@@ -76,9 +60,25 @@ static void prt_links(void)
 
 int main(void)
 {
-    chk_symmetry();
-    prt_links();
-    visit(0, NULL);
+    int const edges[12][12] =
+    {
+        {0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {1, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0},
+        {0, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0},
+        {0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 0},
+        {0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0},
+        {0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 1},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0}
+    };
+
+    chk_symmetry(12, edges);
+    prt_links(12, edges);
+    visit(0, NULL, 12, edges, 11);
     return 0;
 }
 
