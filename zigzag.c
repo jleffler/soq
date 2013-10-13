1ea0bdec710fd001cfc6915fd6749aaa25f1b605
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-10-13 16:21:47 -0700

    zigzag.c: Answer for SO 18801547 (ZigZag Matrices) - originally zigzag5.c

--- zigzag.c
+++ zigzag.c
@@ -2,53 +2,151 @@
 
 #include <stdio.h>
 
-static inline int max(int a, int b) { return (a > b) ? a : b; }
-static inline int min(int a, int b) { return (a < b) ? a : b; }
+static inline int max(int a, int b) { return ((a) > (b) ? (a) : (b)); }
+static inline int min(int a, int b) { return ((a) < (b) ? (a) : (b)); }
 
-static void print_info(int r, int c)
+void dezigzag(int rows, int cols, int out[rows * cols], int in[rows][cols]);
+
+void dezigzag(int rows, int cols, int out[rows * cols], int in[rows][cols])
 {
-    int n = r + c - 1;
-    printf("R = %d, C = %d, N = %d\n", r, c, n);
-    for (int i = 0; i < n; i++)
+    int n = 0;
+    for (int diag = 0; diag < (rows + cols - 1); diag++)
     {
-        /*
-        int max_x = min(i, c-1);
-        int min_x = max(0, i - n + c);
-        int max_y = min(i, r-1);
-        int min_y = max(0, i - n + r);
-        printf("i = %d, min_x = %d, max_x = %d, min_y = %d, max_y = %d\n",
-                i, min_x, max_x, min_y, max_y);
-        */
-        printf("%d:", i);
-        if (i % 2 == 0)
+        for (int i = max(0, diag - (rows - 1)); i <= min(cols - 1, diag); i++)
         {
-            int max_x = min(i, c-1);
-            int min_x = max(0, i - n + c);
-            for (int j = min_x; j <= max_x; j++)
-                printf(" (%d,%d)", j, i - j);
+            int x = diag % 2 == 0 ? i      : diag-i;
+            int y = diag % 2 == 0 ? diag-i : i;
+            printf("v[%2d] = m[%d][%d] = %2d (D = %2d)\n", n, y, x, in[y][x], diag);
+            out[n++] = diag % 2 == 0 ? in[diag - i][i] : in[i][diag - i];
         }
-        else
-        {
-            int max_y = min(i, r-1);
-            int min_y = max(0, i - n + r);
-            for (int j = min_y; j <= max_y; j++)
-                printf(" (%d,%d)", i - j, j);
-        }
+    }
+}
+
+void fonc(int tab2[3][4], int vect[3 * 4]);
+
+void fonc(int tab2[3][4], int vect[3 * 4])
+{
+    static const struct ZigZag
+    {
+        unsigned char y, x;
+    } zigzag[3*4] =
+    {
+        { 0, 0 }, { 1, 0 }, { 0, 1 }, { 0, 2 },
+        { 1, 1 }, { 2, 0 }, { 3, 0 }, { 2, 1 },
+        { 1, 2 }, { 2, 2 }, { 3, 1 }, { 3, 2 },
+    };
+
+    for (int i = 0; i < 3*4; i++)
+        vect[i] = tab2[zigzag[i].x][zigzag[i].y];
+}
+
+static void dump_vector(int rows, int cols, int vector[rows * cols])
+{
+    puts("Vector:");
+    int limit = rows * cols;
+    for (int i = 0; i < limit; i++)
+    {
+        printf("%3d", vector[i]);
+        if (i % cols == cols - 1)
+            putchar('\n');
+    }
+}
+
+static void dump_matrix(int rows, int cols, int matrix[rows][cols])
+{
+    puts("Matrix:");
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+            printf("%3d", matrix[i][j]);
         putchar('\n');
     }
-
 }
 
 int main(void)
 {
-    struct test
-    {
-        int rows;
-        int cols;
-    } tests[] = { { 4, 4 }, { 6, 4 }, { 4, 7 } };
 
-    for (size_t i = 0; i < sizeof(tests)/sizeof(tests[0]); i++)
-        print_info(tests[i].rows, tests[i].cols);
+    if (1)
+    {
+    enum { ROWS = 3, COLS = 4 };
+    int matrix[ROWS][COLS] =
+    {
+        {  1,  2,  6,  7, },
+        {  3,  5,  8, 11, },
+        {  4,  9, 10, 12, },
+    };
+    int vector[ROWS*COLS];
+
+    dump_matrix(ROWS, COLS, matrix);
+    dezigzag(ROWS, COLS, vector, matrix);
+    dump_vector(ROWS, COLS, vector);
+    }
+
+    if (1)
+    {
+    enum { ROWS = 4, COLS = 4 };
+    int matrix[ROWS][COLS] =
+    {
+        {  1,  2,  6,  7, },
+        {  3,  5,  8, 13, },
+        {  4,  9, 12, 14, },
+        { 10, 11, 15, 16, },
+    };
+    int vector[ROWS*COLS];
+
+    dump_matrix(ROWS, COLS, matrix);
+    dezigzag(ROWS, COLS, vector, matrix);
+    dump_vector(ROWS, COLS, vector);
+    }
+
+    if (0)
+    {
+    enum { ROWS = 3, COLS = 4 };
+    int matrix[ROWS][COLS] =
+    {
+        {  1,  2,  6,  7, },
+        {  3,  5,  8, 11, },
+        {  4,  9, 10, 12, },
+    };
+    int vector[ROWS*COLS];
+
+    dump_matrix(ROWS, COLS, matrix);
+    fonc(matrix, vector);
+    dump_vector(ROWS, COLS, vector);
+    }
+
+    if (0)
+    {
+    int out[64] = {-1};
+    int in[8][8];
+
+    for (int i = 0; i < 64; i++)
+        in[i % 8][i / 8] = i;
+
+    dump_matrix(8, 8, in);
+    dezigzag(8, 8, out, in);
+    dump_vector(8, 8, out);
+    }
+
+    if (0)
+    {
+    int out[64] = {-1};
+    int tab2[8][8] =
+    {
+        {  1,  2,  6,  7, 15, 16, 28, 29 },
+        {  3,  5,  8, 14, 17, 27, 30, 43 },
+        {  4,  9, 13, 18, 26, 31, 42, 44 },
+        { 10, 12, 19, 25, 32, 41, 45, 54 },
+        { 11, 20, 24, 33, 40, 46, 53, 55 },
+        { 21, 23, 34, 39, 47, 52, 56, 61 },
+        { 22, 35, 38, 48, 51, 57, 60, 62 },
+        { 36, 37, 49, 50, 58, 59, 63, 64 },
+    };
+
+    dump_matrix(8, 8, tab2);
+    dezigzag(8, 8, out, tab2);
+    dump_vector(8, 8, out);
+    }
 
     return 0;
 }
