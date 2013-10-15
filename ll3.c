b6d3183c2e15abf51d4c8c529f97dcce5c63dc8d
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-10-14 22:43:54 -0700

    ll3.c: introduce test_insert() - clean up printing

--- ll3.c
+++ ll3.c
@@ -11,90 +11,89 @@ struct node
 
 void insert_node(struct node **head, int n);
 void print_list(struct node *head);
+void free_list(struct node **phead);
+void test_insert(struct node **head, int n);
 
 int main(void)
 {
     struct node *head = NULL;
+    free_list(&head);
 
     for (int i = 1; i < 5; i++)
-        insert_node(&head, i*i);
-    insert_node(&head, 0);
-    insert_node(&head, 7);
-    for (int i = 1; i < 5; i++)
-        insert_node(&head, i*i - 3);
-    insert_node(&head, 7);
-    insert_node(&head, 0);
+        test_insert(&head, i*i);
+    test_insert(&head, 0);
+    test_insert(&head, 7);
+    for (int i = 1; i < 6; i++)
+        test_insert(&head, i*i - 3);
+    test_insert(&head, 7);
+    test_insert(&head, 0);
 
     print_list(head);
+    free_list(&head);
     return 0;
+}
+
+void test_insert(struct node **head, int n)
+{
+    printf("%2d:", n);
+    insert_node(head, n);
+    print_list(*head);
 }
 
 void print_list(struct node *head)
 {
     while (head != NULL)
     {
-        printf("%i ", head->data);
+        printf(" %2i", head->data);
         head = head->link;
     }
     putchar('\n');
 }
 
+void free_list(struct node **phead)
+{
+    assert(phead != NULL);
+    struct node *head = *phead;
+    while (head != NULL)
+    {
+        struct node *next = head->link;
+        free(head);
+        head = next;
+    }
+    *phead = 0;
+}
+
 void insert_node(struct node **phead, int n)
 {
-    struct node N = {n, NULL};
     struct node *node = malloc(sizeof(*node));
-    struct node *head = *phead;
 
     if (node == NULL)
     {
         fprintf(stderr, "Failed to allocate node for %d\n", n);
         exit(1);
     }
-    *node = N;
 
-    if (head == NULL)
-    {
-        printf("Case 1: create %d\n", node->data);
-        *phead = node;
-    }
-    else if (n < head->data)
-    {
-        printf("Case 2: insert %d\n", node->data);
-        node->link = head;
-        *phead = node;
-    }
-    else
-    {
-        struct node *next = head;
-        struct node *prev = NULL;
+    node->data = n;
 
-        while (next != NULL)
+    struct node *head = *phead;
+    struct node *next = head;
+    struct node *prev = NULL;
+
+    while (next != NULL && n >= next->data)
+    {
+        if (n == next->data)
         {
-            if (n < next->data)
-            {
-                printf("Case 5: insert %d before %d\n", n, next->data);
-                assert(prev != NULL);
-                prev->link = node;
-                node->link = next;
-                return;
-            }
-            if (n == next->data)
-            {
-                printf("Case 3: double %d == %d\n", n, next->data);
-                free(node);
-                return;
-            }
-            prev = next;        // save the current element
-            next = next->link;  // look at the next element
-        }
-
-        assert(prev != NULL && n > prev->data);
-        if (n > prev->data)
-        {
-            printf("Case 4: append %d\n", node->data);
-            prev->link = node;
+            free(node);
             return;
         }
+        prev = next;
+        next = next->link;
     }
+
+    node->link = next;
+    if (prev == NULL)
+        *phead = node;
+    else
+        prev->link = node;
 }
 
