df456fba687297cf26cfab4dee828fc15ce9d512
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-11-10 21:03:06 -0800

    Add 3-star.c and 4-star.c - solutions for SO 19782086

---
+++ 4-star.c
@@ -0,0 +1,67 @@
+/*
+** http://stackoverflow.com/questions/19782086
+** Allocating memory dynamically to a 2D array of strings
+**
+** The 4-star solution.
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+static void ReadNames(char ****Names, int *rows, int *cols)
+{
+    char name[100];
+    printf("Number of Rows: ");
+    scanf("%d", rows);
+
+    printf("Number of Columns: ");
+    scanf("%d", cols);
+
+    int c;
+    while ((c = getchar()) != EOF && c != '\n')
+        ;
+
+    *Names = (char ***)malloc(sizeof(char ***)*(*rows));
+    for (int i = 0; i < (*rows); i++)
+        (*Names)[i] = (char **)malloc(sizeof(char **)*(*cols));
+
+    for (int i = 0; i < (*rows); i++)
+    {
+        for (int j = 0; j < (*cols); j++)
+        {
+            printf("R%dC%d: ", i, j);
+            if (fgets(name, sizeof(name), stdin) == 0)
+            {
+                fprintf(stderr, "Unexpected EOF\n");
+                exit(1);
+            }
+            name[strlen(name)-1] = '\0';    // Zap newline
+            (*Names)[i][j] = strdup(name);
+        }
+    }
+}
+
+int main(void)
+{
+    int rows;
+    int cols;
+    char ***data = 0;
+
+    ReadNames(&data, &rows, &cols);
+    printf("Rows = %d, cols = %d.\n", rows, cols);
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+            printf("[%d,%d] = <<%s>>\n", i, j, data[i][j]);
+    }
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; j++)
+            free(data[i][j]);
+        free(data[i]);
+    }
+    free(data);
+    return 0;
+}
+
