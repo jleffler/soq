5afdfd36ccc064fd802e64ef88c80d8da1a1befd
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-11-23 09:54:09 -0800

    colourpartition.c: rename color -> Colour

--- colourpartition.c
+++ colourpartition.c
@@ -1,31 +1,31 @@
 #include <stdio.h>
 
-typedef enum { WHITE, BLACK, RED } color;
+typedef enum { WHITE, BLACK, RED } Colour;
 
-static char color_code(color c)
+static char colour_code(Colour c)
 {
     return(c == WHITE ? 'W' : c == RED ? 'R' : 'B');
 }
 
-static void dump_colors(char const *tag, color *data, unsigned num)
+static void dump_colours(char const *tag, Colour *data, unsigned num)
 {
     printf("%s:", tag);
     for (unsigned i = 0; i < num; i++)
-        printf(" %c", color_code(data[i]));
+        printf(" %c", colour_code(data[i]));
     putchar('\n');
 }
 
 static
-void swap(color *p1, color *p2)
+void swap(Colour *p1, Colour *p2)
 {
-    color tmp;
+    Colour tmp;
     tmp = *p1;
     *p1 = *p2;
     *p2 = tmp;
 }
 
 static
-void partition3(color *arr, unsigned n)
+void partition3(Colour *arr, unsigned n)
 {
     if (n <= 1)
         return;
@@ -41,8 +41,8 @@ void partition3(color *arr, unsigned n)
 
     for (unsigned i = w; i < r; i++)
     {
-        //printf("i = %2u, r = %2u, w = %2u, c = %c", i, r, w, color_code(arr[i]));
-        //dump_colors("", arr, n);
+        //printf("i = %2u, r = %2u, w = %2u, c = %c", i, r, w, colour_code(arr[i]));
+        //dump_colours("", arr, n);
         if (arr[i] == RED)
             swap(&arr[i], &arr[--r]);
         if (arr[i] == WHITE)
@@ -54,19 +54,19 @@ int main(void)
 {
     struct test
     {
-        color *data;
+        Colour *data;
         size_t size;
     };
-    color array1[] = { WHITE, BLACK, RED };
-    color array2[] = { WHITE, WHITE, WHITE };
-    color array3[] = { RED, RED, RED };
-    color array4[] = { BLACK, BLACK, BLACK };
-    color array5[] = { RED, BLACK, WHITE };
-    color array6[] = { WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE };
-    color array7[] = { BLACK, BLACK, WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE, BLACK, BLACK, };
-    color array8[] = { WHITE, BLACK };
-    color array9[] = { BLACK, BLACK, RED, RED, WHITE, WHITE, RED };
-    color array0[] = { BLACK, BLACK, RED, WHITE, RED };
+    Colour array1[] = { WHITE, BLACK, RED };
+    Colour array2[] = { WHITE, WHITE, WHITE };
+    Colour array3[] = { RED, RED, RED };
+    Colour array4[] = { BLACK, BLACK, BLACK };
+    Colour array5[] = { RED, BLACK, WHITE };
+    Colour array6[] = { WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE };
+    Colour array7[] = { BLACK, BLACK, WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE, BLACK, BLACK, };
+    Colour array8[] = { WHITE, BLACK };
+    Colour array9[] = { BLACK, BLACK, RED, RED, WHITE, WHITE, RED };
+    Colour array0[] = { BLACK, BLACK, RED, WHITE, RED };
     struct test tests[] =
     {
         { array1, sizeof(array1)/sizeof(array1[0]) },
@@ -83,9 +83,9 @@ int main(void)
 
     for (size_t i = 0; i < sizeof(tests)/sizeof(tests[0]); i++)
     {
-        dump_colors("Before", tests[i].data, tests[i].size);
+        dump_colours("Before", tests[i].data, tests[i].size);
         partition3(tests[i].data, tests[i].size);
-        dump_colors("After ", tests[i].data, tests[i].size);
+        dump_colours("After ", tests[i].data, tests[i].size);
     }
     return 0;
 }
