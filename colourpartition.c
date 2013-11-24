c7290cc06a9bdc7e9078ccb3747bb73084d4e37f
Jonathan Leffler <jonathan.leffler@gmail.com>
2013-11-24 08:31:24 -0800

    colourpartition.c: the invariant is more complex than originally thought.
    The sequence of known-to-be-white at the start has to be ended by a
    known-to-be-non-white value.  Likewise, the sequence of known-to-be-red
    at the end has to be ended by a known-to-be-non-red value.  The initial
    loops that set the invariants have to be used again after each swap.
    This is a working version; it may be possible to put the invariant loops
    as first steps in the main loop - next experiment.

--- colourpartition.c
+++ colourpartition.c
@@ -37,16 +37,29 @@ void partition3(Colour *arr, unsigned n)
         w++;
     while (r > w && arr[r-1] == RED)
         r--;
-    //printf("w = %d; r = %d\n", w, r);
+    printf("w = %d; r = %d\n", w, r);
 
     for (unsigned i = w; i < r; i++)
     {
-        //printf("i = %2u, r = %2u, w = %2u, c = %c", i, r, w, colour_code(arr[i]));
-        //dump_colours("", arr, n);
+        putchar('\n');
+        printf("-| i = %2u, r = %2u, w = %2u, c = %c", i, r, w, colour_code(arr[i]));
+        dump_colours("", arr, n);
         if (arr[i] == RED)
+        {
             swap(&arr[i], &arr[--r]);
+            while (r > w && arr[r-1] == RED)
+                r--;
+            printf("-- i = %2u, r = %2u, w = %2u, c = %c", i, r, w, colour_code(arr[i]));
+            dump_colours("", arr, n);
+        }
         if (arr[i] == WHITE)
+        {
             swap(&arr[i], &arr[w++]);
+            while (r > w && arr[r-1] == RED)
+                r--;
+            printf("|- i = %2u, r = %2u, w = %2u, c = %c", i, r, w, colour_code(arr[i]));
+            dump_colours("", arr, n);
+        }
     }
 }
 
@@ -57,28 +70,30 @@ int main(void)
         Colour *data;
         size_t size;
     };
-    Colour array1[] = { WHITE, BLACK, RED };
-    Colour array2[] = { WHITE, WHITE, WHITE };
-    Colour array3[] = { RED, RED, RED };
-    Colour array4[] = { BLACK, BLACK, BLACK };
-    Colour array5[] = { RED, BLACK, WHITE };
-    Colour array6[] = { WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE };
-    Colour array7[] = { BLACK, BLACK, WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE, BLACK, BLACK, };
-    Colour array8[] = { WHITE, BLACK };
-    Colour array9[] = { BLACK, BLACK, RED, RED, WHITE, WHITE, RED };
-    Colour array0[] = { BLACK, BLACK, RED, WHITE, RED };
+    //Colour array1[] = { WHITE, BLACK, RED };
+    //Colour array2[] = { WHITE, WHITE, WHITE };
+    //Colour array3[] = { RED, RED, RED };
+    //Colour array4[] = { BLACK, BLACK, BLACK };
+    //Colour array5[] = { RED, BLACK, WHITE };
+    //Colour array6[] = { WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE };
+    //Colour array7[] = { BLACK, BLACK, WHITE, WHITE, RED, RED, BLACK, BLACK, WHITE, BLACK, BLACK, };
+    //Colour array8[] = { WHITE, BLACK };
+    //Colour array9[] = { BLACK, BLACK, RED, RED, WHITE, WHITE, RED };
+    //Colour array0[] = { BLACK, BLACK, RED, WHITE, RED };
+    Colour arrayA[] = { RED, BLACK, RED, WHITE, RED, RED, BLACK, WHITE, RED, BLACK, RED, BLACK, BLACK, RED, BLACK, WHITE, BLACK, WHITE, WHITE, WHITE, WHITE, RED, RED, RED, RED, BLACK, WHITE };
     struct test tests[] =
     {
-        { array1, sizeof(array1)/sizeof(array1[0]) },
-        { array2, sizeof(array2)/sizeof(array2[0]) },
-        { array3, sizeof(array3)/sizeof(array3[0]) },
-        { array4, sizeof(array4)/sizeof(array4[0]) },
-        { array5, sizeof(array5)/sizeof(array5[0]) },
-        { array6, sizeof(array6)/sizeof(array6[0]) },
-        { array7, sizeof(array7)/sizeof(array7[0]) },
-        { array8, sizeof(array8)/sizeof(array8[0]) },
-        { array9, sizeof(array9)/sizeof(array9[0]) },
-        { array0, sizeof(array0)/sizeof(array0[0]) },
+        //{ array1, sizeof(array1)/sizeof(array1[0]) },
+        //{ array2, sizeof(array2)/sizeof(array2[0]) },
+        //{ array3, sizeof(array3)/sizeof(array3[0]) },
+        //{ array4, sizeof(array4)/sizeof(array4[0]) },
+        //{ array5, sizeof(array5)/sizeof(array5[0]) },
+        //{ array6, sizeof(array6)/sizeof(array6[0]) },
+        //{ array7, sizeof(array7)/sizeof(array7[0]) },
+        //{ array8, sizeof(array8)/sizeof(array8[0]) },
+        //{ array9, sizeof(array9)/sizeof(array9[0]) },
+        //{ array0, sizeof(array0)/sizeof(array0[0]) },
+        { arrayA, sizeof(arrayA)/sizeof(arrayA[0]) },
     };
 
     for (size_t i = 0; i < sizeof(tests)/sizeof(tests[0]); i++)
