5d5a7d808199cead08fa34282b9f2baae32d8e2a
Jonathan Leffler <jonathan.leffler@gmail.com>
2014-01-05 17:15:02 -0800

    Decrustified - minor tweaks

--- qsel.cpp
+++ qsel.cpp
@@ -17,42 +17,56 @@ template<class T>
 long partition(T *a, T &p, long N)
 {
     long i = 0, j = N - 1;
-    T temp;
 
     printArray("-->>partition", a, N);
     p = a[(i + j) / 2];
     cout << p << endl;
 
-    do {
-        while (a[i] < p) { i++; }
-        while (a[j] > p) { j--; }
-        if (i <= j) {
-            temp = a[i]; a[i] = a[j]; a[j] = temp;
-            i++; j--;
+    do
+    {
+        while (a[i] < p)
+            i++;
+        while (a[j] > p)
+            j--;
+        if (i <= j)
+        {
+            T temp = a[i];
+            a[i++] = a[j];
+            a[j--] = temp;
         }
     } while (i <= j);
     printArray("<<--partition", a, N);
     return ++j;
 }
 
-
 template<class T>
 T quickSelect(T *input, T &p, long N, long k)
 {
     long j = partition(input, p, N);
     long pivot = j + 1;
-    if (2 < N) {
-        if (j == k - 1) {
+    if (2 < N)
+    {
+        if (j == k - 1)
+        {
             return p;
-        } else if (k < pivot) {
+        }
+        else if (k < pivot)
+        {
             return quickSelect(input, p, j, k);
-        } else {
+        }
+        else
+        {
             return quickSelect(input + j, p, N - j, k - j);
         }
-    } else {
-        if (j == k - 1) {
+    }
+    else
+    {
+        if (j == k - 1)
+        {
             return p;
-        } else {
+        }
+        else
+        {
             return input[1];
         }
     }
@@ -60,7 +74,11 @@ T quickSelect(T *input, T &p, long N, long k)
 
 int main()
 {
-    int A[] = {100, 4, 10, -35, 55, 6, 6, 6, -67, 0, 2, 34, 6, 4, 56, 2, 7, -9, 45, -27, 5, 7, 8, 94, -478, -98, 13456};
+    int A[] =
+    {
+        100, 4, 10, -35, 55, 6, 6, 6, -67, 0, 2, 34, 6, 4,
+        56, 2, 7, -9, 45, -27, 5, 7, 8, 94, -478, -98, 13456
+    };
     const size_t A_SIZE = sizeof(A) / sizeof(A[0]);
     for (long i = 2; i < 20; i += 3)
     {
@@ -73,3 +91,4 @@ int main()
     }
     return 0;
 }
+
