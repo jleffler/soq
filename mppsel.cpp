182c34078bd884eaa5db19efcf22687e78f515db
Jonathan Leffler <jonathan.leffler@gmail.com>
2014-01-10 22:20:28 -0800

    mppsel.cpp: Initial phase of conversion to work with N, number of elements in array, instead of u, maximum valid index.

--- mppsel.cpp
+++ mppsel.cpp
@@ -3,15 +3,15 @@
 #include <iostream>
 #include <iomanip>  // setw
 #include <utility>  // swap
-#include <cstdlib>  // rand, srand
 
 using namespace std;
 
 static const int debug = 0;
 
 template<class T>
-static void check_partition(T *a, size_t end, size_t rank)
+static void check_partition(T *a, size_t N, size_t rank)
 {
+    size_t end = N - 1;
     int ok = 1;
     if (debug) cout << "e = " << end << "; r = " << rank << endl;
     assert(0 < end);
@@ -41,12 +41,11 @@ static void check_partition(T *a, size_t end, size_t rank)
 }
 
 template<class T>
-void printArray(char const *tag, T *A, size_t u)
+void printArray(char const *tag, T *A, size_t N)
 {
     size_t i;
-    size_t N = u + 1;
     int const maxw = 15;
-    cout << tag << ": (" << u << ")";
+    cout << tag << ": (" << N << ")";
     if (N > maxw)
         cout << '\n';
     for (i = 0; i < N; i++)
@@ -60,15 +59,17 @@ void printArray(char const *tag, T *A, size_t u)
 }
 
 template<class T>
-size_t partition(T *a, size_t u)        // u = max valid index
+size_t partition(T *a, size_t N)
 {
+    assert(N > 1);
+    size_t u = N - 1;
     assert(u > 0);
-    if (debug) printArray("--->> partition()", a, u);
+    if (debug) printArray("--->> partition()", a, N);
     size_t m = 0;
     swap(a[(u) / 2], a[0]);
     T p = a[0];
     if (debug) cout << "Pivot: [" << (u+1) / 2 << "] = " << p << endl;
-    if (debug) printArray("--0-- partition()", a, u);
+    if (debug) printArray("--0-- partition()", a, N);
 
     for (size_t i = 1; i <= u; i++)
     {
@@ -77,27 +78,29 @@ size_t partition(T *a, size_t u)        // u = max valid index
             ++m;
             if (debug) cout << "swap (" << i << "," << m << ")<=>(" << a[i] << "," << a[m] << ")\n";
             swap(a[i], a[m]);
-            if (debug) printArray("--1-- partition()", a, u);
+            if (debug) printArray("--1-- partition()", a, N);
         }
     }
 
-    if (debug) printArray("--2-- partition()", a, u);
+    if (debug) printArray("--2-- partition()", a, N);
     swap(a[0], a[m]);
-    if (debug) printArray("<<--- partition()", a, u);
-    check_partition(a, u, m);
+    if (debug) printArray("<<--- partition()", a, N);
+    check_partition(a, N, m);
     if (debug) cout << "Returning: " << m << endl;
     return m;
 }
 
 // u is maximum valid index in a, rather than number of elements in a
 template<class T>
-void Select(T *a, size_t u, size_t k)
+void Select(T *a, size_t N, size_t k)
 {
+    assert(N > 0);
+    size_t u = N - 1;
     assert(k <= u);
     while (0 < u)
     {
         assert(k <= u);
-        size_t m = partition(a, u);
+        size_t m = partition(a, u+1);
         assert(m <= u);
         if (k <= m)
         {
@@ -125,15 +128,15 @@ int main()
     };
     const size_t A_SIZE = sizeof(A) / sizeof(A[0]);
     printArray("Initial", A, A_SIZE-1);
-    for (size_t i = 0; i < A_SIZE-1; i++)
+    for (size_t i = 0; i < A_SIZE; i++)
     {
         int B[A_SIZE];
         memmove(B, A, sizeof(B));
         cout << "Rank [" << i << "]" << endl;
-        Select(B, A_SIZE-1, i);
+        Select(B, A_SIZE, i);
         cout << "Rank [" << i << "] = " << B[i] << endl;
-        printArray("Finish", B, A_SIZE-1);
-        check_partition(B, A_SIZE-1, i);
+        printArray("Finish", B, A_SIZE);
+        check_partition(B, A_SIZE, i);
         cout << '\n';
     }
     return 0;
