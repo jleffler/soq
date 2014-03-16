7bd45e1d9b92d6809c8e99a6bd25d18072df694a
Jonathan Leffler <jonathan.leffler@gmail.com>
2014-03-16 11:17:33 -0700

    Old version of wrapper.c had a line trying to treat __FUNCTION__ as a string literal, but it is not a string literal, so it did not compile. New version gets rid of global variable rc and is generally tidier.

--- wrapper.c
+++ wrapper.c
@@ -1,20 +1,20 @@
 /* SO 22395738 */
 #include <stdio.h>
 
-int rc;
 int foo(int a, int b);
 int bar(int a, char *b, int *c);
 
-extern void LogRet(const char *fn, const char *file, const char *from, int ln, int ret);
+extern int LogRet(const char *fn, const char *file, const char *from, int ln, int ret);
 
-void LogRet(const char *fn, const char *file, const char *from, int ln, int ret)
+int LogRet(const char *fn, const char *file, const char *from, int ln, int ret)
 {
     printf("%s.%s.%d: %s()  ret:%08x\n", file, from, ln, fn, ret);
+    return ret;
 }
 
-#define foo(args, ...)  (rc = (foo)(args, ##__VA_ARGS__), LogRet("foo", __FILE__, __FUNCTION__, __LINE__, rc), rc)
+#define foo(args, ...)  LogRet("foo", __FILE__, __FUNCTION__, __LINE__, (foo)(args, ##__VA_ARGS__))
 
-#define bar(args, ...)  (rc = (bar)(args, ##__VA_ARGS__), LogRet("bar", __FILE__, __FUNCTION__, __LINE__, rc), rc)
+#define bar(args, ...)  LogRet("bar", __FILE__, __FUNCTION__, __LINE__, (bar)(args, ##__VA_ARGS__))
 
 extern void caller1(void);
 
@@ -23,14 +23,13 @@ void caller1(void)
     int d;
     int e = foo(1, 2);
     int f = bar(3, "abc", &d);
-    printf("--" __FILE__ ":" __FUNCTION__ "():\n");
     printf("%s(): %d + %d + %d = %d\n", __func__, d, e, f, d + e + f);
 }
 
 #undef foo
 #undef bar
 
-#define WRAPPER(func, ...) ((rc = (func)(__VA_ARGS__)), LogRet(#func, __FILE__, __func__, __LINE__, rc), rc)
+#define WRAPPER(func, ...) LogRet(#func, __FILE__, __func__, __LINE__, (func)(__VA_ARGS__))
 
 #define foo(...) WRAPPER(foo, __VA_ARGS__)
 #define bar(...) WRAPPER(bar, __VA_ARGS__)
