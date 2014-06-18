810d5ed9416c2b40ff0d94b62595134b0b1f148c
Jonathan Leffler <jonathan.leffler@gmail.com>
2014-06-18 07:45:54 -0700

    Give am-pm.pl a __DATA__ section with semi-stringent tests

--- am-pm.pl
+++ am-pm.pl
@@ -20,12 +20,27 @@ sub time_24h_to_12h
     return sprintf("%d:%.2d %s", $hh, $mm, $ampm);
 }
 
-while (<>)
+while (<DATA>)
 {
     chomp;
     my($input, $entry) = split / - /;
     my $time_24h = time_12h_to_24h($input);
     my $time_12h = time_24h_to_12h($time_24h);
-    print "$input - $time_24h : $time_12h - $entry\n";
-    #printf "%8s - %5s : %8s - %s\n", $input, $time_24h, $time_12h, $entry;
+    #print "$input - $time_24h : $time_12h - $entry\n";
+    printf "%8s - %5s : %8s - %s\n", $input, $time_24h, $time_12h, $entry;
 }
+
+__DATA__
+12:00 am - 00:00
+12:01 AM - 00:01
+12:59 Am - 00:59
+1:00 aM - 01:00
+6:00 am - 06:00
+10:37 am - 10:37
+11:59 am - 11:59
+12:00 pm - 12:00
+12:59 pm - 12:59
+1:00 pm - 13:00
+6:15 PM - 18:15
+10:45 Pm - 18:15
+11:59 pM - 23:59
