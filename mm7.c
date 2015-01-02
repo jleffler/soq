60b26add41cf07a350896a36c6dd04edfaa330e9
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-01-01 17:44:18 -0800

    mm7.c - revise to use persistent storage on initialization

--- mm7.c
+++ mm7.c
@@ -8,8 +8,8 @@
 #include <unistd.h>
 
 #define FILEPATH "/tmp/mmapped.bin"
-#define NUMINTS (1000)
-#define FILESIZE (NUMINTS * sizeof(struct med))
+#define NUM_MEDS (1000)
+#define FILESIZE (NUM_MEDS * sizeof(struct med))
 
 struct med
 {
@@ -48,54 +48,38 @@ static int get_new_key(const struct med *map, int num_meds, int *key)
     return EOF;
 }
 
+static int find_max_entry(const struct med *map, int max_meds)
+{
+    int i;
+    for (i = 0; i < max_meds; i++)
+    {
+        if (map[i].key == 0)
+            break;
+    }
+    return i;
+}
+
 int main(void)
 {
     int fd;
     int result;
-    struct med *map;  /* mmapped array of structs */
+    struct med *map;
 
-    /* Open a file for writing.
-     *  - Creating the file if it doesn't exist.
-     *  - Truncating it to 0 size if it already exists. (not really needed)
-     *
-     * Note: "O_WRONLY" mode is not sufficient when mmapping.
-     */
-    fd = open(FILEPATH, O_RDWR | O_CREAT | O_TRUNC, (mode_t)0600);
+    fd = open(FILEPATH, O_RDWR | O_CREAT, (mode_t)0600);
     if (fd == -1)
     {
         perror("Error opening file for writing");
         exit(EXIT_FAILURE);
     }
 
-    /* NB: ftruncate(fd, FILESIZE); is simpler */
-    /* Stretch the file size to the size of the (mmapped) array of structs */
-    result = lseek(fd, FILESIZE - 1, SEEK_SET);
+    result = ftruncate(fd, FILESIZE);
     if (result == -1)
     {
         close(fd);
-        perror("Error calling lseek() to 'stretch' the file");
+        perror("Error calling ftruncate() to 'set' the file size");
         exit(EXIT_FAILURE);
     }
 
-    /* Something needs to be written at the end of the file to
-     * have the file actually have the new size.
-     * Just writing an empty string at the current file position will do.
-     *
-     * Note:
-     *  - The current position in the file is at the end of the stretched
-     *    file due to the call to lseek().
-     *  - An empty string is actually a single '\0' character, so a zero-byte
-     *    will be written at the last byte of the file.
-     */
-    result = write(fd, "", 1);
-    if (result != 1)
-    {
-        close(fd);
-        perror("Error writing last byte of the file");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Now the file is ready to be mmapped.  */
     map = (struct med *)mmap(0, FILESIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (map == MAP_FAILED)
     {
@@ -106,7 +90,7 @@ int main(void)
 
     /* Input loop */
     int num_meds;
-    for (num_meds = 0; num_meds < NUMINTS; num_meds++)
+    for (num_meds = find_max_entry(map, NUM_MEDS); num_meds < NUM_MEDS; num_meds++)
     {
         struct med m;
         memset(&m, '\0', sizeof(m));
@@ -147,7 +131,5 @@ int main(void)
 
     /* Un-mmapping doesn't close the file, so we still need to do that.  */
     close(fd);
-
-    /* Remove file? */
-    /* unlink(FILEPATH); */
+    return 0;
 }
