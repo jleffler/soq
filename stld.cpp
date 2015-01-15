d8bb3a70cc50ff63681eafe4b8243c5ee1a6573a
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-01-15 08:11:13 -0800

    Add ld2_be<I>() and ld2_le<I>() and test them. Add st_be<I> and st_le<I> but not tested yet.

--- stld.cpp
+++ stld.cpp
@@ -10,6 +10,15 @@ I ld_be(const unsigned char *buffer)
 }
 
 template <typename I>
+const unsigned char *ld2_be(const unsigned char *buffer, I &rv)
+{
+    rv = 0;
+    for (size_t i = 0; i < sizeof(I); ++i)
+        rv = (rv << 8) | buffer[i];
+    return &buffer[sizeof(I)];
+}
+
+template <typename I>
 I ld_le(const unsigned char *buffer)
 {
     I rv = 0;
@@ -18,11 +27,42 @@ I ld_le(const unsigned char *buffer)
     return rv;
 }
 
+template <typename I>
+const unsigned char *ld2_le(const unsigned char *buffer, I &rv)
+{
+    rv = 0;
+    for (size_t i = sizeof(I); i > 0; --i)
+        rv = (rv << 8) | buffer[i-1];
+    return &buffer[sizeof(I)];
+}
+
+template <typename I>
+unsigned char *st_be(unsigned char *buffer, I value)
+{
+    for (size_t i = 0; i < sizeof(I); ++i)
+    {
+        *buffer++ = value & 0xFF;
+        value >>= 8;
+    }
+    return buffer;
+}
+
+template <typename I>
+unsigned char *st_le(unsigned char *buffer, I value)
+{
+    for (size_t i = 0; i < sizeof(I); ++i)
+    {
+        *buffer++ = value & 0xFF;
+        value >>= 8;
+    }
+    return buffer;
+}
+
 #include <cstdint>
 #include <iostream>
 using namespace std;
 
-int main(void)
+void test1(void)
 {
     unsigned char data[] = "A1C2E3G4";
 
@@ -45,4 +85,68 @@ int main(void)
     uint8_t le8 = ld_le<uint8_t>(data);
     cout << "Data: " << data << ", BE08 = 0x" << hex << int(be8) << "\n";
     cout << "Data: " << data << ", LE08 = 0x" << hex << int(le8) << "\n";
+}
+
+/*
+** Test ld2_le<I>() and ld2_be<I>().
+** Storing values in the begin array is a test artefact rather than part
+** of normal usage.  The data portion of the output line could be
+** printed before the call to the function, which would render the array
+** unnecessary.
+** The disadvantage of this technique is that it requires variables
+** defined before they're initialized.  If they're part of a structure,
+** though, that isn't a problem.
+** The advantage of this technique is that the controlling type is a
+** parameter, so you don't need to specify the type in the call.  Or is
+** that a disadvantage?
+*/
+void test2(void)
+{
+    const unsigned char data[] = "@0A1B2C3D4E5F6G7H8I9a1b2c3d4e5f6g7h8i9";
+    const unsigned char *ptr = data;
+    const unsigned char *begin[8];
+    int i = 0;
+
+    uint64_t be64;
+    begin[i++] = ptr;
+    ptr = ld2_be(ptr, be64);
+    uint64_t le64;
+    begin[i++] = ptr;
+    ptr = ld2_le(ptr, le64);
+    cout << "Data: " << begin[0] << ", BE64 = 0x" << hex << be64 << "\n";
+    cout << "Data: " << begin[1] << ", LE64 = 0x" << hex << le64 << "\n";
+
+    uint32_t be32;
+    begin[i++] = ptr;
+    ptr = ld2_be(ptr, be32);
+    uint32_t le32;
+    begin[i++] = ptr;
+    ptr = ld2_le(ptr, le32);
+    cout << "Data: " << begin[2] << ", BE32 = 0x" << hex << be32 << "\n";
+    cout << "Data: " << begin[3] << ", LE32 = 0x" << hex << le32 << "\n";
+
+    uint16_t be16;
+    begin[i++] = ptr;
+    ptr = ld2_be(ptr, be16);
+    uint16_t le16;
+    begin[i++] = ptr;
+    ptr = ld2_le(ptr, le16);
+    cout << "Data: " << begin[4] << ", BE16 = 0x" << hex << be16 << "\n";
+    cout << "Data: " << begin[5] << ", LE16 = 0x" << hex << le16 << "\n";
+
+    uint8_t be8;
+    begin[i++] = ptr;
+    ptr = ld2_be(ptr, be8);
+    uint8_t le8;
+    begin[i++] = ptr;
+    ptr = ld2_be(ptr, le8);
+    cout << "Data: " << begin[6] << ", BE08 = 0x" << hex << int(be8) << "\n";
+    cout << "Data: " << begin[7] << ", LE08 = 0x" << hex << int(le8) << "\n";
+    cout << "Unused: " << ptr << "\n";
+}
+
+int main()
+{
+    test1();
+    test2();
 }
