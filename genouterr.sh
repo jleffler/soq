60623620a78447602826d49e7898e8f75ee81afb
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-02-12 21:33:10 -0800

    Remove C, C++, Shell, Perl code, etc.  Add reader.py.

--- genouterr.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in {01..50}
-do
-  echo "stdout $i"
-  echo "stderr $i" >&2
-done
---
+++ reader.py
@@ -0,0 +1,18 @@
+import fileinput
+
+d = {}
+
+for line in fileinput.input():
+    line = line.rstrip('\r\n') # line.strip() for leading and trailing space
+    if line.startswith('Clutch'):
+        key = line
+        d[key] = ''
+        pad = ''
+    else:
+        d[key] += pad + line
+        pad = ', '
+
+print d
+
+for key in d:
+    print "'%s': '%s'" % (key, d[key])
