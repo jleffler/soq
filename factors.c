13b4df7b5af8b2f8af9e7753680bc72af6beddde
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-04-11 07:46:16 -0700

    factors.c: rework so primes are calculated just once

--- factors.c
+++ factors.c
@@ -4,77 +4,76 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
-** This algorithm is horribly inefficient for large numbers because it
-** recomputes primes for each number that it factors.
-*/
-
-static void list_divisors(int n)
+static int *set_primes(int n)
 {
-    int lim = (int)floor(sqrt((double)n));
     int *primes = (int *)calloc(n, sizeof(int));
-    int *divisors = (int *)calloc(n, sizeof(int));
-    int i, j, ctr;
-    ctr = 0;
 
-    if (primes && divisors)
+    if (primes == 0)
     {
-        for (i = 0; i < n; i++)
-        {
-            primes[i] = 1;
-            divisors[i] = 0;
-        }
-
-        for (i = 2; i < lim; i++)
-        {
-            if (primes[i])
-            {
-                for (j = i; i * j < n; j++)
-                {
-                    primes[i * j] = 0;
-                }
-            }
-        }
-
-        for (i = 2; i < n; i++)
-        {
-            if (primes[i])
-            {
-                if (n % i == 0)
-                    divisors[i] = 1;
-                for (j = i; j < n / i; j++)
-                {
-                    int result = n % (i * j);
-                    if (result == 0)
-                    {
-                        //printf("n = %6d; i = %6d; j = %d\n", n, i, j);
-                        divisors[i * j] = 1;
-                    }
-                }
-            }
-        }
-
-        printf("%d: 1", n);
-        for (i = 2; i < n; i++)
-        {
-            if (divisors[i])
-            {
-                printf(",%d", i);
-                ctr++;
-            }
-        }
-        ctr += 2;   /* For 1 and n, of course */
-        printf(",%d (%d factors)\n", n, ctr);
+        fprintf(stderr, "Memory allocation failed (%zu bytes)\n", n * sizeof(int));
+        exit(1);
     }
-    else
+
+    for (int i = 0; i < n; i++)
+        primes[i] = 1;
+
+    int lim = (int)floor(sqrt((double)n));
+    for (int i = 2; i < lim; i++)
     {
-        fprintf(stderr, "Allocation failed.\n");
+        if (primes[i])
+        {
+            for (int j = i; i *j < n; j++)
+                primes[i * j] = 0;
+        }
     }
-    free(primes);
-    free(divisors);
+
+    return primes;
 }
 
-int main(int argc, char * *argv)
+static void list_divisors(int n, int *primes, int *divisors)
+{
+    int ctr = 0;
+
+    for (int i = 0; i < n; i++)
+        divisors[i] = 0;
+
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (primes[i])
+        {
+            if (n % i == 0)
+                divisors[i] = 1;
+            for (int j = i; j < n / i; j++)
+            {
+                int result = n % (i * j);
+                if (result == 0)
+                {
+                    // printf("n = %6d; i = %6d; j = %d\n", n, i, j);
+                    divisors[i * j] = 1;
+                }
+            }
+        }
+    }
+
+    printf("%d: 1", n);
+    for (int i = 2; i <= n / 2; i++)
+    {
+        if (divisors[i])
+        {
+            printf(",%d", i);
+            ctr++;
+        }
+    }
+    ctr++;      // 1 is a factor
+    if (n != 1)
+    {
+        ctr++;  // n is a factor
+        printf(",%d", n);
+    }
+    printf(" (%d factors)\n", ctr);
+}
+
+int main(int argc, char **argv)
 {
     int min = 1;
     int max = 65535;
@@ -92,9 +91,27 @@ int main(int argc, char * *argv)
         fprintf(stderr, "Usage: %s [[min] max]\n", argv[0]);
         return 1;
     }
+    if (min <= 0 || max <= 0 || min > max)
+    {
+        fprintf(stderr, "Arguments must be positive and"
+                " min must not be greater than max\n");
+        exit(1);
+    }
     printf("Range = %d:%d\n", min, max);
+
+    int *primes = set_primes(max);
+    int *divisors = (int *)calloc(max, sizeof(int));
+
+    if (divisors == 0)
+    {
+        fprintf(stderr, "Memory allocation failed (%zu bytes)\n", max * sizeof(int));
+        exit(1);
+    }
+
     for (int i = min; i <= max; i++)
-        list_divisors(i);
+        list_divisors(i, primes, divisors);
+
+    free(primes);
+    free(divisors);
     return 0;
 }
-
