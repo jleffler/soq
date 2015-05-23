d30a907c23b4ff1dbbd323ab8429cf09ce573c09
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-05-23 00:30:59 -0700

    quiz.py: major rewrite, using array of dictionaries, etc.

--- quiz.py
+++ quiz.py
@@ -1,14 +1,64 @@
-cont = ""
+#!/usr/bin/env python3
 
 questions = [
-    "What is the Zeroth Law of Thermodynamics?",
-    "Who is the current Prime Minister of Canada?",
-    "Which document outlines the specific rights guaranteed to all Canadian citizens?",
-    "Which level of government is responsible for Tourism?",
-    "Which of the following is not a Fundamental Freedom?",
-    "(Fill in the blank) The three levels of government are Federal, Provincial and _______?",
-    "(Fill in the blank) A two-house system of government is called ________?",
+    {   'question': "What is the Zeroth Law of Thermodynamics?", },
+
+    {   'question': "Who is the current Prime Minister of Canada?",
+        'correct': "b",
+        'options': [
+        "a) Jean Chretien",
+        "b) Stephen Harper",
+        "c) Cam Guthrie",
+        "d) Dalton McGuinty",
+        "e) Steve Jobs",
+        ],
+    },
+
+    {   'question': "Which document outlines the specific rights guaranteed to all Canadian citizens?",
+        'correct': "c",
+        'options': [
+            "a) The Universal Declaration of Human Rights",
+            "b) The American Constitution",
+            "c) The Canadian Charter of Rights and Freedoms",
+            "d) The Ontario Human Rights Code",
+            "e) The Convention on the Rights of the child",
+        ],
+    },
+
+    {   'question': "Which level of government is responsible for Tourism?",
+        'correct': "d",
+        'options': [
+            "a) Municipal",
+            "b) Federal",
+            "c) Provincial",
+            "d) All",
+            "e) Legislative",
+        ],
+    },
+
+    {   'question': "Which of the following is not a Fundamental Freedom?",
+        'correct': "a",
+        'options': [
+            "a) Presumed innocent until proven guilty",
+            "b) Conscience and Religion",
+            "c) Opinion and Expression",
+            "d) Assembly and Association",
+            "e) Freedom of peaceful assembly",
+        ],
+    },
+
+    {   'question': "The three levels of government are Federal, Provincial and ________?",
+        'correct': "municipal",
+    },
+
+    {   'question': "A two-house system of government is called ________?",
+        'correct': "bicameral",
+    },
+
 ]
+
+right = []
+wrong = []
 
 def get_answer(prompt):
     answer = input(prompt).lower()
@@ -16,123 +66,62 @@ def get_answer(prompt):
         answer = input(prompt).lower()
     return answer
 
+def ask_question(qnum, qinfo):
+    score = 0
+    for tries in range(2):
+        fitb = ""
+        prompt = "Make your choice: "
+        if not qinfo.get('options'):
+            fitb = "(Fill in the blank)"
+            prompt = "Enter your answer: "
+        print("\nQUESTION", qnum, ":", fitb, qinfo['question'])
+        if questions[qnum].get('options'):
+            for opt in qinfo['options']:
+                print(" ", opt)
+        answer = get_answer(prompt)
+        if answer == qinfo['correct']:
+            print("Correct!")
+            score = score + 1
+            right.append(qnum)
+            break
+        else:
+            print("False!")
+    else:
+        wrong.append(qnum)
+        print("Out of Chances!")
+    return score
+
+def right_wrong(tag, qnos):
+    if len(qnos) > 0:
+        print("You got these questions", tag, ":")
+        for n in range(len(qnos)):
+            print(" ", qnos[n], ": ", questions[qnos[n]]['question'])
+
+quiz = "'So You Think You Can Civics'"
+cont = ""
 while cont != "n":
+    cont = "n"
     score = 0
     qnum = 0
     right = []
     wrong = []
-    print("Welcome to the 'So You Think You Can Civics' quiz.")
+    print("Welcome to the", quiz, "quiz.")
     print("This quiz will test your knowledge on basic civics topics.")
-    print("Let's see if you were paying attention in Civics class!")
+    print("Let's see if you were paying attention in civics class!")
 
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        print(" a) Jean Chretien", "\n","b) Stephen Harper", "\n", "c) Cam Guthrie", "\n", "d) Dalton McGuinty", "\n", "e) Steve Jobs")
-        answer = get_answer("Make your choice: ")
-        if answer == "b":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        wrong.append(qnum)
-        print("Out of Chances!")
+    num_q = len(questions) - 1
+    for qnum in range(1, len(questions)):
+        score += ask_question(qnum, questions[qnum])
 
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        print(" a) The Universal Declaration of Human Rights", "\n", "b) The American Constitution", "\n", "c) The Canadian Charter of Rights and Freedoms", "\n", "d) The Ontario Human Rights Code", "\n", "e) The Convention on the Rights of the child")
-        answer = get_answer("Make your choice: ")
-        if answer == "c":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        print("Out of Chances!")
-        wrong.append(qnum)
+    print("")
+    print("Your score is: ", score, "/", num_q)
+    percent = (score / num_q) * 100
+    print(("Your percentage is: {0:2.0f} ".format(percent)), "%")
 
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        print(" a) Municipal\n", "b) Federal\n", "c) Provincial\n", "d) All\n", "e) Legislative")
-        answer = get_answer("Make your choice: ")
-        if answer == "d":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        print("Out of Chances!")
-        wrong.append(qnum)
+    right_wrong("right", right)
+    right_wrong("wrong", wrong)
 
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        print(" a) Presumed innocent until proven guilty", "\n", "b) Conscience and Religion", "\n", "c) Opinion and Expression", "\n", "d) Assembly and Association", "\n", "e) Freedom of peaceful assembly")
-        answer = get_answer("Make your choice: ")
-        if answer == "a":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        print("Out of Chances!")
-        wrong.append(qnum)
+    if score < num_q:
+        cont = input("\nContinue (y) or (n): ")
 
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        answer = get_answer("Enter your answer: ")
-        if answer == "municipal":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        print("Out of Chances!")
-        wrong.append(qnum)
-
-    qnum = qnum + 1
-    for tries in range(2):
-        print("\nQUESTION ", qnum, ":\n", questions[qnum])
-        answer = get_answer("Enter your answer: ")
-        if answer == "bicameral":
-            print("Correct!")
-            score = score + 1
-            right.append(qnum)
-            break
-        else:
-            print("False!")
-    else:
-        print("Out of Chances!")
-        wrong.append(qnum)
-
-    print("Your score is: ", score,"/6")
-    percent = (score/6)*100
-    print(("Your percentage is:{0:2.0f} ".format(percent)),"%")
-
-    if len(right) > 0:
-        print("You got these questions right:")
-        for r in range(len(right)):
-            print(" ", right[r], ": ", questions[right[r]])
-
-    if len(wrong) > 0:
-        print("You got these questions wrong:")
-        for w in range(len(wrong)):
-            print(" ", wrong[w], ": ", questions[wrong[w]])
-
-    cont = input("Continue (y) or (n):")
-
-print("\nThanks for completing the 'So You Think You Can Civics' quiz!")
+print("\nThanks for completing the", quiz, "quiz!")
