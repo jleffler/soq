50a4dfdfa311067679a2c701cee26c8c367d4362
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-05-25 09:01:58 -0700

    common.sh: add basic option handling and preliminary answer by JL

--- common.sh
+++ common.sh
@@ -1,7 +1,18 @@
 #!/bin/bash
 # SO 30428099
 
-awk '
+while getopts n: opt
+do
+    case "$opt" in
+    (n) num_files=$OPTARG;;
+    (*) echo "Usage: $(basename "$0" .sh) [-n number] file [...]" >&2
+        exit 1;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+awk -v num_files=${num_files:-$#} '
 FNR == NR { 
     arr[$1,$2] = 1
     line[$1,$2] = line[$1,$2] (line[$1,$2] ? SUBSEP : "") $0
@@ -10,13 +21,14 @@ FNR == NR {
 FNR == 1 { delete found }
 { if (arr[$1,$2] && ! found[$1,$2]) { arr[$1,$2]++; found[$1,$2] = 1 } }
 END { 
-    num_files = ARGC -1 
+    if (num_files == 0) num_files = ARGC - 1
     for (key in arr) {
-        if (arr[key] < num_files) { continue }
-        split(line[key], line_arr, SUBSEP)
-        for (i = 1; i <= length(line_arr); i++) { 
-            printf "%s\n", line_arr[i]
-        } 
-    } 
+        if (arr[key] == num_files) {
+            split(line[key], line_arr, SUBSEP)
+            for (i = 1; i <= length(line_arr); i++) {
+                printf "%s\n", line_arr[i]
+            }
+        }
+    }
 }
- ' "$@"
+' "$@"
