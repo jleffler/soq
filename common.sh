a3e069ba070fc3eb4d218921823d84d736645eb4
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-05-25 14:05:05 -0700

    common.sh: update to give desired answer.  Rename zalgo.utf8 to zalgo1.utf8

--- common.sh
+++ common.sh
@@ -1,33 +1,57 @@
 #!/bin/bash
 # SO 30428099
 
+# Given that the key for a line is the first two columns, this script
+# lists all appearances in all files of a given key if that key appears
+# in N different files (where N defaults to the number of files). For
+# the benefit of debugging, it includes the file name with each line.
+
+usage()
+{
+    echo "Usage: $(basename "$0" .sh) [-n number] file [...]" >&2
+    exit 1
+}
+
 while getopts n: opt
 do
     case "$opt" in
     (n) num_files=$OPTARG;;
-    (*) echo "Usage: $(basename "$0" .sh) [-n number] file [...]" >&2
-        exit 1;;
+    (*) usage;;
     esac
 done
 
 shift $(($OPTIND - 1))
 
+if [ "$#" = 0 ]
+then usage
+fi
+
+# Record count of each key, regardless of file: keys
+# Record count of each key in each file: key_file
+# Count of different files containing each key: files
+# Accumulate line number, filename, line for each key: lines
+
 awk -v num_files=${num_files:-$#} '
-FNR == NR { 
-    arr[$1,$2] = 1
-    line[$1,$2] = line[$1,$2] (line[$1,$2] ? SUBSEP : "") $0
-    next
+{ 
+    keys[$1,$2]++;
+    if (++key_file[$1,$2,FILENAME] == 1)
+        files[$1,$2]++
+    #printf "%s:%d: Key (%s,%s); keys = %d; key_file = %d; files = %d\n",
+    #        FILENAME, FNR, $1, $2, keys[$1,$2], key_file[$1,$2,FILENAME], files[$1,$2];
+    sep = lines[$1,$2] ? RS : ""
+    #printf "B: [[\n%s\n]]\n", lines[$1,$2]
+    lines[$1,$2] = lines[$1,$2] sep FILENAME OFS FNR OFS $0
+    #printf "A: [[\n%s\n]]\n", lines[$1,$2]
 }
-FNR == 1 { delete found }
-{ if (arr[$1,$2] && ! found[$1,$2]) { arr[$1,$2]++; found[$1,$2] = 1 } }
-END { 
-    if (num_files == 0) num_files = ARGC - 1
-    for (key in arr) {
-        if (arr[key] == num_files) {
-            split(line[key], line_arr, SUBSEP)
-            for (i = 1; i <= length(line_arr); i++) {
-                printf "%s\n", line_arr[i]
-            }
+END {
+    #print "END"
+    for (key in files)
+    {
+        #print "Key =", key, "; files =", files[key]
+        if (files[key] == num_files)
+        {
+            #printf "TAG\n%s\nEND\n", lines[key]
+            print lines[key]
         }
     }
 }
