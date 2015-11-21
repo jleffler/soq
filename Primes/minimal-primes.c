43b0f50aefe0ca83019660f6920fb87914e3601c
Jonathan Leffler <jonathan.leffler@gmail.com>
2015-11-21 09:37:01 -0800

    Fix botch in lower-limit of nested loops (should be previous loop index + 1, not 0). Add recursive has_prime_n_digit_subset() function - the testing of which showed up the other bug.

--- Primes/minimal-primes.c
+++ Primes/minimal-primes.c
@@ -40,13 +40,42 @@ static bool is_prime(int n)
     return true;
 }
 
+static bool has_prime_n_digit_subset(int p0, int n_digits, const char *buffer, int buflen)
+{
+    //printf("-->> p0 = %5d, n = %d (%s)\n", p0, n_digits, buffer);
+    assert(buflen >= 0 && strlen(buffer) == (size_t)buflen);
+    for (int i = 0; i < buflen; i++)
+    {
+        int p1 = 10 * p0 + buffer[i] - '0';
+        if (n_digits > 1)
+        {
+            if (has_prime_n_digit_subset(p1, n_digits - 1, &buffer[i+1], buflen - i - 1))
+            {
+                //printf("<<-- true\n");
+                return true;
+            }
+        }
+        else
+        {
+            //printf("C %d\n", p1);
+            if (is_prime(p1))
+            {
+                //printf("<<-- p1 = %d: true\n", p1);
+                return true;
+            }
+        }
+    }
+    //printf("<<-- false\n");
+    return false;
+}
+
 static bool has_prime_1_digit_subset(const char *buffer, int n_digits)
 {
     /* Single digits - 2, 3, 5, 7 prevent a number being a minimal prime */
     for (int i = 0; i < n_digits - 0; i++)
     {
         int p1 = buffer[i] - '0';
-        //printf("C %d\n", p1);
+        //printf("C1 %d\n", p1);
         if (is_prime(p1))
             return true;
     }
@@ -59,10 +88,10 @@ static bool has_prime_2_digit_subset(const char *buffer, int n_digits)
     for (int i = 0; i < n_digits - 1; i++)
     {
         int p1 = buffer[i] - '0';
-        for (int j = 0; j < n_digits - 0; j++)
+        for (int j = i + 1; j < n_digits - 0; j++)
         {
             int p2 = p1 * 10 + buffer[j] - '0';
-            //printf("C %d\n", p2);
+            //printf("C2 %d\n", p2);
             if (is_prime(p2))
                 return true;
         }
@@ -76,13 +105,13 @@ static bool has_prime_3_digit_subset(const char *buffer, int n_digits)
     for (int i = 0; i < n_digits - 2; i++)
     {
         int p1 = buffer[i] - '0';
-        for (int j = 0; j < n_digits - 1; j++)
+        for (int j = i + 1; j < n_digits - 1; j++)
         {
             int p2 = p1 * 10 + buffer[j] - '0';
-            for (int k = 0; k < n_digits - 0; k++)
+            for (int k = j + 1; k < n_digits - 0; k++)
             {
                 int p3 = p2 * 10 + buffer[k] - '0';
-                //printf("C %d\n", p3);
+                //printf("C3 %d\n", p3);
                 if (is_prime(p3))
                     return true;
             }
@@ -97,16 +126,16 @@ static bool has_prime_4_digit_subset(const char *buffer, int n_digits)
     for (int i = 0; i < n_digits - 3; i++)
     {
         int p1 = buffer[i] - '0';
-        for (int j = 0; j < n_digits - 2; j++)
+        for (int j = i + 1; j < n_digits - 2; j++)
         {
             int p2 = p1 * 10 + buffer[j] - '0';
-            for (int k = 0; k < n_digits - 1; k++)
+            for (int k = j + 1; k < n_digits - 1; k++)
             {
                 int p3 = p2 * 10 + buffer[k] - '0';
-                for (int l = 0; l < n_digits - 0; l++)
+                for (int l = k + 1; l < n_digits - 0; l++)
                 {
                     int p4 = p3 * 10 + buffer[l] - '0';
-                    //printf("C %d\n", p4);
+                    //printf("C4 %d\n", p4);
                     if (is_prime(p4))
                         return true;
                 }
@@ -122,19 +151,19 @@ static bool has_prime_5_digit_subset(const char *buffer, int n_digits)
     for (int i = 0; i < n_digits - 4; i++)
     {
         int p1 = buffer[i] - '0';
-        for (int j = 0; j < n_digits - 3; j++)
+        for (int j = i + 1; j < n_digits - 3; j++)
         {
             int p2 = p1 * 10 + buffer[j] - '0';
-            for (int k = 0; k < n_digits - 2; k++)
+            for (int k = j + 1; k < n_digits - 2; k++)
             {
                 int p3 = p2 * 10 + buffer[k] - '0';
-                for (int l = 0; l < n_digits - 1; l++)
+                for (int l = k + 1; l < n_digits - 1; l++)
                 {
-                    int p4 = p3 * 10 + buffer[k] - '0';
-                    for (int m = 0; m < n_digits - 0; m++)
+                    int p4 = p3 * 10 + buffer[l] - '0';
+                    for (int m = l + 1; m < n_digits - 0; m++)
                     {
-                        int p5 = p4 * 10 + buffer[l] - '0';
-                        //printf("C %d\n", p5);
+                        int p5 = p4 * 10 + buffer[m] - '0';
+                        //printf("C5 %d\n", p5);
                         if (is_prime(p5))
                             return true;
                     }
@@ -145,7 +174,26 @@ static bool has_prime_5_digit_subset(const char *buffer, int n_digits)
     return false;
 }
 
-static void check_minimal_prime(int n)
+static void check_minimal_prime2(int n)
+{
+    assert(n < 1000000);
+    if (is_prime(n))
+    {
+        //printf("P %d\n", n);
+        char buffer[sizeof("999999")];
+        sprintf(buffer, "%d", n);
+        char n_digits = strlen(buffer);
+        for (int i = 1; i < n_digits; i++)
+        {
+            if (has_prime_n_digit_subset(0, i, buffer, n_digits))
+                return;
+        }
+
+        printf("== M2 == %d\n", n);  /* It's a minimal prime */
+    }
+}
+
+static void check_minimal_prime1(int n)
 {
     assert(n < 1000000);
     if (is_prime(n))
@@ -166,19 +214,31 @@ static void check_minimal_prime(int n)
         if (n_digits > 5 && has_prime_5_digit_subset(buffer, n_digits))
             return;
 
-        printf("== M == %d\n", n);  /* It's a minimal prime */
+        printf("== M1 == %d\n", n);  /* It's a minimal prime */
+    }
+}
+
+static void mp_checker(void (*function)(int))
+{
+    int max = 1000000 / 6;
+    (*function)(2);
+    (*function)(3);
+    for (int c = 1; c < max; c++)
+    {
+        (*function)(6 * c - 1);
+        (*function)(6 * c + 1);
     }
 }
 
 int main(void)
 {
-    int max = 1000000 / 6;
-    check_minimal_prime(2);
-    check_minimal_prime(3);
-    for (int c = 1; c < max; c++)
+    //check_minimal_prime2(881);
+    //check_minimal_prime1(946669);
+    //check_minimal_prime2(946669);
+    if (1)
     {
-        check_minimal_prime(6 * c - 1);
-        check_minimal_prime(6 * c + 1);
+    mp_checker(check_minimal_prime1);
+    mp_checker(check_minimal_prime2);
     }
     return 0;
 }
