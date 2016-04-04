a15708aec0d82c962bab9b3b84eeabff185ed57d
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-04-04 00:04:44 -0700

    dllswap.c: get some basic list manipulation working OK.  The list is circular.

--- dllswap.c
+++ dllswap.c
@@ -70,27 +70,43 @@ static inline void  ins_item(List *posn, List *item);
 static inline void  add_item(List *posn, List *item);
 static inline void  rem_item(List *item);
 static inline void  del_item(List *item);
-
 static inline void  prt_item(FILE *fp, List *item);
+
 static inline void  prt_list(FILE *fp, const char *tag, List *list);
 static inline void  zap_list(List *list);
 static inline void  fwd_list(List *list, Apply apply, void *context);
 static inline void  app_item(List *item, void *context);
 
-static List root = { 0, &root, &root };
-static List *head = &root;
+//static List root = { 0, &root, &root };
+//static List *head = &root;
 
 static inline List *new_item(Data value)
 {
     List *item = malloc(sizeof(*item));
     if (item == 0)
         err_syserr("Out of memory! ");
-    item->data = strdup(value);
+    if (value == 0)
+        item->data = 0;
+    else
+        item->data = strdup(value);
     item->next = 0;
     item->prev = 0;
     return item;
 }
 
+static inline void  prt_item(FILE *fp, List *item)
+{
+    if (item != 0)
+    {
+        fprintf(fp, "I = 0x%.12" PRIXPTR ", P = 0x%.12" PRIXPTR ", N = 0x%.12" PRIXPTR ": ",
+                (uintptr_t)item, (uintptr_t)item->prev, (uintptr_t)item->next);
+        if (item->data != 0)
+            fprintf(fp, "[%" PRI_Data "]", item->data);
+        putc('\n', fp);
+    }
+}
+
+/* Insert item before posn */
 static inline void ins_item(List *posn, List *item)
 {
     item->prev = posn->prev;
@@ -99,27 +115,28 @@ static inline void ins_item(List *posn, List *item)
     posn->prev = item;
 }
 
+/* Append item after posn */
 static inline void add_item(List *posn, List *item)
 {
+    printf("Before adding:\n");
+    prt_item(stdout, posn);
+    prt_item(stdout, item);
     item->prev = posn;
     item->next = posn->next;
     posn->next->prev= item;
     posn->next = item;
+    printf("After adding:\n");
+    prt_item(stdout, posn);
+    prt_item(stdout, item);
 }
 
+/* Remove item */
 static inline void rem_item(List *item)
 {
     item->prev->next = item->next;
     item->next->prev = item->prev;
-    if (head == item)
-        head = item->next;
-}
-
-static inline void  prt_item(FILE *fp, List *item)
-{
-    fprintf(fp, "P = 0x%.8" PRIXPTR ", N = 0x%.8" PRIXPTR ": ",
-            (uintptr_t)item->prev, (uintptr_t)item->next);
-    fprintf(fp, "%" PRI_Data "\n", item->data);
+    //if (head == item)
+    //    head = item->next;
 }
 
 static inline void app_item(List *item, void *context)
@@ -129,28 +146,37 @@ static inline void app_item(List *item, void *context)
 
 static inline void fwd_list(List *list, Apply appfun, void *context)
 {
-    List *item = list;
-    while (item != 0 && item != list)
+    if (list != 0)
     {
-        (*appfun)(context, item);
-        item = item->next;
+        List *item = list;
+        while (item->next != list)
+        {
+            assert(item->next != 0);
+            item = item->next;
+            (*appfun)(item, context);   /* Beware: arguments can be reversed without a compiler error! */
+        }
     }
 }
 
 static inline void rev_list(List *list, Apply appfun, void *context)
 {
-    List *item = list;
-    while (item != 0 && item != list)
+    if (list != 0)
     {
-        (*appfun)(context, item);
-        item = item->prev;
+        List *item = list;
+        while (item->prev != list)
+        {
+            assert(item->prev != 0);
+            item = item->prev;
+            (*appfun)(item, context);   /* Beware: arguments can be reversed without a compiler error! */
+        }
     }
 }
 
 static inline void prt_list(FILE *fp, const char *tag, List *list)
 {
-    fprintf(fp, "List: %s (0x%.8" PRIXPTR ")\n", tag, (uintptr_t)list);
+    fprintf(fp, "List: %s (0x%.12" PRIXPTR ")\n", tag, (uintptr_t)list);
     fwd_list(list, app_item, fp);
+    fprintf(fp, "EOL\n");
 }
 
 static inline void del_item(List *item)
@@ -161,11 +187,18 @@ static inline void del_item(List *item)
 
 static inline void zap_list(List *list)
 {
-    while (list != 0)
+    if (list != 0)
     {
-        List *next = list->next;
-        del_item(list);
-        list = next;
+        printf("Zapping list:\n");
+        List *item = list;
+        List *next;
+        while ((next = item->next) != list)
+        {
+            prt_item(stdout, item);
+            del_item(item);
+            item = next;
+        }
+        del_item(item);
     }
 }
 
@@ -195,7 +228,7 @@ static inline void swap_items(List *i1, List *i2)
 
 int main(void)
 {
-    //head->next = head->prev = head;
+    List *head = new_item(0);
     List *i1 = new_item("P1");
     List *i2 = new_item("P2");
     List *i3 = new_item("P3");
@@ -203,6 +236,7 @@ int main(void)
     List *i5 = new_item("B");
 
     printf("Items created\n");
+    assert(head != 0);
     assert(i1 != 0);
     assert(i2 != 0);
     assert(i3 != 0);
@@ -214,23 +248,32 @@ int main(void)
     prt_item(stdout, i4);
     prt_item(stdout, i5);
 
+    head->next = head->prev = head;
+    prt_item(stdout, head);
+
+    prt_list(stdout, "List 1 Before", head);
     printf("Add i3\n");
-    //add_item(head, i3);
+    add_item(head, i3);
+    prt_list(stdout, "List 1 after i3", head);
     printf("Add i5\n");
-    //add_item(head, i5);
+    add_item(head, i5);
+    prt_list(stdout, "List 1 after i5", head);
     printf("Add i2\n");
-    //add_item(head, i2);
+    add_item(head, i2);
+    prt_list(stdout, "List 1 after i2", head);
     printf("Add i4\n");
-    //add_item(head, i4);
+    add_item(head, i4);
+    prt_list(stdout, "List 1 after i4", head);
     printf("Add i1\n");
-    //add_item(head, i1);
+    add_item(head, i1);
+    prt_list(stdout, "List 1 after i1", head);
     printf("Items added\n");
 
     //prt_list(stdout, "List 1 Before", head);
     //swap_items(i4, i5);
     //prt_list(stdout, "List 1 After", head);
 
-    //zap_list(head);
+    zap_list(head);
 
     return 0;
 }
