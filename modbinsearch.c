0fc8b91acc3edae1b34d189f180debafdf2f6e52
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-04-09 18:03:44 -0700

    modbinsearch.c: rename modbinsearch() to BinSearch_D().  Make arrays constant.  Make types in BinSearch_D() consistent with BinSearch_[ABC]. Diagnostic check takes array name.  Test is given array name.  Code testing BinSearch_D() placed in if (0) { ... } block to test compilation without activating the code.  Comments moved around.  Etc.

--- modbinsearch.c
+++ modbinsearch.c
@@ -22,23 +22,16 @@
 #include <string.h>
 #include <unistd.h>
 
-static const char optstr[] = "hV";
-static const char usestr[] = "[-hV]";
-static const char hlpstr[] = 
-    "  -h  Print this help message and exit\n"
-    "  -V  Print version information and exit\n"
-    ;
-
 typedef struct Pair
 {
-    size_t lo;
-    size_t hi;
+    int lo;
+    int hi;
 } Pair;
 
-extern Pair modbinsearch(const int *array, size_t a_size, int value);
-extern int BinSearch_A(int N, int X[N], int T);
-extern int BinSearch_B(int N, int X[N], int T);
-extern int BinSearch_C(int N, int X[N], int T);
+extern Pair BinSearch_D(int N, const int X[N], int T);
+extern int BinSearch_A(int N, const int X[N], int T);
+extern int BinSearch_B(int N, const int X[N], int T);
+extern int BinSearch_C(int N, const int X[N], int T);
 
 #ifndef lint
 /* Prevent over-aggressive optimizers from eliminating ID string */
@@ -47,32 +40,9 @@ const char jlss_id_modbinsearch_c[] = "@(#)$Id$";
 #endif /* lint */
 
 /*
-** If value is found in the array (elements array[0]..array[a_size-1]),
-** then the returned Pair identifies the lowest index at which value is
-** found (in lo) and the highest value (in hi).  The values will be the
-** same if there's only one entry that matches.
-**
-** If value is not found in the array, then the index before (at) which
-** value should be inserted is returned in hi, and lo is set to hi+1.
-**
-** -- If the values in the array are unique, then this is not the binary
-**    search to use.
-** -- If it doesn't matter which one of multiple identical keys are
-**    returned, this is not the binary search to use.
-*/
-
-/*
-** The following description requires a different interpretation of the
-** rules - it is not compatible with the previous definition.
-**
-** Another way to look at this is:
-** -- Lower bound is largest  index lo such that a[lo] < value and a[lo+1] >= value
-** -- Upper bound is smallest index hi such that a[hi] > value and a[hi-1] <= value
-** -- Range is then lo+1..hi-1.
-*/
-
-/*
 ** Programming Pearls, 1st Edn, 8.3 Major Surgery - Binary Search
+**
+** In each fragment, P contains the result (0 indicates 'not found').
 **
 ** Search for T in X[1..N] - BinSearch-A
 **
@@ -118,7 +88,7 @@ const char jlss_id_modbinsearch_c[] = "@(#)$Id$";
 ** C implementations of these deal with X[0..N-1] instead of X[1..N]
 */
 
-int BinSearch_A(int N, int X[N], int T)
+int BinSearch_A(int N, const int X[N], int T)
 {
     int L = 0;
     int U = N-1;
@@ -138,7 +108,7 @@ int BinSearch_A(int N, int X[N], int T)
     assert(0);
 }
 
-int BinSearch_B(int N, int X[N], int T)
+int BinSearch_B(int N, const int X[N], int T)
 {
     int L = -1;
     int U = N;
@@ -158,7 +128,7 @@ int BinSearch_B(int N, int X[N], int T)
     return P;
 }
 
-int BinSearch_C(int N, int X[N], int T)
+int BinSearch_C(int N, const int X[N], int T)
 {
     int L = -1;
     int U = N;
@@ -178,9 +148,56 @@ int BinSearch_C(int N, int X[N], int T)
     return P;
 }
 
-Pair modbinsearch(const int *array, size_t a_size, int value)
+/*
+** If value is found in the array (elements array[0]..array[a_size-1]),
+** then the returned Pair identifies the lowest index at which value is
+** found (in lo) and the highest value (in hi).  The values will be the
+** same if there's only one entry that matches.
+**
+** If value is not found in the array, then the index before (at) which
+** value should be inserted is returned in hi, and lo is set to hi+1.
+**
+** -- If the values in the array are unique, then this is not the binary
+**    search to use.
+** -- If it doesn't matter which one of multiple identical keys are
+**    returned, this is not the binary search to use.
+*/
+
+/*
+** The following description requires a different interpretation of the
+** rules - it is not compatible with the previous definition.
+**
+** Another way to look at this is:
+** -- Lower bound is largest  index lo such that a[lo] < value and a[lo+1] >= value
+** -- Upper bound is smallest index hi such that a[hi] > value and a[hi-1] <= value
+** -- Range is then lo+1..hi-1.
+**
+** Let's review:
+** == Data: 2, 3, 3, 3, 5, 5, 6, 8 (N = 8)
+** Searches and results:
+** == 1 .. lo = -1, hi = 0   R = (0, -1) not found
+** == 2 .. lo = -1, hi = 1   R = (0,  0) found
+** == 3 .. lo =  0, hi = 4   R = (1,  3) found
+** == 4 .. lo =  3, hi = 4   R = (4,  3) not found
+** == 5 .. lo =  3, hi = 6   R = (4,  5) found
+** == 6 .. lo =  5, hi = 7   R = (6,  6) found
+** == 7 .. lo =  6, hi = 7   R = (7,  6) not found
+** == 8 .. lo =  6, hi = 8   R = (7,  7) found
+** == 9 .. lo =  7, hi = 8   R = (8,  7) not found
+*/
+
+/*
+** -- Can we do better than BinSearch_B() for lower bound followed by
+**    BinSearch_C() for upper bound, remembering to adjust range after
+**    finding lower bound?
+** -- Can we modify BinSearch_B() and BinSearch_C() to report where the
+**    insertion should occur?
+** -- How to deal with BinSearch_B() not reporting where the insertion
+**    should occur?
+*/
+
+Pair BinSearch_D(int a_size, const int array[a_size], int value)
 {
-    assert(a_size < SIZE_MAX);
     Pair result = { 1, 0 };
     /* Deal with edge cases */
     if (a_size == 0)
@@ -190,8 +207,8 @@ Pair modbinsearch(const int *array, size_t a_size, int value)
     else if (value > array[a_size  - 1])
         return (Pair) { .lo = a_size + 1, .hi = a_size };
 
-    size_t lo = 0;
-    size_t hi = a_size - 1;
+    int lo = 0;
+    int hi = a_size - 1;
 
     assert(array[lo] <= value && array[hi] >= value);
     /*
@@ -238,14 +255,15 @@ Pair modbinsearch(const int *array, size_t a_size, int value)
     return result;
 }
 
-static void check_sorted(int size, int array[size])
+static void check_sorted(const char *a_name, int size, int array[size])
 {
     bool ok = true;
     for (int i = 1; i < size; i++)
     {
         if (array[i-1] > array[i])
         {
-            fprintf(stderr, "Out of order: A[%d] = %d, A[%d] = %d\n", i-1, array[i-1], i, array[i]);
+            fprintf(stderr, "Out of order: %s[%d] = %d, %s[%d] = %d\n",
+                    a_name, i-1, array[i-1], a_name, i, array[i]);
             ok = false;
         }
     }
@@ -253,13 +271,13 @@ static void check_sorted(int size, int array[size])
         exit(1);
 }
 
-static void test_array(int size, int array[size])
+static void test_array(const char *a_name, int size, int array[size])
 {
     printf("Data: ");
     for (int i = 0; i < size; i++)
         printf(" %2d", array[i]);
     putchar('\n');
-    check_sorted(size, array);
+    check_sorted(a_name, size, array);
 
     for (int i = array[0] - 1; i < array[size - 1] + 2; i++)
     {
@@ -277,69 +295,52 @@ static void test_array(int size, int array[size])
 
 int main(int argc, char **argv)
 {
-    err_setarg0(argv[0]);
+    err_setarg0(argv[argc - argc]);
 
-    int opt;
-    while ((opt = getopt(argc, argv, optstr)) != -1)
+    int A[] = { 1, 2, 2, 4, 5, 5, 5, 5, 5, 6, 8, 8, 9, 10 };
+    enum { A_SIZE = sizeof(A) / sizeof(A[0]) };
+
+    test_array("A", A_SIZE, A);
+
+    int B[] = { 10, 12, 12, 12, 14, 15, 15, 15, 15, 15, 15, 15, 16, 16, 16, 18, 18, 18, 19, 19, 19, 19, };
+    enum { B_SIZE = sizeof(B) / sizeof(B[0]) };
+
+    test_array("B", B_SIZE, B);
+
+    int C[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
+    enum { C_SIZE = sizeof(C) / sizeof(C[0]) };
+
+    test_array("C", C_SIZE, C);
+
+    if (0)
     {
-        switch (opt)
+        /* Results for BinSearch_D() on array A */
+        static const struct Results
         {
-        case 'h':
-            err_help(usestr, hlpstr);
-            /*NOTREACHED*/
-        case 'V':
-            err_version("PROG", &"@(#)$Revision$ ($Date$)"[4]);
-            /*NOTREACHED*/
-        default:
-            err_usage(usestr);
-            /*NOTREACHED*/
+            int lo;
+            int hi;
+        } results[] =
+        {
+            {  1,  0 },
+            {  0,  0 },
+            {  1,  2 },
+            {  4,  3 },
+            {  3,  3 },
+            {  4,  8 },
+            {  9,  9 },
+            { 11, 10 },
+            { 10, 11 },
+            { 12, 12 },
+            { 13, 13 },
+            { 14, 13 },
+        };
+
+        for (int i = A[0] - 1; i < A[A_SIZE - 1] + 2; i++)
+        {
+            Pair result = BinSearch_D(A_SIZE, A, i);
+            printf("%2d: (%d, %d) vs (%d, %d)\n", i, result.lo, result.hi, results[i].lo, results[i].hi);
         }
     }
-
-    int a[] = { 1, 2, 2, 4, 5, 5, 5, 5, 5, 6, 8, 8, 9, 10 };
-    enum { A_SIZE = sizeof(a) / sizeof(a[0]) };
-
-    test_array(A_SIZE, a);
-
-    int b[] = { 10, 12, 12, 12, 14, 15, 15, 15, 15, 15, 15, 15, 16, 16, 16, 18, 18, 18, 19, 19, 19, 19, };
-    enum { B_SIZE = sizeof(b) / sizeof(b[0]) };
-
-    test_array(B_SIZE, b);
-
-    int c[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, };
-    enum { C_SIZE = sizeof(c) / sizeof(c[0]) };
-
-    test_array(C_SIZE, c);
-
-#if 0
-    static const struct Results
-    {
-        size_t lo;
-        size_t hi;
-    } results[] =
-    {
-        {  1,  0 },
-        {  0,  0 },
-        {  1,  2 },
-        {  4,  3 },
-        {  3,  3 },
-        {  4,  8 },
-        {  9,  9 },
-        { 11, 10 },
-        { 10, 11 },
-        { 12, 12 },
-        { 13, 13 },
-        { 14, 13 },
-    };
-#endif
-
-#if 0
-    for (int i = a[0] - 1; i < a[A_SIZE - 1] + 2; i++)
-    {
-        Pair result = modbinsearch(a, A_SIZE, i);
-        printf("%2d: (%zu, %zu) vs (%zu, %zu)\n", i, result.lo, result.hi, results[i].lo, results[i].hi);
-    }
-#endif
 
     return 0;
 }
