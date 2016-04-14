af29ee18bb0c3369058b79f9c824ffb975d8c5e0
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-04-13 23:22:19 -0700

    utf8-test.c: working - maybe the locale setting is not needed (to be tested)

--- utf8-test.c
+++ utf8-test.c
@@ -1,3 +1,4 @@
+#include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -10,11 +11,10 @@ int main(void)
     char *pat = "привет мир";
     char str[SIZE + 2];
 
-    FILE *f1;
-    FILE *f2;
+    setlocale(LC_ALL, "");
 
-    f1 = fopen("вход", "r");
-    f2 = fopen("выход", "w");
+    FILE *f1 = fopen("вход", "r");
+    FILE *f2 = fopen("выход", "w");
     if (f1 == 0 || f2 == 0)
     {
         fprintf(stderr, "Failed to open one or both files\n");
@@ -24,12 +24,19 @@ int main(void)
     size_t nbytes;
     if ((nbytes = fread(buf, 1, SIZE, f1)) > 0)
     {
-        buf[nbytes] = 0;
-
+        buf[nbytes] = '\0';
+        buf[strcspn(buf, "\n")] = '\0';
         if (strncmp(buf, pat, nbytes) == 0)
         {
             sprintf(str, "%.*s\n", (int)nbytes, buf);
             fwrite(str, 1, nbytes, f2);
+            printf("Buffer and pattern are equal: [%s]\n", buf);
+        }
+        else
+        {
+            printf("Buffer and pattern are different\n");
+            printf("Buffer:  [%s]\n", buf);
+            printf("Pattern: [%s]\n", pat);
         }
     }
 
