4233ab6609e5b29d1e3840e66798814cf16031e8
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-04-17 11:08:20 -0700

    so.36668295.c: remove comments, use fgets()

--- postfix-to-infix/so.36668295.c
+++ postfix-to-infix/so.36668295.c
@@ -1,139 +1,119 @@
 #include <stdio.h>
-//#include <conio.h>
 #include  <stdlib.h>
 #include  <string.h>
 
 #define SIZE 30
 typedef struct fix_tag
 {
-    char s[SIZE][SIZE], dest[SIZE] ;
-    char t1[2], t2[2] ;
-    char ch1[SIZE], ch2[SIZE], ch3[SIZE] ;
-    int i, top ;
-}postfix ;
+    char s[SIZE][SIZE], dest[SIZE];
+    char t1[2], t2[2];
+    char ch1[SIZE], ch2[SIZE], ch3[SIZE];
+    int i, top;
+} postfix;
 
-/********** Function Declaration begins **********/
-void initialization ( postfix * ) ;
-void expression ( postfix *, char * ) ;
-void push ( postfix *, char * ) ;
-void pop ( postfix *, char * ) ;
-void trans ( postfix * ) ;
-void display ( postfix ) ;
-/********** Function Declaration ends **********/
+void initialization(postfix *);
+void expression(postfix *, char *);
+void push(postfix *, char *);
+void pop(postfix *, char *);
+void trans(postfix *);
+void display(postfix);
 
 int main(void)
 {
-    postfix PF ;
-    char exp[SIZE] ;
-    //clrscr() ;
+    postfix PF;
+    char exp[SIZE];
 
-    initialization ( &PF ) ;
+    initialization(&PF);
 
-    printf ( "\n\t\t Enter any Postfix expression :\n " ) ;
-    gets ( exp ) ;
+    printf("\n\t\t Enter any Postfix expression :\n ");
+    if (fgets(exp, sizeof(exp), stdin) == 0)
+        return 1;
 
-    expression ( &PF, exp ) ;
-    trans ( &PF ) ;
+    expression(&PF, exp);
+    trans(&PF);
 
-    printf ( "\nThe resultant infix expression is: " ) ;
-    display ( PF ) ;
-
-    //getch() ;
+    printf("\nThe resultant infix expression is: ");
+    display(PF);
 }
 
-
-/********** Initialization of Strucutre variable **********/
-/********** Function Definition begins **********/
-void initialization ( postfix *p )
+void initialization(postfix *p)
 {
-    p -> i = 0 ;
-    p -> top = -1 ;
-    strcpy ( p -> dest, "") ;
+    p->i = 0;
+    p->top = -1;
+    strcpy(p->dest, "");
 }
-/********** Function Definition ends **********/
 
-
-/********** String Copy **********/
-/********** Function Definition begins **********/
-void expression ( postfix *p, char *c )
+void expression(postfix *p, char *c)
 {
-    strcpy ( p -> dest, c ) ;
+    strcpy(p->dest, c);
 }
-/********** Function Definition ends **********/
 
-
-/********** Pushing expression **********/
-/********** Function Definition begins **********/
-void push ( postfix *p, char *str )
+void push(postfix *p, char *str)
 {
-    if ( p -> top == SIZE - 1 )
-    printf ( "\nStack Overflow." ) ;
-    else{
-        p -> top++ ;
-        strcpy ( p -> s[p -> top], str ) ;
+    if (p->top == SIZE - 1)
+        printf("\nStack Overflow.");
+    else
+    {
+        p->top++;
+        strcpy(p->s[p->top], str);
     }
 }
-/********** Function Definition ends **********/
 
-
-/********** Popping expression **********/
-/********** Function Definition begins **********/
-void pop ( postfix *p, char *a )
+void pop(postfix *p, char *a)
 {
-    if ( p -> top == -1 )
-    printf ( "\nStack is empty." ) ;
-    else{
-    strcpy ( a, p -> s[p -> top] ) ;
-    p -> top-- ;
+    if (p->top == -1)
+        printf("\nStack is empty.");
+    else
+    {
+        strcpy(a, p->s[p->top]);
+        p->top--;
     }
 }
-/********** Function Definition ends **********/
 
-
-/********** Postfix to Infix **********/
-/********** Function Definition begins **********/
-void trans ( postfix *p ){
-    while ( p -> dest[p -> i] ){
-        if( p -> dest[p -> i] == ' ' ){
-            p -> i++ ;
+void trans(postfix *p)
+{
+    while (p->dest[p->i])
+    {
+        if (p->dest[p->i] == ' ')
+        {
+            p->i++;
         }
-        if ( p -> dest[p -> i] == '%' || p -> dest[p -> i] == '*' ||
-            p -> dest[p -> i] == '-' || p -> dest[p -> i] == '+' ||
-            p -> dest[p -> i] == '/' || p -> dest[p -> i] == '$' ){
+        if (p->dest[p->i] == '%' || p->dest[p->i] == '*' ||
+            p->dest[p->i] == '-' || p->dest[p->i] == '+' ||
+            p->dest[p->i] == '/' || p->dest[p->i] == '$')
+        {
+            pop(p, p->ch2);
+            pop(p, p->ch3);
 
-            pop ( p, p -> ch2 ) ;
-            pop ( p, p -> ch3 ) ;
+            p->t1[0] = p->dest[p->i];
+            p->t1[1] = '\0';
+            strcpy(p->ch1, "(");
+            strcat(p->ch1, p->ch3);
+            strcat(p->ch1, p->t1);
+            strcat(p->ch1, p->ch2);
+            strcat(p->ch1, ")");
 
-            p -> t1[0] = p -> dest[p -> i] ;
-            p -> t1[1] = '\0' ;
-            strcpy (p -> ch1, "(");
-
-            strcat ( p -> ch1, p -> ch3 ) ;
-            strcat ( p -> ch1, p -> t1 ) ;
-            strcat ( p -> ch1, p -> ch2 ) ;
-
-            strcat ( p -> ch1, ")" ) ;
-
-            push ( p, p -> ch1 ) ;
-        }else{
-            p -> t1[0] = p -> dest[p -> i] ;
-            p -> t1[1] = '\0' ;
-            strcpy ( p -> t2, p -> t1 ) ;
-            push ( p, p -> t2 ) ;
+            push(p, p->ch1);
         }
-        p -> i++ ;
+        else
+        {
+            p->t1[0] = p->dest[p->i];
+            p->t1[1] = '\0';
+            strcpy(p->t2, p->t1);
+            push(p, p->t2);
+        }
+        p->i++;
     }
 }
-/********** Function Definition ends **********/
 
-
-/********** Displaying expression **********/
-/********** Function Definition begins **********/
-void display ( postfix p ){
-    char *t ;
-    t = p.s[0] ;
-    while ( *t ){
-        printf ( "%c ", *t ) ;
-        t++ ;
+void display(postfix p)
+{
+    char *t;
+    t = p.s[0];
+    while (*t)
+    {
+        printf("%c ", *t);
+        t++;
     }
+    putchar('\n');
 }
