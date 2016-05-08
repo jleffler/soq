8866f185a6bd812e48e50fda7563a5c15be5c4fc
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-05-08 11:06:35 -0700

    pthread-37.c: add error checking - and discover that Mac OS X generates error 78 Function not implemented for sem_init().  Code works OK when run on Linux.

--- pthread-37.c
+++ pthread-37.c
@@ -1,10 +1,13 @@
 /* SO 37092445 */
+#include "posixver.h"
 /* Pragma needed on Mac OS X to suppress warnings about sem_init() and sem_destroy() */
 #pragma GCC diagnostic ignored "-Wdeprecated-declarations"
-#include <stdio.h>
+#include "stderr.h"
+#include <errno.h>
 #include <pthread.h>
-#include <stdlib.h>
 #include <semaphore.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 static void *thread_function(void *arg);
 
@@ -28,16 +31,23 @@ void *thread_function(void *arg)
     nanosleep(&sleep_time, NULL);
 
     if (pack->sem1)
-        sem_wait(pack->sem1);   // wait for predecessor
+    {
+        if (sem_wait(pack->sem1) != 0)   // wait for predecessor
+            err_syserr("sem_wait(): thread %s\n", pack->name);
+    }
     printf("\n%s\n", pack->name);
-    fflush(stdout);
+    //fflush(stdout);
     if (pack->sem2)
-        sem_post(pack->sem2);   // unblock successor
+    {
+        if (sem_post(pack->sem2) != 0)   // unblock successor
+            err_syserr("sem_post(): thread %s\n", pack->name);
+    }
     return NULL;
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+    err_setarg0(argv[argc-argc]);
     srandom((unsigned int)time(NULL));
 
     pthread_t threadA, threadB, threadC, threadD;
@@ -46,30 +56,55 @@ int main(void)
     sem_t sem_C_D;
     sem_t sem_B_C;
 
-    sem_init(&sem_A_B, 0, 0);
-    sem_init(&sem_C_D, 0, 0);
-    sem_init(&sem_B_C, 0, 0);
+    if (sem_init(&sem_A_B, 0, 0) != 0 ||
+        sem_init(&sem_C_D, 0, 0) != 0 ||
+        sem_init(&sem_B_C, 0, 0))
+        err_syserr("sem_init()");
 
     struct sem_package pack1 = { NULL,     &sem_A_B, "A" };
     struct sem_package pack2 = { &sem_A_B, &sem_B_C, "B" };
     struct sem_package pack3 = { &sem_B_C, &sem_C_D, "C" };
     struct sem_package pack4 = { &sem_C_D, NULL,     "D" };
 
-    pthread_create(&threadA, NULL, thread_function, &pack1);
-    pthread_create(&threadB, NULL, thread_function, &pack2);
-    pthread_create(&threadC, NULL, thread_function, &pack3);
-    pthread_create(&threadD, NULL, thread_function, &pack4);
+    int ptherr;
+    if ((ptherr = pthread_create(&threadA, NULL, thread_function, &pack1)) != 0 ||
+        (ptherr = pthread_create(&threadB, NULL, thread_function, &pack2)) != 0 ||
+        (ptherr = pthread_create(&threadC, NULL, thread_function, &pack3)) != 0 ||
+        (ptherr = pthread_create(&threadD, NULL, thread_function, &pack4)) != 0)
+    {
+        errno = ptherr;
+        err_syserr("pthread_create()");
+    }
 
     void *vp;
-    pthread_join(threadD, &vp);
+    if ((ptherr = pthread_join(threadD, &vp)) != 0)
+    {
+        errno = ptherr;
+        err_sysrem("pthread_join()");
+    }
 
-    sem_destroy(&sem_A_B);
-    sem_destroy(&sem_B_C);
-    sem_destroy(&sem_C_D);
+    if (sem_destroy(&sem_A_B) != 0 ||
+        sem_destroy(&sem_B_C) != 0 ||
+        sem_destroy(&sem_C_D))
+        err_syserr("sem_destroy()");
 
-    pthread_detach(threadA);
-    pthread_detach(threadB);
-    pthread_detach(threadC);
+    if ((ptherr = pthread_detach(threadA)) != 0)
+    {
+        errno = ptherr;
+        err_sysrem("pthread_detach() thread %s\n", "A");
+    }
+
+    if ((ptherr = pthread_detach(threadB)) != 0)
+    {
+        errno = ptherr;
+        err_sysrem("pthread_detach() thread %s\n", "B");
+    }
+
+    if ((ptherr = pthread_detach(threadC)) != 0)
+    {
+        errno = ptherr;
+        err_sysrem("pthread_detach() thread %s\n", "C");
+    }
 
     printf("\nAll done\n\n");
     return 0;
