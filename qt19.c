1f339d82c3025344452ef7e4a9cc3b8509b99f32
Jonathan Leffler <jleffler@us.ibm.com>
2016-06-02 07:38:07 -0700

    SO 37577522: qt19.c Add data reading code, and memory freeing code. Observe that quadtree_insert() retains a pointer to the particle that is being inserted. Do not free particles that were not allocated. Add debugging code for printing structures. Code is leak-free per valgrind (but there are some new system memory in-use-at-exit suppressions). Code is still fragile - it assumes that the data is within ±100 or the origin and does not validate this (reinstate X_MIN, X_MAX, etc?). More generalized coding would have to be configurable for size/location. There would probably be merit in encapsulating centre + half-width in a structure. Non-square regions would require width and height.

--- qt19.c
+++ qt19.c
@@ -4,6 +4,8 @@
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include "stderr.h"
+#include "emalloc.h"
 
 static const double CTR_X =    0.0;
 static const double CTR_Y =    0.0;
@@ -33,6 +35,65 @@ struct Node
     Node     *ne;
 };
 
+/* Debugging/diagnostic code */
+
+static void print_particle(int i, const Particle *p)
+{
+    printf("# Particle %d: (%6.2f,%6.2f) M %6.2f V (%6.2f,%6.2f)\n",
+           i, p->x_pos, p->y_pos, p->mass, p->x_vel, p->y_vel);
+}
+
+static inline void print_quadtree_info(const char *tag, const Node *n)
+{
+    printf("%s: C (%6.2f,%6.2f) W %6.2f\n", tag, n->center_x, n->center_y, n->width);
+    if (n->p)
+        print_particle(-1, n->p);
+}
+
+static inline void print_quadtree_node(Node *n)
+{
+    print_quadtree_info("Box", n);
+    if (n->p)
+        print_particle(0, n->p);
+    if (n->nw)
+        print_quadtree_info(" NW", n->nw);
+    if (n->sw)
+        print_quadtree_info(" SW", n->sw);
+    if (n->se)
+        print_quadtree_info(" SE", n->se);
+    if (n->ne)
+        print_quadtree_info(" NE", n->ne);
+}
+
+static inline void print_quadtree_details(const char *tag, Node *n, int d)
+{
+    static const char indent[] =
+        "                                                            ";
+    assert(d < 15);
+    const char *prefix = indent + sizeof(indent) - d * 3;
+    printf("%s%s Centre (%6.2f,%6.2f) W %6.2f P %p\n",
+           prefix, tag, n->center_x, n->center_y, n->width, (void *)n);
+    if (n->p != 0)
+        printf("%s%s Point  (%6.2f,%6.2f)\n",
+              prefix, tag, n->p->x_pos, n->p->y_pos);
+    if (n->nw != 0)
+        print_quadtree_details("NW:", n->nw, d+1);
+    if (n->sw != 0)
+        print_quadtree_details("SW:", n->sw, d+1);
+    if (n->se != 0)
+        print_quadtree_details("SE:", n->se, d+1);
+    if (n->ne != 0)
+        print_quadtree_details("NE:", n->ne, d+1);
+}
+
+static inline void print_quadtree(Node *n)
+{
+    assert(n != 0);
+    printf("QT Node %p\n", (void *)n);
+    print_quadtree_details("===", n, 0);
+}
+/* End Debugging/Diagnostic code */
+
 static inline bool in_box(const Particle *p, double center_x, double center_y, double width)
 {
     return((p->x_pos >= center_x - width && p->x_pos < center_x + width) &&
@@ -41,22 +102,17 @@ static inline bool in_box(const Particle *p, double center_x, double center_y, d
 
 static void check_in_box(const Particle *p, double center_x, double center_y, double width)
 {
-        if (!in_box(p, center_x, center_y, width))
-        {
-            fprintf(stderr, "Point (%6.2f,%6.2f) is outside rectangle (%6.2f,%6.2f) W %6.2f\n",
-                    p->x_pos, p->y_pos, center_x, center_y, width);
-            exit(EXIT_FAILURE);
-        }
+    if (!in_box(p, center_x, center_y, width))
+    {
+        fprintf(stderr, "Point (%6.2f,%6.2f) is outside rectangle (%6.2f,%6.2f) W %6.2f\n",
+                p->x_pos, p->y_pos, center_x, center_y, width);
+        exit(EXIT_FAILURE);
+    }
 }
 
 static Node *make_node(double center_x, double center_y, double width)
 {
-    Node *n = malloc(sizeof(*n));
-    if (n == 0)
-    {
-        fprintf(stderr, "Failed to allocate %zu bytes\n", sizeof(*n));
-        exit(EXIT_FAILURE);
-    }
+    Node *n = MALLOC(sizeof(*n));
     n->center_x = center_x;
     n->center_y = center_y;
     n->width = width;
@@ -101,7 +157,13 @@ static void split_node(Node *n)
     else if (in_box(p, n->ne->center_x, n->ne->center_y, n->ne->width))
         n->ne->p = p;
     else
+    {
+        printf("*** Assert firing!\n");
+        print_particle(-2, p);
+        print_quadtree_node(n);
         assert(0);  /* Can't happen! */
+        abort();
+    }
     n->p = 0;
     check_node(n);
 }
@@ -117,7 +179,6 @@ static Node *quadtree_insert(Node *n, struct Particle *p, double center_x, doubl
     {
         n = make_node(center_x, center_y, width);
         n->p = p;
-        check_node(n);
     }
     else if (n->nw == 0 && n->p == 0)
     {
@@ -130,6 +191,7 @@ static Node *quadtree_insert(Node *n, struct Particle *p, double center_x, doubl
             /* Need to split node into four and insert particle into correct node */
             assert(n->p != 0);
             split_node(n);
+            //print_quadtree_node(n);
         }
 
         double new_width = width * 0.5;
@@ -166,57 +228,41 @@ static Node *quadtree_insert(Node *n, struct Particle *p, double center_x, doubl
     return n;
 }
 
-static void print_quadtree_details(const char *tag, Node *n, int d)
+static void free_quadtree(Node *n, bool free_particles)
 {
-    static const char indent[] =
-        "                                                            ";
-    assert(d < 15);
-    const char *prefix = indent + sizeof(indent) - d * 3;
-    printf("%s%s Centre (%6.2f,%6.2f) W %6.2f P %p\n",
-           prefix, tag, n->center_x, n->center_y, n->width, (void *)n);
-    if (n->p != 0)
-        printf("%s%s Point  (%6.2f,%6.2f)\n",
-              prefix, tag, n->p->x_pos, n->p->y_pos);
-    if (n->nw != 0)
-        print_quadtree_details("NW:", n->nw, d+1);
-    if (n->sw != 0)
-        print_quadtree_details("SW:", n->sw, d+1);
-    if (n->se != 0)
-        print_quadtree_details("SE:", n->se, d+1);
-    if (n->ne != 0)
-        print_quadtree_details("NE:", n->ne, d+1);
+    if (n != 0)
+    {
+        if (free_particles)
+            free(n->p);
+        free_quadtree(n->nw, free_particles);
+        free_quadtree(n->sw, free_particles);
+        free_quadtree(n->se, free_particles);
+        free_quadtree(n->ne, free_particles);
+        free(n);
+    }
 }
 
-static void print_quadtree(Node *n)
+/* Test code */
+
+static void built_in(void)
 {
-    assert(n != 0);
-    printf("QT Node %p\n", (void *)n);
-    print_quadtree_details("===", n, 0);
-}
-
-/*
-   random -n 10 -T '    { %6:2[1:20]f, %6:2[-100:100]f, %6:2[-100:100]f, %6:2[-100:100]f, %6:2[-100:100]f },'
- */
-
-static struct Particle particles[] =
-{
-    {  19.99,  96.07,  62.79, -99.46,  19.70 },
-    {  12.94,   1.43, -33.45,  31.80, -66.08 },
-    {   6.49,  16.99, -20.83,  92.51,  35.98 },
-    {  17.01, -28.85, -94.10,  42.82,  -1.30 },
-    {  14.27,  85.07,  88.21,  11.22,  16.85 },
-    {  15.73, -56.37,  46.85,  27.40, -15.15 },
-    {   1.53, -49.44, -64.27, -29.45, -38.25 },
-    {   8.03,  92.11, -47.50,  63.77, -29.99 },
-    {   8.67, -99.81,  73.19,  18.75,  88.66 },
-    {  16.36,  66.33,  14.23,  87.65,  40.01 },
-};
-
-enum { nParticles = sizeof(particles) / sizeof(particles[0]) };
-
-int main(void)
-{
+    /* random -n 10 -T '    { %6:2[1:20]f, %6:2[-100:100]f, %6:2[-100:100]f, %6:2[-100:100]f, %6:2[-100:100]f },' */
+    static struct Particle particles[] =
+    {
+        {  19.99,  96.07,  62.79, -99.46,  19.70 },
+        {  12.94,   1.43, -33.45,  31.80, -66.08 },
+        {   6.49,  16.99, -20.83,  92.51,  35.98 },
+        {  17.01, -28.85, -94.10,  42.82,  -1.30 },
+        {  14.27,  85.07,  88.21,  11.22,  16.85 },
+        {  15.73, -56.37,  46.85,  27.40, -15.15 },
+        {   1.53, -49.44, -64.27, -29.45, -38.25 },
+        {   8.03,  92.11, -47.50,  63.77, -29.99 },
+        {   8.67, -99.81,  73.19,  18.75,  88.66 },
+        {  16.36,  66.33,  14.23,  87.65,  40.01 },
+    };
+    enum { nParticles = sizeof(particles) / sizeof(particles[0]) };
     Node *root = NULL;
+
     printf("# Particle 0: ");
     root = quadtree_insert(root, &particles[0], CTR_X, CTR_Y, WIDTH);
     print_quadtree(root);
@@ -227,6 +273,70 @@ int main(void)
         Node *tree = quadtree_insert(root, &particles[i], CTR_X, CTR_Y, WIDTH);
         assert(tree == root);
         print_quadtree(root);
+    }
+    free_quadtree(root, false); // Do not free particles
+}
+
+static int read_particle(FILE *fp, Particle *p)
+{
+    char  *buffer = 0;
+    size_t buflen = 0;
+    while (getline(&buffer, &buflen, fp) != -1)
+    {
+        /* Skip empty lines (newline only) and comment lines (# in column 1) */
+        if (buffer[0] == '#' || buffer[0] == '\n')
+            continue;
+        if (sscanf(buffer, "%lf %lf %lf %lf %lf",
+                   &p->mass, &p->x_pos, &p->y_pos, &p->x_vel, &p->y_vel) == 5)
+        {
+            free(buffer);
+            return 0;
+        }
+        err_error("Failed to extract 5 numbers from line:\n%s", buffer);
+    }
+    free(buffer);
+    return EOF;
+}
+
+static void read_from_file(const char *file)
+{
+    printf("Data from: %s\n", file);
+    FILE *fp = fopen(file, "r");
+    if (fp == 0)
+        err_syserr("Failed to open file %s: ", file);
+    Node *root = NULL;
+    Particle particle;
+
+    for (int i = 0; read_particle(fp, &particle) != EOF; i++)
+    {
+        Particle *p = MALLOC(sizeof(*p));
+        *p = particle;
+        //print_particle(i, p);
+        Node *tree = quadtree_insert(root, p, CTR_X, CTR_Y, WIDTH);
+        if (root == NULL)
+            root = tree;
+        assert(tree == root);
+        //print_quadtree(root);
+    }
+
+    print_quadtree(root);
+    free_quadtree(root, true);  // Free particles too
+
+    fclose(fp);
+}
+
+int main(int argc, char **argv)
+{
+    err_setarg0(argv[0]);
+    if (argc == 1)
+    {
+        printf("Built-in Data:\n");
+        built_in();
+    }
+    else
+    {
+        for (int i = 1; i < argc; i++)
+            read_from_file(argv[i]);
     }
     return 0;
 }
