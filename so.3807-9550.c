4d8c2ead298232d5c659a1fb85ce3cf19f6d59f1
Jonathan Leffler <jleffler@us.ibm.com>
2016-06-28 22:58:44 -0700

    SO 3807-9550: seems to be working OK on comprehensive tests with nothing invalid

--- so.3807-9550.c
+++ so.3807-9550.c
@@ -19,13 +19,6 @@ struct Link
 
 typedef struct Link Link;
 
-static inline void changePos(Link** anchor_link, char* name1, int pos)
-{
-    assert(anchor_link != 0);
-    assert(name1 != 0);
-    assert(pos >= 0);
-}
-
 static void print_list(Link *root)
 {
     const char *pad = "";
@@ -35,6 +28,47 @@ static void print_list(Link *root)
         root = root->next;
         pad = " -> ";
     }
+}
+
+static inline void changePos(Link **anchor_link, const char *name, int pos)
+{
+    assert(anchor_link != 0);
+    assert(name != 0);
+    assert(pos >= 0);
+    Link *root = *anchor_link;
+    Link *link = root;
+    Link *prev = 0;
+    int count = 0;
+    while (link != 0 && strcmp(link->frame->name, name) != 0)
+    {
+        prev = link;
+        link = link->next;
+        count++;
+    }
+    if (link == 0)      // Name not found - no swap!
+        return;
+    if (count == pos)   // Already in target position - no swap
+        return;
+    if (count == 0)     // Moving first item; update root
+    {
+        assert(link == root);
+        *anchor_link = root->next;
+        root = *anchor_link;
+    }
+    if (prev != 0)
+        prev->next = link->next;
+    // node is detached; now where does it go?
+    if (pos == 0)       // Move to start - update root
+    {
+        link->next = root;
+        *anchor_link = link;
+        return;
+    }
+    Link *node = root;
+    for (int i = 0; i < pos - 1 && node->next != 0; i++)
+        node = node->next;
+    link->next = node->next;
+    node->next = link;
 }
 
 static void free_frame(Frame *frame)
@@ -79,6 +113,22 @@ static Link *make_link(const char *name, unsigned int number)
     return link;
 }
 
+static Link *make_list(int num, Frame *frames)
+{
+    Link *head = 0;
+    Link *tail = 0;
+    for (int k = 0; k < num; k++)
+    {
+        Link *link = make_link(frames[k].name, frames[k].duration);
+        if (head == 0)
+            head = link;
+        if (tail != 0)
+            tail->next = link;
+        tail = link;
+    }
+    return head;
+}
+
 int main(void)
 {
     Frame frames[] =
@@ -94,23 +144,12 @@ int main(void)
     {
         for (int j = 0; j < NUM_FRAMES; j++)
         {
-            Link *head = 0;
-            Link *tail = 0;
-            for (int k = 0; k < NUM_FRAMES; k++)
-            {
-                Link *link = make_link(frames[k].name, frames[k].duration);
-                if (head == 0)
-                    head = link;
-                if (tail != 0)
-                    tail->next = link;
-                tail = link;
-            }
+            Link *head = make_list(NUM_FRAMES, frames);
             print_list(head);
             printf(" == %s to %u == ", frames[i].name, j);
             changePos(&head, frames[i].name, j);
             print_list(head);
             putchar('\n');
-
             free_link(head);
         }
     }
