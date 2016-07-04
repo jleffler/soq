111c67c373617fddd6056c8018bbaa504c5a056f
Jonathan Leffler <jleffler@us.ibm.com>
2016-07-04 06:18:13 -0700

    Add material from branch so/12762944.  Improve cleanup rules.
    Add OUTFILES macro to list of removed files.  Set per makefile, it
    allows a directory to clean up files that might have been generated by
    running the programs.

--- etc/soq-tail.mk
+++ etc/soq-tail.mk
@@ -3,8 +3,10 @@
 # Provides target 'clean' to do sensible cleaning.
 # Relies on user to set macro PROGRAMS (plural) for removing programs.
 # Assumes etc/soq-head.mk is already included.
+# OUTFILES is set per makefile to hold output file names that should be
+# cleaned.
 
 clean:
-	${RM_F} *.o *.a ${DEBRIS}
+	${RM_F} *.o *.a ${DEBRIS} ${OUTFILES}
 	${RM_F} ${PROGRAMS}
 	${RM_FR} *.dSYM
