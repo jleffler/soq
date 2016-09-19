6a1a90d0da439996373c477b6f8a523e6a80fa6a
Jonathan Leffler <jonathan.leffler@gmail.com>
2016-09-18 22:53:57 -0700

    SO 3956-4651: Code size with multiple arrow operators

---
+++ src/so-3956-4651/node3.c
@@ -0,0 +1,17 @@
+#include "node.h"
+
+
+DataNode * findPrevMatching(int x, int y)
+{   
+    DataNode *next = head->next;
+    Data *data = next->data;
+
+    while (!(data->x == x && data->y == y))
+    {
+        head = head->next;
+        next = head->next;
+        data = next->data;
+    }
+
+    return head;
+}
