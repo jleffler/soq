947178d3ad057d2b004d8e35273aa7f33687fc0e
Jonathan Leffler <jleffler@us.ibm.com>
2016-10-03 16:14:20 -0700

    GCC Bug chase: transplant body of read_header() into main()

--- src/so-3982-4536/gccbug.c
+++ src/so-3982-4536/gccbug.c
@@ -17,8 +17,9 @@ typedef struct header
 static void err_format(const char *tag, const char *buffer);
 static void err_error(const char *msg);
 
-static void read_header(header *hdr)
+int main(void)
 {
+    header  hdr;
     char   *buffer = 0;
     size_t  buflen = 0;
     char    delim;
@@ -27,27 +28,20 @@ static void read_header(header *hdr)
         err_error("Failed to read header line");
     buffer[strcspn(buffer, "\n")] = '\t';
     int offset = 0;
-    if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr->r_name, &delim, &offset) != 2)
+    if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr.r_name, &delim, &offset) != 2)
         err_format("header row name", buffer);
 
-    printf("    O[%2d]      N[%s] R[%s]\n", offset, hdr->r_name, buffer + offset);
+    printf("    O[%2d]      N[%s] R[%s]\n", offset, hdr.r_name, buffer + offset);
     for (int i = 0; i < MAX_VALUES; i++)
     {
         int extra;
-        if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr->c_name[i], &delim, &extra) != 2)
+        if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr.c_name[i], &delim, &extra) != 2)
             err_format("header column name", buffer);
-
         offset += extra;
-        printf("%.2d: O[%2d] E[%d] N[%s] R[%s]\n", i, offset, extra, hdr->c_name[i], buffer + offset);
+        printf("%.2d: O[%2d] E[%d] N[%s] R[%s]\n", i, offset, extra, hdr.c_name[i], buffer + offset);
         assert(!isspace(buffer[offset]));
     }
     free(buffer);
-}
-
-int main(void)
-{
-    header  hdr_1;
-    read_header(&hdr_1);
     return 0;
 }
 
