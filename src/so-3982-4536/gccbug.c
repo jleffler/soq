d87b74a5aa7e9f02579ecb0d476b45922cc0fade
Jonathan Leffler <jleffler@us.ibm.com>
2016-10-04 09:05:17 -0700

    GCC Bug Chase: remove stdlib.h.
    Now close to minimal.  Should the code simplify the sscanf formats?  Would eliminating delim eliminate the problem?

--- src/so-3982-4536/gccbug.c
+++ src/so-3982-4536/gccbug.c
@@ -1,17 +1,14 @@
 /* SO 3982-4536 */
 #include <stdio.h>
-#include <stdlib.h>
 
-#define MAX_VALUES  4
-#define MAX_NAMELEN 32
+enum { MAX_VALUES = 4 };
+enum { MAX_NAMELEN = 32 };
 
 typedef struct header
 {
     char    r_name[MAX_NAMELEN];
     char    c_name[MAX_VALUES][MAX_NAMELEN];
 } header;
-
-static void err_format(const char *tag, const char *buffer);
 
 int main(void)
 {
@@ -25,23 +22,23 @@ int main(void)
         return 1;
     buffer[length-1] = '\t';
     int offset = 0;
-    if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr.r_name, &delim, &offset) != 2)
-        err_format("header row name", buffer);
 
+#ifndef SKIP_FIRST_SSCANF
+    if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr.r_name, &delim, &offset) != 2)
+        return 1;
     printf("    O[%2d]      N[%s] R[%s]\n", offset, hdr.r_name, buffer + offset);
+#endif
+
     for (int i = 0; i < MAX_VALUES; i++)
     {
         int extra;
         if (sscanf(buffer + offset, "%31[^\t\n\r]%1[\t\n\r]%n", hdr.c_name[i], &delim, &extra) != 2)
-            err_format("header column name", buffer);
+        {
+            fprintf(stderr, "sscanf() failed (field %d, offset %d) at [%s]\n", i, offset, buffer + offset);
+            return 1;
+        }
         offset += extra;
         printf("%.2d: O[%2d] E[%d] N[%s] R[%s]\n", i, offset, extra, hdr.c_name[i], buffer + offset);
     }
     return 0;
-}
-
-static void err_format(const char *tag, const char *buffer)
-{
-    fprintf(stderr, "Format error for %s in line [%s]\n", tag, buffer);
-    exit(1);
 }
