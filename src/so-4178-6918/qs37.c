3ddf4fc30722b9c85da58ddcb0efa38afe39be46
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-01-23 19:19:30 -0800

    SO 4178-6918: Add qs37.c based on code in Q (loosely)

---
+++ src/so-4178-6918/qs37.c
@@ -0,0 +1,149 @@
+#include <stdbool.h>
+#include <stdio.h>
+
+/*
+**  Amended pseudo-code from SO 4178-6918
+**
+**  Partition(A,p,r)
+**  {
+**      x <- A[p]
+**      i <- p-1
+**      j <- r+1
+**      while (True) {
+**          repeat
+**              j <- j-1
+**          until (A[j] <= x)
+**          repeat
+**              i <- i+1
+**          until (A[i] >= x)
+**          if (i A[j]
+**          else
+**              return(j)
+**      }
+**  }
+**
+**  Wikipedia says (Lomuto Partitioning):
+**
+**  algorithm quicksort(A, lo, hi) is
+**      if lo < hi then
+**          p := partition(A, lo, hi)
+**          quicksort(A, lo, p – 1)
+**          quicksort(A, p + 1, hi)
+**
+**  algorithm partition(A, lo, hi) is
+**      pivot := A[hi]
+**      i := lo     // place for swapping
+**      for j := lo to hi – 1 do
+**          if A[j] ≤ pivot then
+**              swap A[i] with A[j]
+**          i := i + 1
+**      swap A[i] with A[hi]
+**      return i
+**
+**  Wikipedia says (Hoare Partitioning):
+**
+**  algorithm quicksort(A, lo, hi) is
+**      if lo < hi then
+**          p := partition(A, lo, hi)
+**          quicksort(A, lo, p)
+**          quicksort(A, p + 1, hi)
+**
+**  algorithm partition(A, lo, hi) is
+**      pivot := A[lo]
+**      i := lo - 1
+**      j := hi + 1
+**      loop forever
+**          do
+**              i := i + 1
+**          while A[i] < pivot
+**          do
+**              j := j - 1
+**          while A[j] > pivot
+**          if i >= j then
+**              return j
+**          swap A[i] with A[j]
+**
+**  Note difference between Lomuto and Hoare in post-partition boundaries!
+*/
+
+static void dump_data(const char *tag, int *data, int start, int end)
+{
+    printf("%s (%d):", tag, end - start + 1);
+    for (int i = start; i <= end; i++)
+    {
+        printf(" %d", data[i]);
+    }
+    putchar('\n');
+}
+
+static void swap_ints(int *A, int i, int j)
+{
+    int t = A[i];
+    A[i] = A[j];
+    A[j] = t;
+}
+
+static int partition(int a[], int start, int end)
+{
+    int pivot = a[start]; // select first elem as the pivot
+    printf("-->> P(%d,%d) - pivot %d\n", start, end, pivot);
+    int i = start - 1;      // point i and j to before/after start end of the arr
+    int j = end + 1;
+
+    while (true)
+    {
+        //printf("---- a[%d]=%d, a[%d]=%d\n", i, a[i], j, a[j]);
+        do
+        {
+            j--;
+            printf("---- j a[%d] = %d\n", j, a[j]);
+        } while (a[j] > pivot);
+        do
+        {
+            i++;
+            printf("---- i a[%d] = %d\n", i, a[i]);
+        } while (a[i] < pivot);
+        if (i >= j)
+            break;
+        printf("-<>- a[%d]=%d, a[%d]=%d\n", j, a[j], i, a[i]);
+        swap_ints(a, i, j);
+        printf("-><- a[%d]=%d, a[%d]=%d\n", j, a[j], i, a[i]);
+        dump_data("Post-swap", a, start, end);
+    }
+    dump_data("Partition", a, start, end);
+    printf("<<-- P(%d) = %d\n", j, a[j]);
+    return j;
+}
+
+void quick_sort(int arr[], int start, int end);
+
+void quick_sort(int arr[], int start, int end)
+{
+    if (start < end)
+    {
+        dump_data("QS Pre-partition", arr, start, end);
+        int boundary = partition(arr, start, end);
+        printf("QS Partition: %d:%d:%d\n", start, boundary, end);
+        dump_data("QS Pre-recursion L", arr, start, boundary);
+        quick_sort(arr, start, boundary);
+        dump_data("QS Pre-recursion H", arr, boundary + 1, end);
+        quick_sort(arr, boundary + 1, end);
+        dump_data("QS Post-Sort", arr, start, end);
+    }
+}
+
+int main(void)
+{
+    int data[] =
+    {
+        9, 7, 0, 9, 5, 4, 8, 7, 9, 9,
+        2, 9, 9, 7, 0, 3, 9, 6, 8, 5,
+    };
+    enum { NUM_DATA = sizeof(data) / sizeof(data[0]) };
+
+    dump_data("Before", data, 0, NUM_DATA - 1);
+    quick_sort(data, 0, NUM_DATA - 1);
+    dump_data("After", data, 0, NUM_DATA - 1);
+
+    return 0;
+}
