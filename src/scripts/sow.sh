75b26e9602efabd25aea76a8f6fffbb361ca143b
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-03-04 15:43:08 -0800

    Add script sow and define CP in makefile

---
+++ src/scripts/sow.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+#
+# Generate a standard Welcome to SO message for MCVE.
+
+cat <<'EOF'
+Welcome to Stack Overflow.
+Please read the [About] and [Ask] pages soon, but even more importantly,
+please read about how to create an MCVE ([MCVE]).
+EOF
