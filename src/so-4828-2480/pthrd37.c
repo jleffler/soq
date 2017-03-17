df75f045cf8d4a415825cedf3781a165c33b87c7
Jonathan Leffler <jleffler@us.ibm.com>
2017-03-17 16:00:50 -0700

    pthrd37 seems to be working
    Remove the consumed mutex and condition.
    Considerable renaming.  Tighten up the condition handling.

--- src/so-4828-2480/pthrd37.c
+++ src/so-4828-2480/pthrd37.c
@@ -5,66 +5,58 @@
 ** A new random number is needed for each cycle, but all threads must
 ** use the same random number on each cycle.
 ** Any thread may evaluate the new random number.
-**
-** This code is currently broken!  Synchronization is a problem.
 */
 
 #include <assert.h>
 #include <errno.h>
 #include <pthread.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
 #include "stderr.h"
 
-enum { NUM_THREADS = 5 };
-enum { NUM_ITERATIONS = 7 };
+enum { MAX_THREADS = 3 };
+enum { MAX_CYCLES  = 5 };
 
 static pthread_mutex_t mtx_waiting = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cnd_waiting = PTHREAD_COND_INITIALIZER;
-static int waiting = 0;
-
-static pthread_mutex_t mtx_consumed = PTHREAD_MUTEX_INITIALIZER;
-static pthread_cond_t cnd_consumed = PTHREAD_COND_INITIALIZER;
-static int consumed = NUM_THREADS;
-
-static int cycle = -1;
+static pthread_cond_t  cnd_waiting = PTHREAD_COND_INITIALIZER;
+static int             num_waiting = 0;
+static int             cycle   = -1;
 
 static float gl_rand = 0;
 static long  gl_long = 0;
 
 static float next_iteration_random_number(int tid, int iteration)
 {
-    pthread_mutex_lock(&mtx_consumed);
-    while (consumed != NUM_THREADS)
-        pthread_cond_wait(&cnd_consumed, &mtx_consumed);
-    pthread_mutex_unlock(&mtx_consumed);
-
     pthread_mutex_lock(&mtx_waiting);
-    printf("-->> TID %d, I = %d (C = %d, W = %d)\n", tid, iteration, cycle, waiting);
+    assert(cycle == iteration || cycle == iteration - 1);
+    num_waiting++;
+    printf("-->> TID %d, I = %d (C = %d, W = %d)\n",
+           tid, iteration, cycle, num_waiting);
+    while (cycle != iteration && num_waiting != MAX_THREADS)
+    {
+        assert(num_waiting > 0 && num_waiting <= MAX_THREADS);
+        printf("-CW- TID %d, I = %d (C = %d, W = %d)\n",
+               tid, iteration, cycle, num_waiting);
+        pthread_cond_wait(&cnd_waiting, &mtx_waiting);
+    }
+    assert(cycle == iteration || num_waiting == MAX_THREADS);
+    printf("---- TID %d, I = %d (C = %d, W = %d)\n",
+           tid, iteration, cycle, num_waiting);
+
     if (cycle != iteration)
     {
-        assert(waiting >= 0 && waiting <= NUM_THREADS);
-        waiting++;
-        while (waiting != 0 && waiting != NUM_THREADS)
-        {
-            printf("-CW- TID %d, I = %d (C = %d, W = %d)\n", tid, iteration, cycle, waiting);
-            pthread_cond_wait(&cnd_waiting, &mtx_waiting);
-        }
-        if (cycle != iteration)
-        {
-            gl_long = lrand48();
-            gl_rand = (float)gl_long;
-            printf("---- TID %d generates cycle %d: L = %ld, F = %g\n", tid, iteration, gl_long, gl_rand);
-            waiting = 0;
-            cycle = iteration;
-            consumed = 0;
-            pthread_cond_broadcast(&cnd_waiting);
-        }
+        gl_long = lrand48();
+        gl_rand = (float)gl_long;
+        num_waiting = 0;
+        cycle = iteration;
+        printf("---- TID %d generates cycle %d: L = %ld, F = %g\n",
+               tid, cycle, gl_long, gl_rand);
+        pthread_cond_broadcast(&cnd_waiting);
     }
-    consumed++;
-    printf("<<-- TID %d, I = %d (C = %d, W = %d)\n", tid, iteration, cycle, waiting);
+
+    printf("<<-- TID %d, I = %d (C = %d, W = %d) L = %ld, F = %g\n",
+           tid, iteration, cycle, num_waiting, gl_long, gl_rand);
     pthread_mutex_unlock(&mtx_waiting);
     return gl_rand;
 }
@@ -73,7 +65,7 @@ static void *thread_function(void *vp)
 {
     int tid = (int)(uintptr_t)vp;     // Thuggish!
 
-    for (int i = 0; i < NUM_ITERATIONS; i++)
+    for (int i = 0; i < MAX_CYCLES; i++)
     {
         float f = next_iteration_random_number(tid, i);
         printf("TID %d at work: I = %d, F = %g\n", tid, i, f);
@@ -93,9 +85,9 @@ int main(int argc, char **argv)
     err_setarg0(argv[0]);
     assert(argc == 1);
 
-    pthread_t thread[NUM_THREADS];
+    pthread_t thread[MAX_THREADS];
 
-    for (int i = 0; i < NUM_THREADS; i++)
+    for (int i = 0; i < MAX_THREADS; i++)
     {
         int rc = pthread_create(&thread[i], 0, thread_function, (void *)(uintptr_t)i);
         if (rc != 0)
@@ -105,7 +97,7 @@ int main(int argc, char **argv)
         }
     }
 
-    for (int i = 0; i < NUM_THREADS; i++)
+    for (int i = 0; i < MAX_THREADS; i++)
     {
         void *vp;
         int rc = pthread_join(thread[i], &vp);
