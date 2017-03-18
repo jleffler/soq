d0f533fc07b3d749f62ca8c5a9734ff58eb74b62
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-03-17 23:08:59 -0700

    Add config.h appropriate for macOS Sierra 10.12.3
    This config.h probably works for modern Linux (and even some fairly old ones).

---
+++ src/libsoq/config.h
@@ -0,0 +1,11 @@
+/* Default config.h for macOS Sierra 10.12.3 etc */
+#ifndef CONFIG_H_INCLUDED
+#define CONFIG_H_INCLUDED
+
+#define HAVE_UNISTD_H
+#define HAVE_CLOCK_GETTIME
+#define HAVE_MEMMEM
+#define HAVE_STRNLEN
+#define HAVE_STRNDUP
+
+#endif /* CONFIG_H_INCLUDED */
