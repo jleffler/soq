39ad61562a4c65cb6a215bd007044867e471e599
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-04-15 13:55:14 -0700

    Add oddascevendesc.c

---
+++ doc/qsort/oddascevendesc.c
@@ -0,0 +1,90 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* odd numbers in ascending order before even numbers in descending order */
+static int oddasc_evendesc(const void *vp1, const void *vp2)
+{
+    int v1 = *(int *)vp1;
+    int v2 = *(int *)vp2;
+    if ((v1 & 1) != (v2 & 1))
+    {
+        /* One odd, one even */
+        if (v1 & 1)
+            return -1;
+        return +1;
+    }
+    /* Both odd or both even */
+    if ((v1 & 1) == 1)
+        return (v1 < v2) ? -1 : (v1 > v2) ? +1 : 0;     /* Ascending */
+    return (v1 < v2) ? +1 : (v1 > v2) ? -1 : 0;         /* Descending */
+}
+
+/* even numbers descending before odd numbers ascending */
+static int evendesc_oddasc(const void *vp1, const void *vp2)
+{
+    int v1 = *(int *)vp1;
+    int v2 = *(int *)vp2;
+    if ((v1 & 1) != (v2 & 1))
+    {
+        /* One odd, one even */
+        if (v1 & 1)
+            return +1;
+        return -1;
+    }
+    /* Both odd or both even */
+    if ((v1 & 1) == 1)
+        return (v1 < v2) ? -1 : (v1 > v2) ? +1 : 0;     /* Ascending */
+    return (v1 < v2) ? +1 : (v1 > v2) ? -1 : 0;         /* Descending */
+}
+
+static void dump_array(const char *tag, int num, int *data)
+{
+    printf("%s:\n", tag);
+    int i;
+    const char *pad = "";
+    for (i = 0; i < num; i++)
+    {
+        printf("%s%+3d", pad, data[i]);
+        pad = " ";
+        if (i % 10 == 9)
+        {
+            putchar('\n');
+            pad = "";
+        }
+    }
+    if (i % 10 != 0)
+        putchar('\n');
+}
+
+int main(void)
+{
+    int data[] =
+    {
+        /* random -n 80 -- -99 99 | commalist -b '        ' -n 10 */
+        +39, +36, +78, -92, +63, -21, -51, +49,   0, -77,
+        -10, -49, -98, -17, +60, +83, +30, -97, -68, +86,
+        +70, +84, -56,  +3, +33, -34, +14, -40, -72, -86,
+        -95, -87, -73, -20, -72, -86,  -3, -71, -55, -80,
+        -60,  -4, -26, -64, -31, -84, -79, +25, +41, +80,
+        -54, -51, +24, -48, +13, +61, -99, +60,  -2, +16,
+        -66, -30, +24, +88,  +5, -77, +13,  +3, +16, -69,
+        -60, +26, +51, +16, -13, +71,  -9,  -2, +51, +72,
+    };
+    enum { NUM_DATA = sizeof(data) / sizeof(data[0]) };
+    int data2[NUM_DATA];
+
+    memmove(data2, data, sizeof(data));
+
+    printf("Sort odd numbers ascending before even numbers descending\n");
+    dump_array("Before", NUM_DATA, data);
+    qsort(data, NUM_DATA, sizeof(data[0]), oddasc_evendesc);
+    dump_array("After", NUM_DATA, data);
+
+    printf("Sort even numbers descending before odd numbers ascending\n");
+    dump_array("Before", NUM_DATA, data2);
+    qsort(data2, NUM_DATA, sizeof(data2[0]), evendesc_oddasc);
+    dump_array("After", NUM_DATA, data2);
+
+    return 0;
+}
