c5d1bacc2feaf64abfccbcbfa9b4df3d716a98e5
Jonathan Leffler <jleffler@us.ibm.com>
2017-06-20 17:09:49 -0700

    Add support script so-late.sh for late answers

---
+++ src/scripts/so-late.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Blurb for a comment to a late SO answer
+
+exec cat <<'EOF'
+Welcome to Stack Overflow.
+If you decide to answer an older question that has well established and
+correct answers, adding a new answer late in the day may not get you any
+credit.
+If you have some distinctive new information, or you're convinced the
+other answers are all wrong, by all means add a new answer, but 'yet
+another answer' giving the same basic information a long time after the
+question was asked usually won't earn you much credit.
+EOF
