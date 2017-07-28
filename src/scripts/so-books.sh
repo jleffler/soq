23991e5cc7a136151f36599b445591f469aa04cb
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-07-28 16:28:05 -0700

    Add script so-books.sh and a README, etc

---
+++ src/scripts/so-books.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# URLs for key books for SO in Markdown
+
+cat << 'EOF'
+
+W. Richard Stevens, Bill Fenner, Andrew M. Rudoff 
+[UNIX® Network Programming, Vol 1: The Sockets Networking API, 3rd Edn](http://www.amazon.com/Unix-Network-Programming-Volume-Networking/dp/0131411551)
+[UNIX® Network Programming, Vol 1: The Sockets Networking API](http://unpbook.com/)
+
+W Richard Stevens
+[Unix Network Programming, Vol 2: Interprocess Communications, 2nd Edn](http://www.amazon.com/UNIX-Network-Programming-Volume-Communications/dp/0132974290)
+
+W Richard Stevens, Stephen A Rago
+[Advanced Programming in the Unix Environment, 3rd Edn](http://www.amazon.com/Advanced-Programming-UNIX-Environment-Edition/dp/0321637739)
+
+Marc J Rochkind
+[Advanced Unix Programming, 2nd Edn](http://www.amazon.com/Advanced-UNIX-Programming-2nd-Edition/dp/0131411543)
+
+Michael Kerrisk
+[The Linux Programming Interface: A Linux and Unix System Programming Handbook](http://www.amazon.com/The-Linux-Programming-Interface-Handbook/dp/1593272200) 1st Edn
+
+Brian W Kernighan, Rob Pike
+[The Unix Programming Environment](http://www.amazon.com/Programming-Environment-Prentice-Hall-Software-Series/dp/013937681X) Nov 1983.
+
+Brian W Kernighan, Rob Pike
+[The Practice of Programming](http://www.cs.princeton.edu/~bwk/tpop.webpage/)
+[The Practice of Programming](http://www.amazon.com/Practice-Programming-Addison-Wesley-Professional-Computing/dp/020161586X)
+
+EOF
