152718f75f1288bc2acdbb999dedced36441371e
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-08-08 00:07:05 -0700

    SO 4555-1869: Generalize pth19 to handle 2..20 threads

--- src/so-4555-1869/pth19.c
+++ src/so-4555-1869/pth19.c
@@ -6,9 +6,11 @@
 #include <pthread.h>
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 typedef struct ThreadControl
 {
+    pthread_t       ctl_thread;
 	pthread_mutex_t mtx_thread;
 	pthread_cond_t  cvr_thread;
 	pthread_mutex_t mtx_parent;
@@ -16,26 +18,6 @@ typedef struct ThreadControl
 	int             var_parent;
 	int             thr_number;
 } ThreadControl;
-
-static ThreadControl thr_ctl[2] =
-{
-	{
-		.mtx_thread = PTHREAD_MUTEX_INITIALIZER, 
-		.cvr_thread = PTHREAD_COND_INITIALIZER, 
-		.mtx_parent = PTHREAD_MUTEX_INITIALIZER, 
-		.cvr_parent = PTHREAD_COND_INITIALIZER, 
-		.var_parent = 0,
-		.thr_number = 1,
-	},
-	{
-		.mtx_thread = PTHREAD_MUTEX_INITIALIZER, 
-		.cvr_thread = PTHREAD_COND_INITIALIZER, 
-		.mtx_parent = PTHREAD_MUTEX_INITIALIZER, 
-		.cvr_parent = PTHREAD_COND_INITIALIZER, 
-		.var_parent = 0,
-		.thr_number = 2,
-	},
-};
 
 static void err_ptherr(int errnum, const char *fmt, ...)
 {
@@ -50,21 +32,45 @@ static void err_ptherr(int errnum, const char *fmt, ...)
 static void *threadFunc(void *args)
 {
 	ThreadControl *ct = args;
-    pthread_mutex_lock(&ct->mtx_thread);
+    int rc;
+    if ((rc = pthread_mutex_lock(&ct->mtx_thread)) != 0)
+        err_ptherr(rc, "failed to lock thread mutex %d: ", ct->thr_number);
     printf("T%d: thread mutex locked\n", ct->thr_number);
-    pthread_mutex_lock(&ct->mtx_parent);
+    if ((rc = pthread_mutex_lock(&ct->mtx_parent)) != 0)
+        err_ptherr(rc, "failed to lock parent mutex %d: ", ct->thr_number);
     printf("T%d: parent mutex locked\n", ct->thr_number);
     ct->var_parent = 1;
-    pthread_cond_signal(&ct->cvr_parent);
+    if ((rc = pthread_cond_signal(&ct->cvr_parent)) != 0)
+        err_ptherr(rc, "failed to signal parent condition %d: ", ct->thr_number);
     printf("T%d: parent condition signalled\n", ct->thr_number);
-    pthread_mutex_unlock(&ct->mtx_parent);
+    if ((rc = pthread_mutex_unlock(&ct->mtx_parent)) != 0)
+        err_ptherr(rc, "failed to unlock parent mutex %d: ", ct->thr_number);
     printf("T%d: parent mutex unlocked\n", ct->thr_number);
-    pthread_cond_wait(&ct->cvr_thread, &ct->mtx_thread);
+    if ((rc = pthread_cond_wait(&ct->cvr_thread, &ct->mtx_thread)) != 0)
+        err_ptherr(rc, "failed to wait on thread condition %d: ", ct->thr_number);
     printf("T%d: thread condition waited on\n", ct->thr_number);
     printf("X modified by thread %d\n", ct->thr_number);
-    pthread_mutex_unlock(&ct->mtx_thread);
+    if ((rc = pthread_mutex_unlock(&ct->mtx_thread)) != 0)
+        err_ptherr(rc, "failed to unlock thread mutex %d: ", ct->thr_number);
     printf("T%d: thread mutex unlocked\n", ct->thr_number);
     return NULL;
+}
+
+static void init_thread_control(int thr_number, ThreadControl *ct)
+{
+	int rc;
+    ct->thr_number = thr_number;
+    ct->var_parent = 0;
+    // ct->ctl_thread = 0;     // Neither portable nor necessary
+    if ((rc = pthread_mutex_init(&ct->mtx_thread, NULL)) != 0)
+        err_ptherr(rc, "failed to initialize thread mutex %d: ", ct->thr_number);
+    if ((rc = pthread_cond_init(&ct->cvr_thread, NULL)) != 0)
+        err_ptherr(rc, "failed to initialize thread condition %d: ", ct->thr_number);
+    if ((rc = pthread_mutex_init(&ct->mtx_parent, NULL)) != 0)
+        err_ptherr(rc, "failed to initialize parent mutex %d: ", ct->thr_number);
+    if ((rc = pthread_cond_init(&ct->cvr_parent, NULL)) != 0)
+        err_ptherr(rc, "failed to initialize parent condition %d: ", ct->thr_number);
+    printf("Initialization complete for thread %d\n", ct->thr_number);
 }
 
 static void destroy_thread_control(ThreadControl *ct)
@@ -122,10 +128,10 @@ static void wait_parent_condition(ThreadControl *ct)
     }
 }
 
-static void create_thread(pthread_t *pt, ThreadControl *ct)
+static void create_thread(ThreadControl *ct)
 {
 	int rc;
-    if ((rc = pthread_create(pt, NULL, threadFunc, ct)) != 0)
+    if ((rc = pthread_create(&ct->ctl_thread, NULL, threadFunc, ct)) != 0)
         err_ptherr(rc, "failed to create thread %d: ", ct->thr_number);
 }
 
@@ -136,10 +142,10 @@ static void signal_thread_condition(ThreadControl *ct)
         err_ptherr(rc, "failed to signal thread condition %d: ", ct->thr_number);
 }
 
-static void join_thread(pthread_t *pt, ThreadControl *ct)
+static void join_thread(ThreadControl *ct)
 {
 	int rc;
-    if ((rc = pthread_join(*pt, NULL)) != 0)
+    if ((rc = pthread_join(ct->ctl_thread, NULL)) != 0)
         err_ptherr(rc, "failed to join thread %d: ", ct->thr_number);
 }
 
@@ -147,51 +153,61 @@ int main(int argc, char **argv)
 {
     assert(argc > 0);
     err_setarg0(argv[0]);
-    pthread_t thread[2];
+    int nthreads = 2;
+    if (argc == 2)
+    {
+        nthreads = atoi(argv[1]);
+        if (nthreads < 2 || nthreads > 20)
+            err_error("Number of threads out of control ('%s' mapped to %d; %s)\n",
+                      argv[1], nthreads, "should be in range 2..20");
+    }
+    ThreadControl thr_ctl[nthreads];
 
-	lock_thread_mutex(&thr_ctl[0]);
-	lock_thread_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        init_thread_control(i+1, &thr_ctl[i]);
+
+    for (int i = 0; i < nthreads; i++)
+        lock_thread_mutex(&thr_ctl[i]);
     printf("Thread mutexes locked\n");
 
-	lock_parent_mutex(&thr_ctl[0]);
-	lock_parent_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        lock_parent_mutex(&thr_ctl[i]);
     printf("Parent mutexes locked\n");
 
-	create_thread(&thread[0], &thr_ctl[0]);
-	create_thread(&thread[1], &thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        create_thread(&thr_ctl[i]);
     printf("Threads created\n");
 
-    /* Let the threads lock their mutexes */
-	unlock_thread_mutex(&thr_ctl[0]);
-	unlock_thread_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        unlock_thread_mutex(&thr_ctl[i]);
     printf("Thread mutexes unlocked\n");
 
-    wait_parent_condition(&thr_ctl[0]);
-    wait_parent_condition(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        wait_parent_condition(&thr_ctl[i]);
     printf("Parent conditions waited on\n");
 
-	unlock_parent_mutex(&thr_ctl[0]);
-	unlock_parent_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        unlock_parent_mutex(&thr_ctl[i]);
     printf("Parent mutexes unlocked\n");
 
-	lock_thread_mutex(&thr_ctl[0]);
-	lock_thread_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        lock_thread_mutex(&thr_ctl[i]);
     printf("Thread mutexes locked again\n");
 
-	signal_thread_condition(&thr_ctl[0]);
-	signal_thread_condition(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        signal_thread_condition(&thr_ctl[i]);
     printf("Thread conditions signalled\n");
 
-	unlock_thread_mutex(&thr_ctl[0]);
-	unlock_thread_mutex(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        unlock_thread_mutex(&thr_ctl[i]);
     printf("Thread mutexes unlocked again\n");
 
-    join_thread(&thread[0], &thr_ctl[0]);
-    join_thread(&thread[1], &thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        join_thread(&thr_ctl[i]);
     printf("Threads joined\n");
 
-	destroy_thread_control(&thr_ctl[0]);
-	destroy_thread_control(&thr_ctl[1]);
+    for (int i = 0; i < nthreads; i++)
+        destroy_thread_control(&thr_ctl[i]);
 
     return 0;
 }
