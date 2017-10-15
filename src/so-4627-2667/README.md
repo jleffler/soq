eb30d3ee1deabfdb53f3f7267a23e55c04c37710
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-10-15 12:39:09 -0700

    SO 4627-2667: Add verbosity and better error reporting
    Tests with negative numbers on relational operators fail for negative numbers (equality, inequality OK)

--- src/so-4627-2667/README.md
+++ src/so-4627-2667/README.md
@@ -2,3 +2,7 @@
 
 [SO 4627-2667](https://stackoverflow.com/q/46272667) &mdash;
 Water tank logic using only C arithmetic operators?
+
+See also [SO 0144-9260](https://stackoverflow.com/questions/1449260)
+&mdash; Bitwise operations without bitwise operators?
+My answer there uses brute force and ignorance with loops to implement the operations.
--- src/so-4627-2667/arithlogic71.c
+++ src/so-4627-2667/arithlogic71.c
@@ -9,6 +9,8 @@
 ** -- If H >   0, R3 = 1.1 else R3 = 0.0
 ** -- Determine R = R1 + R2 + R3 for given H.
 **
+** Ignoring issues of overflow (small enough numbers), and mixed signs,
+** the macros below work.
 */
 
 /*
@@ -17,26 +19,27 @@
 ** from Ryan https://stackoverflow.com/users/707111/ryan)
 ** If (a-b) is zero, the divisor is 1 and the result is 1.
 ** If (a-b) is not zero, the divisor is at least 2 and the result is 0.
+** Note that EQ works without overflow if a and b are unsigned.
+** They could, of course, be coerced into being unsigned.
 */
 #define EQ(a, b)     (1 / (((a) - (b)) * ((a) - (b)) + 1))
+#define NE(x, y)     (1 - EQ(x, y))
 
 /*
 ** From SO 4627-2667 (by Ajay Brahmakshatriya
 ** https://stackoverflow.com/users/2858773/ajay-brahmakshatriya), comes
 ** the key observation that for integers, x/200 is 0 for x 0..199, and 1
 ** or more for higher values.
+** Given EQ() and one of the other relational operators, we can easily
+** deduce the other operators
 */
-#define GE(x, y)        (1 - EQ((x)/(y), 0))
-
-/*
-** Given GE() and EQ(), we can deduce the other operators
-*/
-#define NE(x, y)        (1 - EQ(x, y))
-#define GT(x, y)        (GE(x, y) - EQ(x, y))
-#define LE(x, y)        (1 - GT(x, y))
-#define LT(x, y)        (1 - GE(x, y))
+#define LT(x, y)     EQ((x)/(y), 0)
+#define GE(x, y)     (1 - LT(x, y))
+#define GT(x, y)     (GE(x, y) - EQ(x, y))
+#define LE(x, y)     (1 - GT(x, y))
 
 #include <stdio.h>
+#include "stderr.h"
 
 static inline int gt(int x, int y) { return x >  y; }
 static inline int ge(int x, int y) { return x >= y; }
@@ -65,54 +68,57 @@ static void test_array_pair(int size1, const int *data1, int size2, const int *d
             int res1 = (*cmp1)(data1[i], data2[j]);
             int res2 = (*cmp2)(data1[i], data2[j]);
             if (verbose || res1 != res2)
-                printf("x = %4d, y = %4d, %s(x,y) = %d, (x %s y) = %d\n",
-                       data1[i], data2[j], name1, res1, op, res2);
+                printf("x = %4d, y = %4d, %s(x,y) = %d, (x %s y) = %d%s\n",
+                       data1[i], data2[j], name1, res1, op, res2, (res1 == res2) ? "" : " !!FAIL!!");
         }
     }
 }
 
-int main(void)
+int main(int argc, char **argv)
 {
+    err_setarg0(argv[0]);
+    int verbose = (argc > 1);
+
     int a[] = { 1, 2, -4, 6, 10, 200 };
     enum { A_SIZE = sizeof(a) / sizeof(a[0]) };
     int b[] = { 1, 4, -4, 10, 20, 203 };
     enum { B_SIZE = sizeof(b) / sizeof(b[0]) };
 
     printf("Testing equality (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_EQ, eq, 0, "EQ", "==");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_EQ, eq, verbose, "EQ", "==");
 
     printf("Testing equality (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_EQ, eq, 0, "EQ", "==");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_EQ, eq, verbose, "EQ", "==");
 
     printf("Testing inequality (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_NE, ne, 0, "NE", "!=");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_NE, ne, verbose, "NE", "!=");
 
     printf("Testing inequality (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_NE, ne, 0, "NE", "!=");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_NE, ne, verbose, "NE", "!=");
 
     printf("Testing greater-than-or-equal-to (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_GE, ge, 0, "GE", ">=");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_GE, ge, verbose, "GE", ">=");
 
     printf("Testing greater-than-or-equal-to (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_GE, ge, 0, "GE", ">=");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_GE, ge, verbose, "GE", ">=");
 
     printf("Testing greater-than (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_GT, gt, 0, "GT", ">");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_GT, gt, verbose, "GT", ">");
 
     printf("Testing greater-than (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_GT, gt, 0, "GT", ">");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_GT, gt, verbose, "GT", ">");
 
     printf("Testing less-than-or-equal-to (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_LE, le, 0, "LE", "<=");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_LE, le, verbose, "LE", "<=");
 
     printf("Testing less-than-or-equal-to (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_LE, le, 0, "LE", "<=");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_LE, le, verbose, "LE", "<=");
 
     printf("Testing less-than (on arrays a and b)\n");
-    test_array_pair(A_SIZE, a, B_SIZE, b, func_LT, lt, 0, "LT", "<");
+    test_array_pair(A_SIZE, a, B_SIZE, b, func_LT, lt, verbose, "LT", "<");
 
     printf("Testing less-than (on array a and itself)\n");
-    test_array_pair(A_SIZE, a, A_SIZE, a, func_LT, lt, 0, "LT", "<");
+    test_array_pair(A_SIZE, a, A_SIZE, a, func_LT, lt, verbose, "LT", "<");
 
     return 0;
 }
