7e2002b3674a1372c010c3250e334dede5305c1f
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-10-22 08:04:42 -0700

    SO 4685-9611: Third solution by ctac marked as such.

--- src/so-4685-9611/ctac13.sh
+++ src/so-4685-9611/ctac13.sh
@@ -2,7 +2,7 @@
 #
 # SO 4685-9611
 #
-# Second suggested version from ctac_
+# Third suggested version from ctac_
 # https://stackoverflow.com/users/8592975/ctac
 
 awk -F. 'NF>2{s[$(NF-2)FS$(NF-1)FS$(NF)]++}END{for(i in s)if(s[i]>2){sub("[^.]*.","",i);print i}}' "$@"
