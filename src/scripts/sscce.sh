b6d720587e2f55a0f619338cd48b77d2538fd794
Jonathan Leffler <jonathan.leffler@gmail.com>
2017-11-21 15:58:23 -0800

    Add sscce.sh script to src/scripts

---
+++ src/scripts/sscce.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#
+# Analog to WSO for SSCCE and MCVE
+
+echo 'MCVE ([How to create a Minimal, Complete, and Verifiable Example?](http://stackoverflow.com/help/mcve))'
+echo 'SSCCE ([Short, Self-Contained, Correct Example](http://sscce.org/))'
