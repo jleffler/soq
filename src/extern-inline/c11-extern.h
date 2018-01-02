eab9f20b40147e756dbd6ee7a3d400d4956d3c5c
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-01-01 17:37:16 -0800

    First stages of experimentation with extern/inline in C

---
+++ src/extern-inline/c11-extern.h
@@ -0,0 +1,9 @@
+#ifndef C11_EXTERN_H_INCLUDED
+#define C11_EXTERN_H_INCLUDED
+
+#include "c11-inline.h"
+
+extern int fn_regular1(int x);
+extern int fn_regular2(int x);
+
+#endif /* C11_EXTERN_H_INCLUDED */
