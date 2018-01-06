22be3ef29df61d28ca7f5d3862783a080e9d5930
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-01-05 23:36:43 -0800

    src/libsoq/refresh.sh: apply githeader.pl to named files

---
+++ src/libsoq/refresh.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#
+# Refresh SOQ code from reference version
+
+tmp=$(mktemp "${TMPDIR:-/tmp}/tmp.XXXXXXXXX")
+trap "rm -f $tmp; exit 1" 0 1 2 3 13 15
+
+for file in "$@"
+do
+    reffile="$HOME/lib/JL/$file"
+    if [ ! -f "$reffile" ]
+    then echo "Unmatched - $file"
+    elif ${PERL:-perl} githeader.pl "$reffile" > "$tmp"
+         cmp -s "$file" "$tmp"
+    then echo "Unchanged - $file"
+    else echo "Refreshed - $file"
+         cp "$tmp" "$file"
+    fi
+done
+
+rm -f "$tmp"
+trap 0
