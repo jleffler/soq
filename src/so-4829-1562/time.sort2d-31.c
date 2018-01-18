fe5669cb0a4f66103e96d494af12ec6af4225ebd
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-01-18 01:19:15 -0800

    SO 4829-1562: Add timing test
    NB: got a segfault on quick_sort() for 1000x1000.
    Not the right time to investigate why - but it is puzzling.
    However, it was clear that the copy-quicksort-copy code is
    dramatically faster than the non-copying sort.  The benefit
    is marginal on 10x10 (15µs vs 12µs), but 20x20 shows 172µs
    vs 32µs in favour of quicksort, and by 900x900, the basic
    sort was taking 285s vs 0.054s for quicksort.

---
+++ src/so-4829-1562/time.sort2d-31.c
@@ -0,0 +1,96 @@
+/* SO 4829-1562 */
+
+/*
+**  + Enter a square matrix of dimensions n .
+**  + Elements below main diagonal sort in ascending order.
+**  + Elements above main diagonal sort in descending order.
+**  + Elements on main diagonal sort :
+**    - first even numbers in ascending order.
+**    - then odd numbers in descending order.
+*/
+
+/* Timing harness */
+
+#include "stderr.h"
+#include "timer.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "time.sort2d-31.h"
+
+static void time_matrix(const char *tag, size_t n, int matrix[n][n], void (*sorter)(size_t n, int matrix[n][n]))
+{
+    Clock clk;
+    clk_init(&clk);
+    clk_start(&clk);
+    (*sorter)(n, matrix);
+    clk_stop(&clk);
+    char buffer[32];
+    printf("%s sort - elapsed (%zux%zu): %s\n", tag, n, n, clk_elapsed_us(&clk, buffer, sizeof(buffer)));
+}
+
+static void cmp_matrix(size_t n, int matrix1[n][n], int matrix2[n][n])
+{
+    for (size_t r = 0; r < n; r++)
+    {
+        for (size_t c = 0; c < n; c++)
+        {
+            if (matrix1[r][c] != matrix2[r][c])
+                err_remark("Mismatch m1[%zu][%zu] = %d; m2[%zu][%zu] = %d\n",
+                        r, c, matrix1[r][c], r, c, matrix2[r][c]);
+        }
+    }
+}
+
+static void dump_matrix(const char *tag, size_t r, size_t c, int matrix[r][c])
+{
+    printf("%s:\n", tag);
+    for (size_t i = 0; i < r; i++)
+    {
+        for (size_t j = 0; j < c; j++)
+            printf("%4d", matrix[i][j]);
+        putchar('\n');
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 0)
+        err_setarg0(argv[0]);
+    srand(time(0));
+    int multipliers[] = { 10, 100, 1000, 10000 };
+    enum { NUM_MULTIPLIERS = sizeof(multipliers) / sizeof(multipliers[0]) };
+    int verbose = 0;
+
+    for (int i = 0; i < NUM_MULTIPLIERS; i++)
+    {
+        for (int j = 1; j < 10; j++)
+        {
+            int n = multipliers[i] * j;
+            int (*matrix1)[n] = malloc(n * n * sizeof(matrix1[0][0]));
+            int (*matrix2)[n] = malloc(n * n * sizeof(matrix1[0][0]));
+            if (matrix1 == 0 || matrix2 == 0)
+                err_syserr("failed to allocate %zu bytes of memory: ",
+                           n * n * sizeof(matrix1[0][0]));
+            for (int r = 0; r < n; r++)
+            {
+                for (int c = 0; c < n; c++)
+                {
+                    matrix1[r][c] = matrix2[r][c] = rand() % 900 + 100;
+                }
+            }
+            if (verbose)
+                dump_matrix("Before", n, n, matrix1);
+            cmp_matrix(n, matrix1, matrix2);
+            time_matrix("Basic", n, matrix1, basic_sort);
+            time_matrix("Quick", n, matrix2, quick_sort);
+            if (verbose)
+                dump_matrix("After", n, n, matrix1);
+            cmp_matrix(n, matrix1, matrix2);
+            free(matrix1);
+            free(matrix2);
+        }
+    }
+
+    return 0;
+}
---
+++ src/so-4829-1562/time.sort2d-83.c
@@ -0,0 +1,146 @@
+/* SO 4829-1562 */
+
+#include "time.sort2d-31.h"
+
+/*
+**  + Enter a square matrix of dimensions n .
+**  + Elements below main diagonal sort in ascending order.
+**  + Elements above main diagonal sort in descending order.
+**  + Elements on main diagonal sort :
+**    - first even numbers in ascending order.
+**    - then odd numbers in descending order.
+*/
+
+/* Variation 1: Using direct sorting of triangles of matrix without copying */
+
+static inline int cmp_asc(int x, int y) { return (x > y) - (x < y); }
+static inline int cmp_dsc(int x, int y) { return (x < y) - (x > y); }
+static inline int cmp_eaod(int x, int y)
+{
+    int px = x & 1;
+    int py = y & 1;
+    if (px != py)
+        return px - py;
+    if (px == 1)
+        return cmp_dsc(x, y);
+    return cmp_asc(x, y);
+}
+
+static void sort_diagonal(size_t n, int matrix[n][n])
+{
+    for (size_t i = 0; i < n; i++)
+    {
+        for (size_t j = i + 1; j < n; j++)
+        {
+            if (cmp_eaod(matrix[i][i], matrix[j][j]) > 0)
+            {
+                int t = matrix[i][i];
+                matrix[i][i] = matrix[j][j];
+                matrix[j][j] = t;
+            }
+        }
+    }
+}
+
+/*
+**   D0  U0  U1  U2  U3
+**   L0  D1  U4  U5  U6
+**   L1  L2  D3  U7  U8
+**   L3  L4  L5  D4  U9
+**   L6  L7  L8  L9  D5
+**
+** D0 = (0, 0); U0 = (0, 1); U1 = (0, 2); U2 = (0, 3); U3 = (0, 4);
+** L0 = (1, 0); D1 = (1, 1); U4 = (1, 2); U5 = (1, 3); U6 = (1, 4);
+** L1 = (2, 0); L2 = (2, 1); D2 = (2, 2); U7 = (2, 3); U8 = (2, 4);
+** L3 = (3, 0); L4 = (3, 1); L5 = (3, 2); D3 = (3, 3); U9 = (3, 4);
+** L6 = (4, 0); L7 = (4, 1); L8 = (4, 2); L9 = (4, 3); D4 = (4, 4); 
+*/
+
+static void init_lt_map(size_t n, size_t map[][2])
+{
+    size_t m = (n * (n - 1)) / 2;
+    size_t r = 1;
+    size_t c = 0;
+    for (size_t i = 0; i < m; i++)
+    {
+        map[i][0] = r;
+        map[i][1] = c++;
+        if (c == r)
+        {
+            r++;
+            c = 0;
+        }
+    }
+}
+
+static void sort_lt(size_t n, int matrix[n][n])
+{
+    size_t m = (n * (n - 1)) / 2;
+    size_t lt[m][2];
+    init_lt_map(n, lt);
+
+    for (size_t i = 0; i < m; i++)
+    {
+        size_t xi = lt[i][0];
+        size_t yi = lt[i][1];
+        for (size_t j = i + 1; j < m; j++)
+        {
+            size_t xj = lt[j][0];
+            size_t yj = lt[j][1];
+            if (cmp_asc(matrix[xi][yi], matrix[xj][yj]) > 0)
+            {
+                int t = matrix[xi][yi];
+                matrix[xi][yi] = matrix[xj][yj];
+                matrix[xj][yj] = t;
+            }
+        }
+    }
+}
+
+static void init_ut_map(size_t n, size_t map[][2])
+{
+    size_t m = (n * (n - 1)) / 2;
+    size_t r = 0;
+    size_t c = 0;
+    for (size_t i = 0; i < m; i++)
+    {
+        map[i][0] = r;
+        map[i][1] = ++c;
+        if (c == n - 1)
+        {
+            r++;
+            c = r;
+        }
+    }
+}
+
+static void sort_ut(size_t n, int matrix[n][n])
+{
+    size_t m = (n * (n - 1)) / 2;
+    size_t ut[m][2];
+    init_ut_map(n, ut);
+
+    for (size_t i = 0; i < m; i++)
+    {
+        size_t xi = ut[i][0];
+        size_t yi = ut[i][1];
+        for (size_t j = i + 1; j < m; j++)
+        {
+            size_t xj = ut[j][0];
+            size_t yj = ut[j][1];
+            if (cmp_dsc(matrix[xi][yi], matrix[xj][yj]) > 0)
+            {
+                int t = matrix[xi][yi];
+                matrix[xi][yi] = matrix[xj][yj];
+                matrix[xj][yj] = t;
+            }
+        }
+    }
+}
+
+void basic_sort(size_t n, int matrix[n][n])
+{
+    sort_diagonal(n, matrix);
+    sort_lt(n, matrix);
+    sort_ut(n, matrix);
+}
