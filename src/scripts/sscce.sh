d1962fd3594e874341b10e0aa7a4f8123b6946d4
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-05-06 19:26:31 -0700

    src/scripts: Add ESR How to Ask Questions link
    Upgrade SO to https; the other two are verified http-only.

--- src/scripts/sscce.sh
+++ src/scripts/sscce.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 #
-# Analog to WSO for SSCCE and MCVE
+# Analog to WSO for SSCCE and MCVE (and HTAQTSW)
 
-echo 'MCVE ([How to create a Minimal, Complete, and Verifiable Example?](http://stackoverflow.com/help/mcve))'
+echo 'MCVE ([How to create a Minimal, Complete, and Verifiable Example?](https://stackoverflow.com/help/mcve))'
+
+# Verified http not https 2018-05-06
 echo 'SSCCE ([Short, Self-Contained, Correct Example](http://sscce.org/))'
+echo '[How to Ask Questions the Smart Way](http://www.catb.org/~esr/faqs/smart-questions.html)'
