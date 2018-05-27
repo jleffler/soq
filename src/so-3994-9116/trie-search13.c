b83877fc0237d3920fd2a9f20a6f92c1798258ad
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-05-26 22:59:55 -0700

    Search mainly working.
    There is heavy debug, and code missing to iterate over a long string,
    but it seems to find single words reasonably reliably, though the test
    has not yet been exhaustive.

--- src/so-3994-9116/trie-search13.c
+++ src/so-3994-9116/trie-search13.c
@@ -22,6 +22,7 @@
 ** files of words.  It isn't clear yet how strings such as the spaceless
 ** one at the top will be provided.
 */
+
 #include "posixver.h"
 #include "stderr.h"
 #include <assert.h>
@@ -88,7 +89,7 @@ bool load(const char *dictionary)
 
     char *word = 0;
     size_t wordsize = 0;
-    while (getline(&word, &wordsize, dic) != 0)
+    while (getline(&word, &wordsize, dic) != -1)
     {
         word[strcspn(word, "\r\n")] = '\0';
         dictionary_size++;
@@ -104,7 +105,7 @@ bool load(const char *dictionary)
     return true;
 }
 
-static void print_subtrie(const char *prefix, char nchar, node *child)
+static void print_subtrie(FILE *fp, const char *prefix, char nchar, const node *child)
 {
     if (child != 0)
     {
@@ -114,16 +115,16 @@ static void print_subtrie(const char *prefix, char nchar, node *child)
         buffer[len] = nchar;
         buffer[len+1] = '\0';
         if (child->is_word)
-            printf("%s\n", buffer);
+            fprintf(fp, "%s\n", buffer);
         for (int i = 0; i < 26; i++)
-            print_subtrie(buffer, 'a' + i, child->children[i]);
+            print_subtrie(fp, buffer, 'a' + i, child->children[i]);
     }
 }
 
-static void print_trie(node *top)
+static void print_trie(FILE *fp, const node *top)
 {
     for (int i = 0; i < 26; i++)
-        print_subtrie("", 'a' + i, top->children[i]);
+        print_subtrie(fp, "", 'a' + i, top->children[i]);
 }
 
 static void free_trie(node *trie)
@@ -144,24 +145,69 @@ static void load_dictionary(const char *dictionary)
     if (load(dictionary))
     {
         printf("Nominal dictionary size: %d\n", dictionary_size);
-        print_trie(root);
+        print_trie(stdout, root);
     }
     else
         err_remark("failed to load dictionary file %s\n", dictionary);
 }
 
+/*
+** Identify a word in the dictionary that is a prefix of the current
+** word.  That is, if the word is "onethousand", and "one" is in the
+** dictionary, then the returned result should be 3.  Assuming that the
+** dictionary only contains number-related words, then if the word is
+** "forklift", the result should be 0. If the word is "seventen", then
+** the result should be 5; if it was "seventeenwords", then 9.
+** 
+** So much for the specification!  Now the implementation.  At any given
+** level, the trie node might contain information about word[0].  There
+** are N relevant pieces of information:
+** -- word[0] == '\0' - return 0.
+** -- word[0] is a letter and the node has no entry for that letter;
+**    there is no word that contains this prefix; return 0.
+** -- word[0] is a letter and the node has an entry for that letter;
+**    call recursively to see whether there's a longer word;
+**    if there is (rv > 0), return the returned length plus one;
+**    if there is not and the current node's is_word element is true,
+**    then we have found a word and should return 1;
+**    else there is not a word ending here so return 0.
+*/
 static size_t find_prefix_word(const char *word, const node *trie)
 {
     assert(islower((unsigned char)word[0]) || word[0] == '\0');
+fprintf(stderr, "-->> %s(): word [%s]\n", __func__, word);
     if (word[0] == '\0')
-        return 1;
+    {
+fprintf(stderr, "<<-- %s(): empty word\n", __func__);
+        return 0;
+    }
     else
     {
         int code = word[0] - 'a';
         if (trie->children[code] == 0)
+        {
+fprintf(stderr, "<<-- %s(): empty node %d\n", __func__, code);
             return 0;
-        return find_prefix_word(&word[1], trie->children[code]);
+        }
+        else
+        {
+            size_t rv = find_prefix_word(&word[1], trie->children[code]);
+            if (rv > 0)
+            {
+fprintf(stderr, "<<-- %s(): found word (%zu) return %zu\n", __func__, rv, rv+1);
+                return rv + 1;
+            }
+            else if (trie->children[code]->is_word)
+            {
+fprintf(stderr, "<<-- %s(): child node is word return 1\n", __func__);
+                return 1;
+            }
+fprintf(stderr, "<<-- %s(): child node is not word return 0\n", __func__);
+            return 0;
+        }
     }
+fprintf(stderr, "<<-- %s(): unreached\n", __func__);
+exit(1);
 }
 
 static void check_word(const char *word)
@@ -170,11 +216,11 @@ static void check_word(const char *word)
     size_t max_word = find_prefix_word(word, root);
     assert(max_word <= wordlen);
     if (wordlen == max_word)
-        printf("[%s] is a word\n", word);
+        printf("[%s] (%zu) is a word\n", word, wordlen);
     else if (max_word == 0)
         printf("[%s] does not start with a known word\n", word);
     else
-        printf("[%s] starts with word [%.*s]\n", word, (int)max_word, word);
+        printf("[%s] starts with word [%.*s] (%zu)\n", word, (int)max_word, word, max_word);
 }
 
 static const char optstr[] = "hVd:w:";
