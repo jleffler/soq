2021219f4b308f1d1b7cd834517cf89145bba105
Jonathan Leffler <jonathan.leffler@gmail.com>
2018-10-12 12:08:40 -0700

    Add some notes about what might be wanted.
    It isn't 100% clear a priori what the results should be with mixed signs
    on the operands.  If the quotient is -14.75, should rounding up go to
    -15 or -14?  You can make a case for either.  Maybe -15 is 'round away
    (from zero)' and the other is 'round up'.
    
    Round nearest is also an option, and there is code for that (which may
    not be working, but that's a separate issue).  It's mostly straight
    forward except perhaps when the quotient is -14.50; should that round
    towards +∞, to -∞, towards 0, to even, to odd.  Most, if not all, of
    these have been used in some context or another.  Presumably, there
    should be no difference between positive/negative and negative/positive?
    
    Unsigned integers avoid most of the trickiness, of course!

--- src/so-0242-2722/irnd11.c
+++ src/so-0242-2722/irnd11.c
@@ -4,6 +4,22 @@
 #include <limits.h>
 #include <math.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+/*
+** What should the results be for:
+** Division    Round Up    Nearest      Round Away
+** +57 / +4      +15         +14          +15
+** +59 / +4      +15         +15          +15
+** +57 / -4      -14         +14          -15
+** +59 / -4      -14         *15          -15
+** -57 / +4      *15         *14          -15
+** -59 / +4      *15         *15          -15
+** -57 / -4      *15         *14          +15
+** -59 / -4      *15         *15          +15
+*/
+
+static inline int signum(int n) { return (n < 0) ? -1 : (n > 0) ? +1 : 0; }
 
 static inline int round_up(int i, int j)
 {
@@ -29,6 +45,19 @@ static void test_one_set(int i, int j)
 
 int main(void)
 {
+    for (int i = 57; i <= 59; i += 2)
+    {
+        int j = 4;
+        /* Positive, positive seems to be working correctly */
+        test_one_set(+i, +j);
+        /* Mixed negative, positive mostly working - maybe some hiccups */
+        test_one_set(-i, +j);
+        /* Mixed positive, negative is badly broken */
+        test_one_set(+i, -j);
+        /* Negative, negative is badly broken */
+        test_one_set(-i, -j);
+    }
+
     for (int i = 50; i <= 70; i++)
     {
         for (int j = 1; j <= 10; j++)
@@ -43,5 +72,6 @@ int main(void)
             //test_one_set(-i, -j);
         }
     }
+
     return 0;
 }
