478927e6740061a5381e35857cec7869597f4927
Jonathan Leffler <jonathan.leffler@gmail.com>
2019-03-02 14:27:51 -0800

    SO 3038-8085 - Add wchar37.c to demonstrate wint_t
    OK; so the file name is a misnomer - it matters not.
    Code in `wchar37.c` deonstrates the use of `fwide()`, etc.

---
+++ src/so-3038-8085/wchar37.c
@@ -0,0 +1,84 @@
+/*
+** Code vaguely related to SO 3038-8085
+**
+** Demonstrate the use of fwide() and wint_t etc.
+**
+** Code of uncertain origin, probably not written by Jonathan Leffler,
+** but probably modified by Jonathan Leffler.
+*/
+#include <locale.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <wchar.h>
+
+static void fileManipulator(FILE *source, FILE *dest);
+static void manipulateToken(wint_t token, FILE *dest);
+
+int main(int argc, const char *argv[])
+{
+    FILE *source, *dest;
+    if (argc != 3)
+    {
+        fprintf(stderr, "Usage: %s input output\n", argv[0]);
+        exit(1);
+    }
+
+    setlocale(LC_ALL, "");
+
+    source = fopen(argv[1], "r");
+    if (source == NULL)
+    {
+        fprintf(stderr, "could not open source file %s\n", argv[1]);
+        exit(1);
+    }
+
+    dest = fopen(argv[2], "w+");
+    if (dest == NULL)
+    {
+        fclose(source);
+        fprintf(stderr, "could not open dest file %s\n", argv[2]);
+        exit(1);
+    }
+
+    fwide(source, 1);
+    fwide(dest, 1);
+
+    fileManipulator(source, dest);
+
+    fclose(source);
+    fclose(dest);
+
+    return 0;
+}
+
+static void fileManipulator(FILE *source, FILE *dest)
+{
+    wint_t token;
+
+    while (WEOF != (token = getwc(source)))
+    {
+        manipulateToken(token, dest);
+    }
+}
+
+static void manipulateToken(wint_t token, FILE *dest)
+{
+    wchar_t *pre = L"- ";
+    wchar_t *post = L" -\n";
+
+    if (EOF == fputws(pre, dest))
+    {
+        fprintf(stderr, "Failed to write prefix string\n");
+        exit(1);
+    }
+    if (WEOF == fputwc(token, dest))
+    {
+        fprintf(stderr, "Failed to write wide character %d\n", (int)token);
+        exit(1);
+    }
+    if (EOF == fputws(post, dest))
+    {
+        fprintf(stderr, "Failed to write suffix string\n");
+        exit(1);
+    }
+}
