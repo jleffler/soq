aae701f004e3f338cb4db4048ec643f8a807aeb5
Jonathan Leffler <jonathan.leffler@gmail.com>
2019-05-17 08:04:08 -0700

    Upgrade src/scripts/pipe-rot.sh
    SO 5618-7565 shows a case where the parent process was not closing the
    pipes soon enough, so a child process was hanging because the parent was
    waiting before closing the pipe descriptors.  Add verbiage to cover that
    case.

--- src/scripts/pipe-rot.sh
+++ src/scripts/pipe-rot.sh
@@ -25,7 +25,9 @@ if [ "$mode" = "answer" ]
 then
 
 cat <<'EOF'
-You aren't closing enough file descriptors in the child.
+You aren't closing enough file descriptors in the child process.
+
+<hr>
 
 **Rule of thumb**: If you
 [`dup2()`](http://pubs.opengroup.org/onlinepubs/9699919799/functions/dup2.html)
@@ -42,6 +44,17 @@ The rule also applies if you duplicate the descriptors with either
 or
 [`fcntl()`](http://pubs.opengroup.org/onlinepubs/9699919799/functions/fcntl.html)
 with `F_DUPFD`
+
+<hr>
+
+If the parent process will not communicate with any of its children via
+the pipe, it must ensure that it closes both ends of the pipe early
+enough (before waiting, for example) so that its children can receive
+EOF indications on read (or get SIGPIPE signals or write errors on
+write), rather than blocking indefinitely.
+Even if the parent uses the pipe without using `dup2()`, it should
+normally close at least one end of the pipe — it is extremely rare for
+a program to read and write on both ends of a single pipe.
 
 EOF
 
