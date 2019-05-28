cc6bbcabd7df94c1258edee49988261c67c5b347
Jonathan Leffler <jonathan.leffler@gmail.com>
2019-05-27 21:46:38 -0700

    SO 5631-4784: Basic data gathering - no timeouts

--- so-5631-4784/chunker79.c
+++ so-5631-4784/chunker79.c
@@ -12,11 +12,13 @@
 
 #include "posixver.h"
 
-#include "time_io.h"
 #include "stderr.h"
+#include "time_io.h"
+#include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/uio.h>
 #include <unistd.h>
 
 static const char optstr[] = "hVd:e:f:s:";
@@ -35,6 +37,56 @@ static const char hlpstr[] =
 extern const char jlss_id_chunker79_c[];
 const char jlss_id_chunker79_c[] = "@(#)$Id$";
 #endif /* lint */
+
+static void read_chunks(FILE *fp, const char *name, struct timespec delay,
+                        struct timespec empty, struct timespec start)
+{
+    size_t num_data = 0;
+    size_t max_data = 0;
+    struct iovec *data = 0;
+    size_t buflen = 0;
+    char *buffer = 0;
+    ssize_t length;
+    size_t chunk_len = 0;
+
+    printf("delay: %3lld.%09ld\n", (long long)delay.tv_sec, delay.tv_nsec);
+    printf("empty: %3lld.%09ld\n", (long long)empty.tv_sec, empty.tv_nsec);
+    printf("start: %3lld.%09ld\n", (long long)start.tv_sec, start.tv_nsec);
+    printf("file:  %s\n", name);
+
+    while ((length = getline(&buffer, &buflen, fp)) != -1)
+    {
+        if (num_data >= max_data)
+        {
+            size_t new_size = (num_data * 2) + 2;
+            void *newspace = realloc(data, new_size * sizeof(data[0]));
+            if (newspace == 0)
+                err_syserr("failed to allocate %zu bytes data: ", new_size * sizeof(data[0]));
+            data = newspace;
+            max_data = new_size;
+        }
+        data[num_data].iov_base = buffer;
+        data[num_data].iov_len = length;
+        num_data++;
+        chunk_len += length;
+        buffer = 0;
+        buflen = 0;
+    }
+
+    if (chunk_len > 0)
+    {
+        if ((length = writev(STDOUT_FILENO, data, num_data)) < 0)
+            err_syserr("failed to write %zu bytes to standard output: ", chunk_len);
+        else if ((size_t)length != chunk_len)
+            err_error("failed to write %zu bytes to standard output "
+                      "(short write of %zu bytes)\n", chunk_len, (size_t)length);
+    }
+
+    for (size_t i = 0; i < num_data; i++)
+        free(data[i].iov_base);
+    free(data);
+    free(buffer);
+};
 
 int main(int argc, char **argv)
 {
@@ -82,10 +134,14 @@ int main(int argc, char **argv)
     if (optind != argc)
         err_usage(usestr);
 
+#if 0
     printf("delay: %3lld.%09ld\n", (long long)delay.tv_sec, delay.tv_nsec);
     printf("empty: %3lld.%09ld\n", (long long)empty.tv_sec, empty.tv_nsec);
     printf("start: %3lld.%09ld\n", (long long)start.tv_sec, start.tv_nsec);
     printf("file:  %s\n", name);
+#endif /* 0 */
+
+    read_chunks(fp, name, delay, empty, start);
 
     return 0;
 }
