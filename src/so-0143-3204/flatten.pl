db71914ae5a11dfb7bbdbc496afcd9d7f9716a74
Jonathan Leffler <jonathan.leffler@gmail.com>
2020-05-29 15:55:00 -0600

    The answer is too big for comfort.
    Flattening lines (flatten.pl) and not indenting code (use ``` .. ```)
    reduces the size to barely enough under 30,000 to be OK.  Further
    expansion of the answer will require paring away verbosity.

---
+++ src/so-0143-3204/flatten.pl
@@ -0,0 +1,11 @@
+#!/usr/bin/perl -p00
+#
+# Read file in paragraph mode.  Flatten running text - six or more
+# alphanumeric words in a row somewhere in a paragraph
+
+if (m/\w+(?: \w+){5,}/)
+{
+    s/\n/ /gms;
+    s/\s+/ /g;
+    $_ .= "\n\n";
+}
