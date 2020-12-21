fbc5da77ae64ca3ae0bb778988df285d1a252324
Jonathan Leffler <jonathan.leffler@gmail.com>
2020-12-20 22:00:10 -0700

    src/scripts: add so-swrecs (standard spiel)

---
+++ src/scripts/so-swrecs.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# Standard spiel on Software Recommendations
+
+cat <<'EOF'
+Welcome to Stack Overflow.
+Please read the [About] and [Ask] pages, concentrating on which
+questions are ['on-topic'](https://stackoverflow.com/help/on-topic)
+here.
+This question is off-topic — one of the close reasons on SO is
+_Seeking recommendations for books, tools, software libraries, and more
+— This question is likely to lead to opinion-based answers._ You might
+be able to ask appropriately on [Software
+Recommendations](https://softwarerecs.stackexchange.com/), but check
+what is [on-topic](https://softwarerecs.stackexchange.com/help/on-topic)
+there, too.
+EOF
