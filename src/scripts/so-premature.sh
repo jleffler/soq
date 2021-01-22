ff627113495c7607b08f9c0db5ef0f1de04143cf
Jonathan Leffler <jonathan.leffler@gmail.com>
2021-01-22 10:54:04 -0700

    script/so-premature.sh - "premature optimization is the root of all evil"

---
+++ src/scripts/so-premature.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Standard quote about premature optimization
+
+cat <<'EOF'
+Donald Knuth's 1974 paper [Structured Programming with go to
+Statements](https://citeseerx.ist.psu.edu/viewdoc/summary?doi=10.1.1.103.6084)
+says:
+
+_"Programmers waste enormous amounts of time thinking about, or worrying
+about, the speed of noncritical parts of their programs, and these
+attempts at efficiency actually have a strong negative impact when
+debugging and maintenance are considered.
+We should forget about small efficiencies, say about 97% of the time:
+premature optimization is the root of all evil.
+Yet we should not pass up our opportunities in that critical 3%."_
+
+See also:
+
+* [Premature Optimization](https://wiki.c2.com/?PrematureOptimization)
+* [Programming Optimization](https://en.wikipedia.org/wiki/Program_optimization)
+* [Donald Knuth](https://en.wikipedia.org/wiki/Donald_Knuth)
+* [Tony Hoare](https://en.wikipedia.org/wiki/Tony_Hoare)
+
+EOF
