4691b15650b69d198e36ac4e9adc7e29fe43b732
Jonathan Leffler <jonathan.leffler@gmail.com>
2021-04-12 13:33:51 -0600

    Basic operation working (tests 2, 3, 4, 8)
    Closing file descriptors TBD

--- src/so-4626-6329/pipe97.c
+++ src/so-4626-6329/pipe97.c
@@ -23,17 +23,17 @@
 **
 ** ********************************************************************
 **
-** Designate the processes P0..P3.
-** Designate the pipes (channels) C0..C3.
+** Designate the 4 processes P0..P3.
+** Designate the 4 pipes (channels) C0..C3.
 **
 ** C0 must be created by P0 before creating P1.
 ** C1 must be created by P0 before creating P1.
-** C2 must be created by P1 before creating P2.
-** C3 must be created by P2 before creating P3.
-** P0 will write to C1 and read from C0.
-** P1 will read from C1 and write to C2.
-** P2 will read from C2 and write to C3.
-** P3 will read from C3 and write to C0.
+** C2 must be created by P0 before creating P1 (because P1 needs to write to it).
+** C3 must be created by P0 before creating P2 (because P2 needs to write to it).
+** P0 will write to  C1 and read from C0.
+** P1 will read from C1 and write to  C2.
+** P2 will read from C2 and write to  C3.
+** P3 will read from C3 and write to  C0.
 **
 ** Hence Pn always reads from Cn and writes to C((n+1)%4).
 ** P0 writes then reads; the other processes read then write.
@@ -46,6 +46,7 @@
 ** this program.
 */
 
+#include <assert.h>
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -67,7 +68,7 @@ static void no_children(void)
     int corpse;
     int status;
     while ((corpse = wait(&status)) > 0)
-        printf("PID %d: child %d exited 0x%.4X\n", (int)getpid(), corpse, status);
+        printf("PID %5d: child %5d exited 0x%.4X\n", (int)getpid(), corpse, status);
 }
 
 static void check_fds(void)
@@ -75,7 +76,7 @@ static void check_fds(void)
     struct stat sb;
     char buffer[128];
     char *bufptr = buffer;
-    int n = snprintf(bufptr, sizeof(buffer), "PID %d: ", (int)getpid());
+    int n = snprintf(bufptr, sizeof(buffer), "PID %5d: ", (int)getpid());
     bufptr += n;
     for (int i = 0; i < 20; i++)
     {
@@ -91,55 +92,107 @@ static void check_fds(void)
 
 static void pr_result(const char *tag, const struct Node *result)
 {
-    printf("%s: PID = %d, PPID = %d, Node = %d (PID %d), Value = %d\n", tag,
+    char buffer[16];
+    snprintf(buffer, sizeof(buffer), "%.12s:", tag);
+    printf("%-13s PID = %5d, PPID = %5d, Node = %d (PID %5d), Value = %5d\n", buffer,
            (int)getpid(), (int)getppid(), result->node, result->pid, result->val);
     fflush(stdout);
 }
 
-static _Noreturn void be_parental(int p0[2], int p1[2])
+static inline int new_bid(void)
+{
+    return rand() % 10000;
+}
+
+static inline void pr_channel(int i, int c[2])
+{
+    printf("%5d: C%d(%d,%d)\n", (int)getpid(), i, c[0], c[1]);
+    fflush(stdout);
+}
+
+static inline void mk_channel(int i, int c[2])
+{
+    if (pipe(c) != 0)
+    {
+        fprintf(stderr, "%5d: failed to create pipe %d, (%d: %s)\n",
+                (int)getpid(), i, errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    pr_channel(i, c);
+}
+
+static _Noreturn void be_parental(int i_pipe[2], int o_pipe[2])
 {
     int pid = getpid();
-    struct Node proc0 = { 0, rand(), pid };
+    struct Node proc0 = { 0, new_bid(), pid };
     struct Node winner = { 0, 0, 0 };
-    printf("PID %d (PPID %d) i = %d: parent at work\n", pid, (int)getppid(), 0);
-    printf("FD {%d, %d, %d, %d}\n", p0[0], p0[1], p1[0], p1[1]);
+    printf("PID %5d (PPID %5d): parent at work\n", pid, (int)getppid());
+    printf("FD {%d, %d, %d, %d}\n", i_pipe[0], i_pipe[1], o_pipe[0], o_pipe[1]);
     check_fds();
 
     pr_result("Opening bid", &proc0);
-    close(p1[0]);
-    if (write(p1[1], &proc0, sizeof(proc0)) != sizeof(proc0))
+    close(o_pipe[0]);
+    printf("Writing opening bid to fd %d\n", o_pipe[1]);
+    if (write(o_pipe[1], &proc0, sizeof(proc0)) != sizeof(proc0))
+    {
+        fprintf(stderr, "%5d: failed to write entry to fd %d (%d: %s)\n",
+                pid, o_pipe[1], errno, strerror(errno));
         exit(EXIT_FAILURE);
+    }
 
-    close(p0[1]);
-    if (read(p0[0], &winner, sizeof(winner)) != sizeof(winner))
+    close(i_pipe[1]);
+    if (read(i_pipe[0], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to read entry from fd %d (%d: %s)\n",
+                pid, i_pipe[0], errno, strerror(errno));
         exit(EXIT_FAILURE);
-    close(p0[0]);
+    }
+    //close(i_pipe[0]);
 
-    pr_result("Winning bid", &winner);
-    if (write(p1[1], &winner, sizeof(winner)) != sizeof(winner))
+    pr_result("Winning node", &winner);
+    if (write(o_pipe[1], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to write entry to fd %d (%d: %s)\n",
+                pid, o_pipe[1], errno, strerror(errno));
         exit(EXIT_FAILURE);
-    close(p1[1]);
+    }
+    close(o_pipe[1]);
+
+    /* Read final result once more */
+    if (read(i_pipe[0], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to read entry from fd %d (%d: %s)\n",
+                pid, i_pipe[0], errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    close(i_pipe[0]);
+    pr_result("Recirculated", &winner);
+
     no_children();
     check_fds();
     exit(EXIT_SUCCESS);
 }
 
-static _Noreturn void be_childish(int i, int p0[2], int p1[2])
+static _Noreturn void be_childish(int i, int i_pipe[2], int o_pipe[2])
 {
     struct Node winner;
     int pid = getpid();
-    printf("PID %d (PPID %d) i = %d: child at work\n", pid, (int)getppid(), i);
-    printf("FD {%d, %d, %d, %d}\n", p0[0], p0[1], p1[0], p1[1]);
+    printf("PID %5d (PPID %5d) i = %d: child at work\n", pid, (int)getppid(), i);
+    printf("FD {%d, %d, %d, %d}\n", i_pipe[0], i_pipe[1], o_pipe[0], o_pipe[1]);
     check_fds();
 
     srand(pid);
-    close(p0[1]);
-    if (read(p0[0], &winner, sizeof(winner)) != sizeof(winner))
+    close(i_pipe[1]);
+    if (read(i_pipe[0], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to read entry from fd %d (%d: %s)\n",
+                pid, i_pipe[0], errno, strerror(errno));
         exit(EXIT_FAILURE);
+    }
     pr_result("Incoming bid", &winner);
 
-    int newValue = rand();
-    printf("PID: %d, new offer %d\n", pid, newValue);
+    int newValue = new_bid();
+    printf("PID: %5d, new offer %5d\n", pid, newValue);
 
     if (newValue > winner.val)
     {
@@ -148,15 +201,30 @@ static _Noreturn void be_childish(int i, int p0[2], int p1[2])
         winner.pid = pid;
     }
 
-    close(p1[0]);
+    close(o_pipe[0]);
     pr_result("Outgoing bid", &winner);
-    if (write(p1[1], &winner, sizeof(winner)) != sizeof(winner))
+    if (write(o_pipe[1], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to write entry to fd %d (%d: %s)\n",
+                pid, o_pipe[1], errno, strerror(errno));
         exit(EXIT_FAILURE);
+    }
 
-    if (read(p0[0], &winner, sizeof(winner)) != sizeof(winner))
+    if (read(i_pipe[0], &winner, sizeof(winner)) != sizeof(winner))
         exit(EXIT_FAILURE);
     pr_result("Final winner", &winner);
-    close(p0[0]);
+    close(i_pipe[0]);
+
+    /* Relay final result to next process */
+    if (write(o_pipe[1], &winner, sizeof(winner)) != sizeof(winner))
+    {
+        fprintf(stderr, "%5d: failed to write entry to fd %d (%d: %s)\n",
+                pid, o_pipe[1], errno, strerror(errno));
+        exit(EXIT_FAILURE);
+    }
+    close(o_pipe[1]);
+
+    /* There should be no children */
     no_children();
     check_fds();
     exit(i);
@@ -168,50 +236,36 @@ int main(int argc, const char *argv[])
     if (argc > 1)
         numproc = atoi(argv[1]);
 
+    if (numproc < 2 || numproc > 10)
+    {
+        fprintf(stderr, "Number of processes %d is out of the range 2..10\n", numproc);
+        exit(EXIT_FAILURE);
+    }
+
     int seed = time(NULL);
-    printf("seed: %d\n", seed);
+    printf("%5d: seed: %d\n", (int)getpid(), seed);
     srand(seed);
 
     int tube[numproc][2];
-    if (pipe(tube[0]) != 0)
-        exit(EXIT_FAILURE);
+
+    mk_channel(0, tube[0]);
+    mk_channel(1, tube[1]);
 
     for (int i = 1; i < numproc; ++i)
     {
-        if (pipe(tube[i]) != 0)
-            exit(EXIT_FAILURE);
+        if (i + 1 < numproc)
+            mk_channel(i + 1, tube[i + 1]);
         int pid = fork();
         if (pid == -1)
         {
-            fprintf(stderr, "fork error (%d: %s)\n", errno, strerror(errno));
+            fprintf(stderr, "fork error for child %d (%d: %s)\n", i, errno, strerror(errno));
             exit(1);
         }
-        else if (pid != 0)
-        {
-            if (i == 1)
-            {
-                printf("PID %d (PPID %d) (i = %d): original parent at work\n",
-                       (int)getpid(), (int)getppid(), i);
-                be_parental(tube[0], tube[1]);
-            }
-            else
-            {
-                printf("PID %d (PPID %d) (i = %d): forked parent at work - about to be childish\n",
-                       (int)getpid(), (int)getppid(), i);
-                be_childish(i, tube[i-1], tube[i]);
-            }
-        }
-        else
-        {
-            printf("PID %d (PPID %d): (i = %d) child at work\n",
-                   (int)getpid(), (int)getppid(), i);
-            if (i > 1)
-            {
-                close(tube[i-1][0]);
-                close(tube[i-1][1]);
-            }
-        }
+        else if (pid == 0)
+            be_childish(i, tube[i], tube[(i+1) % numproc]);
     }
+
+    be_parental(tube[0], tube[1]);
 
     return 0;
 }
