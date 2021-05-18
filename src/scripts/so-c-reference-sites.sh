6237cdcd646f1d9e522b67f95ffa9e86b4d090d5
Jonathan Leffler <jonathan.leffler@gmail.com>
2021-05-18 08:42:56 -0600

    src/scripts: add so-c-reference-sites

---
+++ src/scripts/so-c-reference-sites.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# A collection of reasonably reliable C reference sites
+
+cat <<'EOF'
+
+Reasonably reliable references for C:
+
+* http://port70.net/~nsz/c/c11/n1570.html
+* http://port70.net/~nsz/c/c99/n1256.html
+* http://pubs.opengroup.org/onlinepubs/9699919799
+* http://man7.org/linux/man-pages/man3/printf.3.html
+* https://linux.die.net/man/3/printf
+* https://en.cppreference.com/w/
+* https://docs.microsoft.com/en-us/cpp/c-runtime-library/reference/crt-alphabetical-function-reference?view=vs-2019
+* http://www.open-std.org/jtc1/sc22/wg14/
+* http://www.iso-9899.info/wiki/The_Standard
+
+Less desirable:
+* https://www.cplusplus.com/
+
+Still less desirable:
+* https://www.W3Schools.com/
+* (https://www.w3schools.in/c-tutorial/format-specifiers/)
+
+EOF
