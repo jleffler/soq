2e58936392f95e8193ad9ecbda3023578ef1e135
Jonathan Leffler <jonathan.leffler@gmail.com>
2021-11-11 23:27:43 -0700

    Code working as required

--- so-6993-0640/mm13.c
+++ so-6993-0640/mm13.c
@@ -9,12 +9,14 @@
 
 // SOF - matrix.h
 enum { N = 3 };
-typedef unsigned char *Cell;
+typedef char *Cell;
 typedef Cell Matrix[N][N];
 
 extern void init_matrix(Matrix matrix);
 extern void print_matrix(const char *tag, const Matrix matrix);
 extern void read_matrix(Matrix matrix);
+extern void free_matrix(Matrix matrix);
+extern void multiply_matrices(Matrix m1, Matrix m2, Matrix result);
 // EOF - matrix.h
 
 void init_matrix(Matrix matrix)
@@ -28,13 +30,13 @@ void init_matrix(Matrix matrix)
 
 static void read_cell(int r, int c, Cell *cell)
 {
-    unsigned char buffer[256];
+    char buffer[256];
     if (scanf("%s", buffer) != 1)
         err_error("failed to read value for cell[%d,%d]\n", r, c);
     // validate data in cell?
     if (*cell != 0)
         free(*cell);
-    *cell = (Cell)STRDUP((char *)buffer);
+    *cell = STRDUP(buffer);
 }
 
 void read_matrix(Matrix matrix)
@@ -46,6 +48,18 @@ void read_matrix(Matrix matrix)
     }
 }
 
+void free_matrix(Matrix matrix)
+{
+    for (int r = 0; r < N; r++)
+    {
+        for (int c = 0; c < N; c++)
+        {
+            free(matrix[r][c]);
+            matrix[r][c] = NULL;
+        }
+    }
+}
+
 static int max_cell_length(const Matrix matrix)
 {
     int maxlen = 0;
@@ -53,7 +67,8 @@ static int max_cell_length(const Matrix matrix)
     {
         for (int c = 0; c < N; c++)
         {
-            assert(matrix[r][c] != NULL);
+            if (matrix[r][c] == NULL)
+                continue;
             int curlen = strlen((char *)matrix[r][c]);
             if (curlen > maxlen)
                 maxlen = curlen;
@@ -69,55 +84,53 @@ void print_matrix(const char *tag, const Matrix matrix)
     for (int r = 0; r < N; r++)
     {
         for (int c = 0; c < N; c++)
-            printf("    %*s", width, matrix[r][c]);
+        {
+            const char *value = matrix[r][c];
+            if (value == NULL)
+                value = ".";
+            printf("    %*s", width, value);
+        }
         putchar('\n');
     }
 }
 
-#if 0
-static Cell cell_concat(Cell p1, Cell p2)
+static Cell multiply_row_by_column(int r, int c, Matrix m1, Matrix m2)
 {
-    size_t len1 = strlen(p1);
-    size_t len2 = strlen(p2);
-    size_t tlen = len1 + len2 + sizeof("()*()");
-    Cell result = MALLOC(tlen);
-    snprintf(result, tlen, "(%s)*(%s)", p1, p2);
-    return result;
-}
-
-static Cell mul_add(Cell prefix, Cell c1, Cell c2)
-{
-    Cell result = NULL;
-    if (prefix != 0)
-        result = cell_concat(prefix, "+");
-    result = cell_multiply(result, c1, c2);
+    const char *pad = "";
+    size_t len = N * sizeof("().()");
+    for (int k = 0; k < N; k++)
+        len += strlen(m1[r][k]) + strlen(m2[k][c]);
+    Cell result = MALLOC(len);
+    result[0] = '\0';
+    size_t reslen = 0;
+    for (int k = 0; k < N; k++)
+    {
+        int nbytes = snprintf(&result[reslen], len - reslen, "%s(%s).(%s)",
+                              pad, m1[r][k], m2[k][c]);
+        reslen += nbytes;
+        pad = "+";
+    }
+    // result = simplify(result);
     return result;
 }
 
 void multiply_matrices(Matrix m1, Matrix m2, Matrix result)
 {
-    for (int i = 0; i < N; i++)
+    for (int r = 0; r < N; r++)
     {
-        for (int j = 0; j < N; j++)
+        for (int c = 0; c < N; c++)
         {
-            Cell cell = NULL;
-            for (int k = 0; k < N; k++)
-            {
-                cell = mul_add(cell, m1[i][k], m2[k][j]);
-            }
-            if (result[i][j] != NULL)
-                free(result[i][j]);
-            result[i][j] = cell;
+            if (result[r][c] != NULL)
+                free(result[r][c]);
+            result[r][c] = multiply_row_by_column(r, c, m1, m2);
         }
     }
 }
-#endif /* 0 */
 
 int main(void)
 {
     Matrix m1 = { 0 };
     Matrix m2 = { 0 };
-    //Matrix m3 = { 0 };
     err_setarg0("3x3 Matrix Multiplication");
 
     read_matrix(m1);
@@ -126,11 +139,15 @@ int main(void)
     print_matrix("M1", m1);
     print_matrix("M2", m2);
 
-    //init_matrix(m1);
-    //init_matrix(m2);
-    //init_matrix(m2);
-    //multiply_matrices(m1, m2, m3);
-    //print_matrix("M1.M2", m3);
+    Matrix m3;
+    init_matrix(m3);
+
+    multiply_matrices(m1, m2, m3);
+    print_matrix("M1.M2", m3);
+
+    free_matrix(m1);
+    free_matrix(m2);
+    free_matrix(m3);
 
     return 0;
 }
