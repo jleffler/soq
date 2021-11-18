d223b14c3f432df8eb24f511d49032d0ba9213d2
Jonathan Leffler <jonathan.leffler@gmail.com>
2021-11-17 21:04:41 -0700

    src/scripts: Add soac.sh, sostats.sh, tpop.sh

---
+++ src/scripts/tpop.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# URLs for "The Practice of Programming"
+
+cat <<EOF
+
+See ['The Practice of Programming'](http://www.cs.princeton.edu/~bwk/tpop.webpage/)
+by Kernighan and Pike.
+(See also Wikipedia on [TPOP](https://en.wikipedia.org/wiki/The_Practice_of_Programming)).
+
+<sup>
+_TPOP was previously at
+http://plan9.bell-labs.com/cm/cs/tpop and
+http://cm.bell-labs.com/cm/cs/tpop but both are now ($(isodate date)) broken._
+</sup>
+
+EOF
