4c02984696297403420a351105e54c260f6d52f8
Jonathan Leffler <jonathan.leffler@gmail.com>
2022-01-17 11:51:09 -0700

    Updated to more or less match answer.
    Code includes a minor variation on the comments in the README file.

--- src/so-7073-1987/README.md
+++ src/so-7073-1987/README.md
@@ -3,4 +3,150 @@
 [SO 7073-1987](https://stackoverflow.com/q/70731987) &mdash;
 Represent a tridiagonal sparse matrix in column major order
 
+Summary
+=======
 
+You'll find that (indexing from 1), for an NxN tridiagonal matrix X
+linearized in column-major order into an array Z, the entry in row r:
+
+* On the main diagonal of X is at index (3 * c - 2) of Y for c = 1..N;
+* On the upper-diagonal of X is at (3 * c - 3) of Y (for c = 1..N-1);
+* On the lower-diagonal of X is at (3 * c - 1) of Y (for c = 2..N).
+
+For an element with row r, column c (both in the range 1..N):
+
+* if |r - c| > 1 then the value is zero;
+* if c == r then it is on the leading diagonal;
+* if c = r + 1 then it is on the upper diagonal;
+* if c = r - 1 then it is on the lower diagonal.
+
+Of course, in C, arrays are indexed from zero, not one.
+
+Cogitations
+===========
+
+* NxN tri-diagonal matrix X
+* Linearized in row-major order into array Y with `3*N-2` entries 
+* Linearized in column-major order into array Z with `3*N-2` entries 
+* Non-zero elements have c = r + { -1, 0, +1 }
+* Equivalently they have r = c + { -1, 0, +1 }
+* With one-based indexing, the first row with an element on the lower
+  diagonal is r = 2 and the last row with an element on the upper
+  diagonal is r = N - 1.
+* With zero-based indexing, the first row with an element on the lower
+  diagonal is r = 1 and the last rows with an element on the upper
+  diagonal is r = N - 2.
+* In the linearized vectors, the notation is:
+  - D = diagonal, L = lower diagonal, U = upper diagonal
+
+Row-Major Order with Zero-Based Indexing
+========================================
+
+    X   c = 0   1   2   3   4   5
+          +----------------------
+    r = 0 | 1   2   0   0   0   0
+    r = 1 | 3   4   5   0   0   0
+    r = 2 | 0   6   7   8   0   0
+    r = 3 | 0   0   9  10  11   0
+    r = 4 | 0   0   0  12  13  14
+    r = 5 | 0   0   0   0  15  16
+
+           D  U  L  D  U  L  D  U  L  D  U  L  D  U  L D
+    Y = {  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 }
+    Index  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
+
+* Elements on main  diagonal at indexes: 0, 3, 6, 9, 12, 15
+* Elements on upper diagonal at indexes: 1, 4, 7, 10, 13
+* Elements on lower diagonal at indexes: 2, 5, 8, 11, 14
+
+Function `get_rm_0()`:
+
+* `X[r,c] = 0         if |r - c| > 1`
+* `X[r,c] = 3 * r + 0 if r == c + 0` — Main diagonal
+* `X[r,c] = 3 * r + 1 if r == c - 1` — Upper diagonal
+* `X[r,c] = 3 * r - 1 if r == c + 1` — Lower diagonal
+
+Always subject to 0 <= r < N; 0 <= c < N
+
+Row-Major Order with One-Based Indexing
+=======================================
+
+    X   c = 1   2   3   4   5   6
+          +----------------------
+    r = 1 | 1   2   0   0   0   0
+    r = 2 | 3   4   5   0   0   0
+    r = 3 | 0   6   7   8   0   0
+    r = 4 | 0   0   9  10  11   0
+    r = 5 | 0   0   0  12  13  14
+    r = 6 | 0   0   0   0  15  16
+
+           D  U  L  D  U  L  D  U  L  D  U  L  D  U  L D
+    Y = {  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 }
+    Index  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
+
+* Elements on main  diagonal at indexes: 1, 4, 7, 10, 13, 16
+* Elements on upper diagonal at indexes: 2, 5, 8, 11, 14
+* Elements on lower diagonal at indexes: 3, 6, 9, 12, 15
+
+Function `get_rm_1()`:
+
+* `X[r,c] = 0         if |r - c| > 1`
+* `X[r,c] = 3 * r - 2 if r == c + 0` — Main diagonal
+* `X[r,c] = 3 * r - 1 if r == c - 1` — Upper diagonal
+* `X[r,c] = 3 * r - 3 if r == c + 1` — Lower diagonal
+
+Always subject to 0 < r <= N; 0 <= c <= N
+
+Column-major instead of row-major order
+=======================================
+
+>You are correct.  Here you are considering row-major order.  I
+>wanted to store the elements in the array as:
+>
+> Z = { 1, 3, 2, 4, 6, 5, 7, 9, 8, 10, 12, 11, 13, 15, 14, 16 }
+>
+>as per your example
+
+Column-Major Order with One-Based Indexing
+==========================================
+
+So, you can make a parallel argument to the one I gave.  Annotate
+your column-major array Z similarly to the way I annotated the
+row-major array Y:
+
+           D   L   U   D   L   U   D   L   U   D   L   U   D   L   U   D
+    Z = {  1,  3,  2,  4,  6,  5,  7,  9,  8, 10, 12, 11, 13, 15, 14, 16 }
+    Index  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
+
+* Elements on main  diagonal at indexes: 1, 4, 7, 10, 13, 16
+* Elements on upper diagonal at indexes: 3, 6, 9, 12, 15
+* Elements on lower diagonal at indexes: 2, 5, 8, 11, 14
+
+Function `get_cm_1()`:
+
+* `X[r,c] = 0         if |c - r| > 1`
+* `X[r,c] = 3 * c - 2 if c == r + 0` — Main diagonal
+* `X[r,c] = 3 * c - 3 if c == r + 1` — Upper diagonal
+* `X[r,c] = 3 * c - 1 if c == r - 1` — Lower diagonal
+
+Always subject to 0 < r <= N; 0 < c <= N.
+
+Column-Major Order with Zero-Based Indexing
+===========================================
+
+           D   L   U   D   L   U   D   L   U   D   L   U   D   L   U   D
+    Z = {  1,  3,  2,  4,  6,  5,  7,  9,  8, 10, 12, 11, 13, 15, 14, 16 }
+    Index  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
+
+* Elements on main  diagonal at indexes:  0,  3,  6,  9, 12, 15
+* Elements on upper diagonal at indexes:  2,  5,  8, 11, 14
+* Elements on lower diagonal at indexes:  1,  4,  7, 10, 13
+
+Function `get_cm_0()`:
+
+* `X[r,c] = 0         if |c - r| > 1`
+* `X[r,c] = 3 * c + 0 if c == r + 0` — Main diagonal
+* `X[r,c] = 3 * c - 1 if c == r + 1` — Upper diagonal
+* `X[r,c] = 3 * c + 1 if c == r - 1` — Lower diagonal
+
+Always subject to 0 <= r < N; 0 <= c < N.
--- src/so-7073-1987/td59.c
+++ src/so-7073-1987/td59.c
@@ -3,7 +3,18 @@
 
 /*
 ** NxN tri-diagonal matrix X
-** Linearized into array Y with 3i*N-2 entries 
+** Linearized in row-major order into array Y with 3i*N-2 entries 
+** Linearized in column-major order into array Z with 3i*N-2 entries 
+** Non-zero elements have c = r + { -1, 0, +1 }
+** Equivalently they have r = c + { -1, 0, +1 }
+** With one-based indexing, the first row with an element on the lower
+** diagonal is r = 2 and the last row with an element on the upper
+** diagonal is r = N - 1.
+** With zero-based indexing, the first row with an element on the lower
+** diagonal is r = 1 and the last rows with an element on the upper
+** diagonal is r = N - 2.
+** In the linearized vectors, the notation is:
+** - D = diagonal, L = lower diagonal, U = upper diagonal
 **
 ** Row-Major Order with Zero-Based Indexing
 ** ========================================
@@ -17,9 +28,6 @@
 ** r = 4 | 0   0   0  12  13  14
 ** r = 5 | 0   0   0   0  15  16
 **
-** Linearized:
-**
-** D = diagonal, L = lower diagonal, U = upper diagonal
 **        D  U  L  D  U  L  D  U  L  D  U  L  D  U  L D
 ** Y = {  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 }
 ** Index  0  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
@@ -28,17 +36,12 @@
 ** Elements on upper diagonal at indexes: 1, 4, 7, 10, 13
 ** Elements on lower diagonal at indexes: 2, 5, 8, 11, 14
 **
-** Non-zero elements have c = r + { -1, 0, +1 }
+** Function get_rm_0():
 ** X[r,c] = 0         if |r - c| > 1
 ** X[r,c] = 3 * r + 0 if r == c + 0     Main diagonal
 ** X[r,c] = 3 * r + 1 if r == c - 1     Upper diagonal
 ** X[r,c] = 3 * r - 1 if r == c + 1     Lower diagonal
 ** Always subject to 0 <= r < N; 0 <= c < N
-**
-** Check: Indexing both rows and columns of X and elements of Y from 0
-** X[2,2] = Y[3*2+0] = Y[6] = 7 ✓       Main diagonal
-** X[2,1] = Y[3*2-1] = Y[5] = 6 ✓       Lower diagonal
-** X[2,3] = Y[3*2+1] = Y[7] = 8 ✓       Upper diagonal
 **
 ** Row-Major Order with One-Based Indexing
 ** =======================================
@@ -52,9 +55,6 @@
 ** r = 5 | 0   0   0  12  13  14
 ** r = 6 | 0   0   0   0  15  16
 **
-** Linearized:
-**
-** D = diagonal, L = lower diagonal, U = upper diagonal
 **        D  U  L  D  U  L  D  U  L  D  U  L  D  U  L D
 ** Y = {  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16 }
 ** Index  1  2  3  4  5  6  7  8  9 10 11 12 13 14 15 16
@@ -63,111 +63,62 @@
 ** Elements on upper diagonal at indexes: 2, 5, 8, 11, 14
 ** Elements on lower diagonal at indexes: 3, 6, 9, 12, 15
 **
-** Non-zero elements have c = r + { -1, 0, +1 }
+** Function get_rm_1():
 ** X[r,c] = 0         if |r - c| > 1
 ** X[r,c] = 3 * r - 2 if r == c + 0     Main diagonal
 ** X[r,c] = 3 * r - 1 if r == c - 1     Upper diagonal
 ** X[r,c] = 3 * r - 3 if r == c + 1     Lower diagonal
-** Always subject to 1 <= r <= N; 1 <= c <= N
-**
-** NB: First lower-diagonal entry is for r = 2, not r = 1!
-**
-** Check:
-**
-** X[3,3] = Y[3*3-2] = Y[7] = 7 ✓
-** X[3,4] = Y[3*3-1] = Y[8] = 8 ✓
-** X[3,2] = Y[3*3-0] = y[6] = 6 ✓
+** Always subject to 0 < r <= N; 0 <= c <= N
 **
 ** Column-major instead of row-major order
 ** =======================================
 **
-** >You are correct.  Here you are considering row-major order.  I wanted
-** >to store the elements in the array as:
+** >You are correct.  Here you are considering row-major order.  I
+** >wanted to store the elements in the array as:
 ** >
-** > { 1, 3, 2, 4, 6, 5, 7, 9, 8, 10, 12, 11, 13, 15, 14, 16 }
+** > Z = { 1, 3, 2, 4, 6, 5, 7, 9, 8, 10, 12, 11, 13, 15, 14, 16 }
 ** >
 ** >as per your example
 **
 ** Column-Major Order with One-Based Indexing
 ** ==========================================
 **
-** So, you can make a parallel argument to the one I gave.  Annotate your
-** column-major array Z similarly to the way I annotated the row-major
-** array Y:
+** So, you can make a parallel argument to the one I gave.  Annotate
+** your column-major array Z similarly to the way I annotated the
+** row-major array Y:
 **
 **        D   L   U   D   L   U   D   L   U   D   L   U   D   L   U   D
 ** Z = {  1,  3,  2,  4,  6,  5,  7,  9,  8, 10, 12, 11, 13, 15, 14, 16 }
 ** Index  1   2   3   4   5   6   7   8   9  10  11  12  13  14  15  16
 **
-** X   c = 1   2   3   4   5   6
-**       +----------------------
-** r = 1 | 1   2   0   0   0   0
-** r = 2 | 3   4   5   0   0   0
-** r = 3 | 0   6   7   8   0   0
-** r = 4 | 0   0   9  10  11   0
-** r = 5 | 0   0   0  12  13  14
-** r = 6 | 0   0   0   0  15  16
-**
-** As before, non-zero elements have c = r + { -1, 0, +1 } or,
-** equivalently, r = c + { -1, 0, +1 }.
-**
 ** Elements on main  diagonal at indexes: 1, 4, 7, 10, 13, 16
 ** Elements on upper diagonal at indexes: 3, 6, 9, 12, 15
 ** Elements on lower diagonal at indexes: 2, 5, 8, 11, 14
 **
-** Assuming 1-based indexing, arrange an appropriate permutation of the
-** additions:
-**
+** Function get_cm_1():
 ** X[r,c] = 0         if |c - r| > 1
 ** X[r,c] = 3 * c - 2 if c == r + 0 Main diagonal
 ** X[r,c] = 3 * c - 3 if c == r + 1 Upper diagonal
 ** X[r,c] = 3 * c - 1 if c == r - 1 Lower diagonal
-** Always subject to 1 <= r <= N; 1 <= c <= N.
-**
-** Check (indexing rows and columns of X and elements of Z from 1):
-**
-** X[3,3] = Z[3*3-2] = Z[7] = 7 ✓   Main diagonal
-** X[3,4] = Z[3*4-3] = Z[9] = 8 ✓   Upper diagonal
-** X[3,2] = Z[3*2-1] = Z[5] = 6 ✓   Lower diagonal
-**
-** X[5,5] = Z[3*5-2] = Z[13] = 13 ✓ Main diagonal
-** X[4,5] = Z[3*5-3] = Z[12] = 11 ✓ Upper diagonal
-** X[6,5] = Z[3*5-1] = Z[14] = 15 ✓ Lower diagonal
+** Always subject to 0 < r <= N; 0 < c <= N.
 **
 ** Column-Major Order with Zero-Based Indexing
 ** ===========================================
-**
-** X   c = 0   1   2   3   4   5
-**       +----------------------
-** r = 0 | 1   2   0   0   0   0
-** r = 1 | 3   4   5   0   0   0
-** r = 2 | 0   6   7   8   0   0
-** r = 3 | 0   0   9  10  11   0
-** r = 4 | 0   0   0  12  13  14
-** r = 5 | 0   0   0   0  15  16
 **
 **        D   L   U   D   L   U   D   L   U   D   L   U   D   L   U   D
 ** Z = {  1,  3,  2,  4,  6,  5,  7,  9,  8, 10, 12, 11, 13, 15, 14, 16 }
 ** Index  0   1   2   3   4   5   6   7   8   9  10  11  12  13  14  15
 **
-** And assuming 0-based indexing, arrange an appropriate permutation of
-** the additions:
-**
 ** Elements on main  diagonal at indexes:  0,  3,  6,  9, 12, 15
 ** Elements on upper diagonal at indexes:  2,  5,  8, 11, 14
 ** Elements on lower diagonal at indexes:  1,  4,  7, 10, 13
 **
+** Function get_cm_0():
 ** X[r,c] = 0         if |c - r| > 1
 ** X[r,c] = 3 * c + 0 if c == r + 0 Main diagonal
 ** X[r,c] = 3 * c - 1 if c == r + 1 Upper diagonal
 ** X[r,c] = 3 * c + 1 if c == r - 1 Lower diagonal
 ** Always subject to 0 <= r < N; 0 <= c < N.
-**
-** Check (indexing rows and columns of X and elements of Z from 0):
-**
-** X[2,3] = Z[3*3+0] = Z[9] = 7 ✓   Main diagonal
-** X[3,4] = Z[3*4+3] = Z[9] = 8 ✓
-** X[3,2] = Z[3*2+1] = Z[5] = 6 ✓
 */
 
 #include <assert.h>
@@ -200,7 +151,7 @@ static const int Z[3*N-2] =
 
 /* rm = row major, cm = column major, 0 = zero-based, 1 = one-based */
 
-static inline int get_rm_0(int r, int c)
+static int get_rm_0(int r, int c)
 {
     assert(r >= 0 && r < N);
     assert(c >= 0 && c < N);
@@ -210,7 +161,7 @@ static inline int get_rm_0(int r, int c)
     int index;
     if (r == c)                 /* Main diagonal */
         index = 3 * r + 0;
-    else if (r == c - 1)             /* Upper diagonal */
+    else if (r == c - 1)        /* Upper diagonal */
         index = 3 * r + 1;
     else
     {
@@ -221,7 +172,7 @@ static inline int get_rm_0(int r, int c)
     return Y[index];
 }
 
-static inline int get_rm_1(int r, int c)
+static int get_rm_1(int r, int c)
 {
     assert(r > 0 && r <= N);
     assert(c > 0 && c <= N);
@@ -242,7 +193,7 @@ static inline int get_rm_1(int r, int c)
     return Y[index-1];
 }
 
-static inline int get_cm_0(int r, int c)
+static int get_cm_0(int r, int c)
 {
     assert(r >= 0 && r < N);
     assert(c >= 0 && c < N);
@@ -263,7 +214,7 @@ static inline int get_cm_0(int r, int c)
     return Z[index];
 }
 
-static inline int get_cm_1(int r, int c)
+static int get_cm_1(int r, int c)
 {
     assert(r > 0 && r <= N);
     assert(c > 0 && c <= N);
@@ -311,7 +262,7 @@ static void dump_vector(const char *tag, int values, const int vector[values], i
     {
         len += printf("%s[%2d] = %2d,", pad, i + base, vector[i]);
         pad = " ";
-        if (len > 60)
+        if (len > 80)
         {
             putchar('\n');
             len = 0;
@@ -343,6 +294,7 @@ int main(void)
 {
     dump_matrix("Tridiagonal matrix X", N, N, X, 0);
     reconstruct_matrix("Reconstructed Row-Major Matrix", "Y", 3 * N - 2, Y, N, N, get_rm_0, 0);
+
     dump_matrix("Tridiagonal matrix X", N, N, X, 1);
     reconstruct_matrix("Reconstructed Row-Major Matrix", "Y", 3 * N - 2, Y, N, N, get_rm_1, 1);
 
