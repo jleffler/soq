0e4f6120362c01d66aed148d20dab23e1f49c1a2
Jonathan Leffler <jonathan.leffler@gmail.com>
2022-01-23 08:17:29 -0700

    src/matrices/matmul99.c - revised
    The old matrix multiplication function is still present, renamed to
    matrix_multiply_arcane().  A replacement version which simulates how
    you'd multiply two matrices by hand is added as matrix_multiply().
    The code uses double rather than float for the calculations - but
    the array initializers were accidentally left using float constants.
    
    The difference in the calculation is that the roles of the two inner
    loops are reversed, and by using the sum variable, avoids writing to
    any cell in the output matrix more than once.  One consequence of this
    is that the result matrix does not need to be zeroed first.

--- src/matrices/matmul99.c
+++ src/matrices/matmul99.c
@@ -1,17 +1,18 @@
 /* SO 1719-2011 */
+/* Matrix multiplication using C99 and VLA notation */
 #if defined(__cplusplus)
 #error "This code uses VLA and should not compile under C++"
 #endif
 
 #include <stdio.h>
 
-static float m1[3][4] =
+static double m1[3][4] =
 {
     { +1.0F, -0.02F, +0.76F, +3.14F },
     { +0.0F, +1.97F, +2.13F, +0.21F },
     { +4.1F, -1.37F, -1.00F, -2.11F },
 };
-static float m2[4][2] =
+static double m2[4][2] =
 {
     { +1.50F, -7.22F },
     { +2.31F, +1.99F },
@@ -19,29 +20,52 @@ static float m2[4][2] =
     { -3.41F, -5.26F },
 };
 
-void matrix_multiply(int m, int p, int n, float matrix1[m][p],
-                     float matrix2[p][n], float output[m][n]);
-void matrix_print(const char *tag, int m, int n, float matrix[m][n]);
-void matrix_zero(int m, int n, float matrix[m][n]);
+void matrix_multiply(int m, int p, int n, double matrix1[m][p],
+                     double matrix2[p][n], double output[m][n]);
+void matrix_multiply_arcane(int m, int p, int n, double matrix1[m][p],
+                            double matrix2[p][n], double output[m][n]);
+void matrix_print(const char *tag, int m, int n, double matrix[m][n]);
+void matrix_zero(int m, int n, double matrix[m][n]);
 
 int main(void)
 {
-    float result[3][2];
-    matrix_multiply(3, 4, 2, m1, m2, result);
+    double result[3][2];
     matrix_print("m1", 3, 4, m1);
     matrix_print("m2", 4, 2, m2);
-    matrix_print("m3", 3, 2, result);
+    matrix_multiply(3, 4, 2, m1, m2, result);
+    matrix_print("m3 - simple", 3, 2, result);
+    matrix_multiply_arcane(3, 4, 2, m1, m2, result);
+    matrix_print("m3 - arcane", 3, 2, result);
 }
 
-void matrix_zero(int m, int n, float matrix[m][n])
+/* This code simulates how you'd multiply two matrices by hand */
+void matrix_multiply(int m, int p, int n, double matrix1[m][p],
+                     double matrix2[p][n], double output[m][n])
+{
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            double sum = 0.0;
+            for (int k = 0; k < p; k++)
+                sum += matrix1[i][k] * matrix2[k][j];
+            output[i][j] = sum;
+        }
+    }
+}
+
+void matrix_zero(int m, int n, double matrix[m][n])
 {
     for (int i = 0; i < m; i++)
         for (int j = 0; j < n; j++)
             matrix[i][j] = 0.0;
 }
 
-void matrix_multiply(int m, int p, int n, float matrix1[m][p],
-                     float matrix2[p][n], float output[m][n])
+/* Although correct, the sequence of nested loops here is arcane */
+/* It also requires the result matrix to be zeroed first */
+/* This has poor locality of reference - it skips around the output matrix */
+void matrix_multiply_arcane(int m, int p, int n, double matrix1[m][p],
+                            double matrix2[p][n], double output[m][n])
 {
     matrix_zero(m, n, output);
 
@@ -51,7 +75,7 @@ void matrix_multiply(int m, int p, int n, float matrix1[m][p],
                 output[i][k] += matrix1[i][j] * matrix2[j][k];
 }
 
-void matrix_print(const char *tag, int m, int n, float matrix[m][n])
+void matrix_print(const char *tag, int m, int n, double matrix[m][n])
 {
     printf("%s (%d x %d):\n", tag, m, n);
     for (int i = 0; i < m; i++)
@@ -59,7 +83,7 @@ void matrix_print(const char *tag, int m, int n, float matrix[m][n])
         char *pad = "[ ";
         for (int j = 0; j < n; j++)
         {
-            printf("%s%+7.3f", pad, matrix[i][j]);
+            printf("%s%+7.4f", pad, matrix[i][j]);
             pad = ", ";
         }
         printf("%s", " ]\n");
