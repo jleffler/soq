45b53ce1cf3c37d45b2e4b147d3811ab5504702f
Jonathan Leffler <jonathan.leffler@gmail.com>
2022-03-30 08:49:29 -0600

    Add script so-shuffle for questions/links about shuffling arrays

---
+++ src/scripts/so-shuffle.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# Blurb for an SO question about shuffling arrays (in C)
+
+nbncl <<'EOF'
+
+See also:
+
+• [Shuffle array in C](https://stackoverflow.com/q/06127503/15168)
+• Wikipedia on [Fisher-Yates Shuffle](http://en.wikipedia.org/wiki/Fisher%E2%80%93Yates_shuffle)
+• Jeff Atwood on [The Danger of Naivete](http://www.codinghorror.com/blog/2007/12/the-danger-of-naivete.html).
+• Ben Pfaff's [implementation](http://benpfaff.org/writings/clc/shuffle.html)
+• [Why does this simple shuffle algorithm produce biassed results?](https://stackoverflow.com/q/859253/15168)
+• [Is this C implementation of Fisher-Yates shuffle correct?](https://stackoverflow.com/q/3343797/15168)
+
+EOF
