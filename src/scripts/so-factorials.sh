93e22f7c4d636b5abce8c8e0775d843e1b9d2717
Jonathan Leffler <jonathan.leffler@gmail.com>
2022-05-12 11:53:33 -0600

    so-factorials.sh: range for double and long double

--- src/scripts/so-factorials.sh
+++ src/scripts/so-factorials.sh
@@ -4,11 +4,17 @@
 
 cat <<'EOF'
 
-A 32-bit integer can store factorials up to 12!.
-A 64-bit integer can store factorials up to 20!.
-A 128-bit (unsigned) integer can store factorials up to 34!.
-A 256-bit integer can store factorials up to 57!.
+A 32-bit integer can store factorials up to 12!
+A 64-bit integer can store factorials up to 20!
+A 128-bit (unsigned) integer can store factorials up to 34!
+A 256-bit integer can store factorials up to 57!
 A 512-bit (unsigned) integer can store factorials up to 98!
+
+Using IEEE 754 64-bit floating-point arithmetic, you can store
+approximations up to 170!  (7.257415615307994E+306)
+Using IEEE 754 128-bit floating-point arithmetic, you can store
+approximations up to 1754!  (1.979261890105010E+4930)
+
 You probably don't have a compiler that handles sizes bigger than 64-bit
 integers (though GCC has rudimentary support for 128-bit integers).
 
