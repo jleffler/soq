3a67836aaa9b86960daadb3fef0093151037c874
Jonathan Leffler <jonathan.leffler@gmail.com>
2022-07-01 12:23:20 -0600

    src/scripts: add so-codereview
    Generate a standard comment appropriate for code that might be
    better placed on Code Review.

---
+++ src/scripts/so-codereview.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Standard comment on Code Review
+
+tr -s ' \t\n' '   ' <<'EOF'
+
+If your code works but you are seeking advice on how to improve it, then
+you should probably post your question on
+[Code Review](https://codereview.stackexchange.com),
+but be sure to read their
+[rules of engagement](https://codereview.stackexchange.com/about)
+first (see also
+[On Topic](https://codereview.stackexchange.com/help/on-topic)
+and
+[How to Ask](https://codereview.stackexchange.com/help/how-to-ask)).
+
+EOF
+echo ''
