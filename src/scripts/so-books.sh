260d5206639a93e53db651892bf7f992e6266702
Jonathan Leffler <jonathan.leffler@gmail.com>
2023-03-01 09:44:58 -0700

    scripts/so-books.sh - smile --> www
    Amazon has cancelled the smile.amazon.com system - use www.amazon.com instead

--- src/scripts/so-books.sh
+++ src/scripts/so-books.sh
@@ -5,90 +5,90 @@
 cat << 'EOF'
 
 W. Richard Stevens, Bill Fenner, Andrew M. Rudoff
-[UNIX® Network Programming, Vol 1: The Sockets Networking API, 3rd Edn](https://smile.amazon.com/dp/0131411551) 2003.
+[UNIX® Network Programming, Vol 1: The Sockets Networking API, 3rd Edn](https://www.amazon.com/dp/0131411551) 2003.
 [UNIX® Network Programming, Vol 1: The Sockets Networking API](http://unpbook.com/)
 
 W Richard Stevens
-[Unix Network Programming, Vol 2: Interprocess Communications, 2nd Edn](https://smile.amazon.com/dp/0132974290) 1998.
+[Unix Network Programming, Vol 2: Interprocess Communications, 2nd Edn](https://www.amazon.com/dp/0132974290) 1998.
 
 W Richard Stevens, Stephen A Rago
-[Advanced Programming in the Unix Environment, 3rd Edn](https://smile.amazon.com/dp/0321637739)
+[Advanced Programming in the Unix Environment, 3rd Edn](https://www.amazon.com/dp/0321637739)
 2013.
 
 Marc J Rochkind
-[Advanced Unix Programming, 2nd Edn](https://smile.amazon.com/dp/0131411543) 2004.
+[Advanced Unix Programming, 2nd Edn](https://www.amazon.com/dp/0131411543) 2004.
 
 Michael Kerrisk
-[The Linux Programming Interface: A Linux and Unix System Programming Handbook, 1st Edn](https://smile.amazon.com/dp/1593272200) 2010.
+[The Linux Programming Interface: A Linux and Unix System Programming Handbook, 1st Edn](https://www.amazon.com/dp/1593272200) 2010.
 
 Brian W Kernighan, Rob Pike
-[The Unix Programming Environment](https://smile.amazon.com/dp/013937681X) Nov 1983.
+[The Unix Programming Environment](https://www.amazon.com/dp/013937681X) Nov 1983.
 
 Brian W Kernighan, Rob Pike
 [The Practice of Programming](http://www.cs.princeton.edu/~bwk/tpop.webpage/)
-[The Practice of Programming](https://smile.amazon.com/dp/020161586X) 1999.
+[The Practice of Programming](https://www.amazon.com/dp/020161586X) 1999.
 
 CodeSourcery LLC
 [Advanced Linux Programming](http://advancedlinuxprogramming.com/)
-[Advanced Linux Programming](https://smile.amazon.com/dp/0735710430)
+[Advanced Linux Programming](https://www.amazon.com/dp/0735710430)
 by Mark Mitchell, Jeffrey Oldham, and Alex Samuel, of CodeSourcery LLC
 published by New Riders Publishing
 ISBN 0-7357-1043-0
 First Edn, June 2001
 
 Jon Bentley
-[Programming Pearls, 2nd Edn](https://smile.amazon.com/dp/0201657880/) 1999.
+[Programming Pearls, 2nd Edn](https://www.amazon.com/dp/0201657880/) 1999.
 
 Jon Bentley
-[Programming Pearls, 1st Edn](https://smile.amazon.com/dp/B00RWQILIS/) 1986.
+[Programming Pearls, 1st Edn](https://www.amazon.com/dp/B00RWQILIS/) 1986.
 
 Jon Bentley
-[More Programming Pearls](https://smile.amazon.com/dp/0201118890/) 1988.
+[More Programming Pearls](https://www.amazon.com/dp/0201118890/) 1988.
 
 Andrew Koenig
-[C Traps and Pitfalls](https://smile.amazon.com/gp/product/0201179288) 1989.
+[C Traps and Pitfalls](https://www.amazon.com/gp/product/0201179288) 1989.
 
 P J Plauger
-[The Standard C Library](https://smile.amazon.com/dp/0131315099) 1992.
+[The Standard C Library](https://www.amazon.com/dp/0131315099) 1992.
 
 K N King
-[C Programming: A Modern Approach, 2nd Edn](https://smile.amazon.com/dp/0393979504/) 2008.
+[C Programming: A Modern Approach, 2nd Edn](https://www.amazon.com/dp/0393979504/) 2008.
 
 David R Hanson
-[C Interfaces and Implementations](https://smile.amazon.com/dp) 1996.
+[C Interfaces and Implementations](https://www.amazon.com/dp) 1996.
 
 Brian W Kernighan and Dennis M Ritchie
-[The C Programming Language, 2nd Edn](https://smile.amazon.com/dp/0131103628) 1988.
+[The C Programming Language, 2nd Edn](https://www.amazon.com/dp/0131103628) 1988.
 Excellent but dated.
 
 Brian W Kernighan and Dennis M Ritchie
-[The C Programming Language, 1st Edn](https://smile.amazon.com/dp/0131101633) 1978.
+[The C Programming Language, 1st Edn](https://www.amazon.com/dp/0131101633) 1978.
 Excellent but even more dated than the second edition.
 
 Steve Maguire
-[Writing Solid Code: 20th Anniversary 2nd Edn](https://smile.amazon.com/dp/1570740550) 2013.
-[Writing Solid Code](https://smile.amazon.com/dp/1556155514) 1993.
+[Writing Solid Code: 20th Anniversary 2nd Edn](https://www.amazon.com/dp/1570740550) 2013.
+[Writing Solid Code](https://www.amazon.com/dp/1556155514) 1993.
 There are those who intensely dislike this book; I think it is quite useful.
 
 Eric S Raymond
-[The Art of Unix Programming](https://smile.amazon.com/dp/0131429019/) 2004.
+[The Art of Unix Programming](https://www.amazon.com/dp/0131429019/) 2004.
 
 Steve McConnell
-[Code Complete 2nd Edn](https://smile.amazon.com/dp/0735619670/) 2004.
+[Code Complete 2nd Edn](https://www.amazon.com/dp/0735619670/) 2004.
 
 Jens Gustedt
-[Modern C](https://smile.amazon.com/dp/1617295817) 2020.
+[Modern C](https://www.amazon.com/dp/1617295817) 2020.
 [Modern C](https://www.manning.com/books/modern-c)
 
 Kamran Amini
-[Extreme C](https://smile.amazon.com/dp/1789343623) 2019.
+[Extreme C](https://www.amazon.com/dp/1789343623) 2019.
 [Extreme C](https://www.packtpub.com/free-ebook/extreme-c/9781789343625)
 
 Martin Fowler
-[Refactoring: Improving the Design of Existing Code, 2nd Edition](https://smile.amazon.com/dp/0134757599) May 2021. 
-[Refactoring: Improving the Design of Existing Code](https://smile.amazon.com/dp/0201485672) June 1999. 
+[Refactoring: Improving the Design of Existing Code, 2nd Edition](https://www.amazon.com/dp/0134757599) May 2021. 
+[Refactoring: Improving the Design of Existing Code](https://www.amazon.com/dp/0201485672) June 1999. 
 
 Robert C Martin
-[Clean Code: A Handbook of Agile Software Craftsmanship](https://smile.amazon.com/dp/0132350882) Aug 2008.
+[Clean Code: A Handbook of Agile Software Craftsmanship](https://www.amazon.com/dp/0132350882) Aug 2008.
 
 EOF
