3cae5c81d923d09ebbc9bf0e64db3687c0dedc38
Jonathan Leffler <jonathan.leffler@gmail.com>
2024-08-14 10:47:26 -0600

    Update posixfun, posixcmd, posixhdr to POSIX 2024

--- src/scripts/posixfun.sh
+++ src/scripts/posixfun.sh
@@ -2,7 +2,9 @@
 #
 # Echo URLs for POSIX functions
 
-baseurl="https://pubs.opengroup.org/onlinepubs/9699919799"
+#baseurl="https://pubs.opengroup.org/onlinepubs/9699919799" # POSIX 2014
+
+baseurl="https://pubs.opengroup.org/onlinepubs/9799919799" # POSIX 2024
 bq='`'
 
 for function in "$@"
